0479cde1742266c60a4f8c207182e9c045d3cb89
Wishoon <heonga2@gmail.com>
2022-06-30 13:37:04 +0900

    chore: 백엔드 프로젝트 초기 환경 설정

---
+++ backend/src/main/java/com/woowacourse/kkogkkog/KkogKkogApplication.java
@@ -0,0 +1,13 @@
+package com.woowacourse.kkogkkog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkogKkogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkogKkogApplication.class, args);
+    }
+
+}
