d1471629708600d94707f47d1fb3adc2d968791f
junzzi <jjh01027717502@gmail.com>
2022-07-03 21:28:35 +0900

    chore: webpack config, ts config 설정

---
+++ frontend/src/index.tsx
@@ -0,0 +1,9 @@
+import App from "@/App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(<App></App>);
