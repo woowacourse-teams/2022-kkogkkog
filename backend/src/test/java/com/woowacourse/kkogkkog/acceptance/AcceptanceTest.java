93d214e8f7d5a9e169748ab2f8eff4137cd9bfcf
Wishoon <heonga2@gmail.com>
2022-07-06 11:39:26 +0900

    test: AcceptanceTest에 DatabaseCleaner 설정

---
+++ backend/src/test/java/com/woowacourse/kkogkkog/acceptance/AcceptanceTest.java
@@ -0,0 +1,25 @@
+package com.woowacourse.kkogkkog.acceptance;
+
+import com.woowacourse.kkogkkog.acceptance.support.DatabaseCleaner;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class AcceptanceTest {
+
+    @LocalServerPort
+    protected int port;
+
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+        databaseCleaner.execute();
+    }
+}
