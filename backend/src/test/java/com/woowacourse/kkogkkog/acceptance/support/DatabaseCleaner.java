31e5d6a6adff0648757d3cbc02cc3b7a89043ce7
Hyunta <mohai2618@naver.com>
2022-07-06 12:38:36 +0900

    fix: 테이블에 auto increment 값 초기화 버그 수정

--- backend/src/test/java/com/woowacourse/kkogkkog/acceptance/support/DatabaseCleaner.java
+++ backend/src/test/java/com/woowacourse/kkogkkog/acceptance/support/DatabaseCleaner.java
@@ -21,9 +21,9 @@ public class DatabaseCleaner implements InitializingBean {
     @Override
     public void afterPropertiesSet() throws Exception {
         tableNames = entityManager.getMetamodel().getEntities().stream()
-            .filter(e -> e.getJavaType().getAnnotation(Entity.class) != null)
-            .map(e -> CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, e.getName()))
-            .collect(Collectors.toList());
+                .filter(e -> e.getJavaType().getAnnotation(Entity.class) != null)
+                .map(e -> CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, e.getName()))
+                .collect(Collectors.toList());
     }
 
     @Transactional
@@ -34,7 +34,7 @@ public class DatabaseCleaner implements InitializingBean {
         for (String tableName : tableNames) {
             entityManager.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
             entityManager.createNativeQuery("ALTER TABLE " + tableName + " ALTER COLUMN "
-                + "ID RESTART WITH 1").executeUpdate();
+                    + tableName + "_" + "ID RESTART WITH 1").executeUpdate();
         }
         entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
     }
