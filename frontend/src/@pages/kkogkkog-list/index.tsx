376a36cc2766b831dd1d0b04b183df05a7214313
junzzi <jjh01027717502@gmail.com>
2022-07-07 21:36:31 +0900

    feat: kkogkkog-list page Suspense UI 추가
    Co-authored-by: 김의진 <gytks4@naver.com>

--- frontend/src/@pages/kkogkkog-list/index.tsx
+++ frontend/src/@pages/kkogkkog-list/index.tsx
@@ -1,27 +1,46 @@
 import styled from '@emotion/styled';
+import { Suspense } from 'react';
 import { useQuery } from 'react-query';
 import { Link } from 'react-router-dom';
 
 import PageTemplate from '@/@components/@shared/PageTemplate';
+import KkogKkogItem from '@/@components/kkogkkog/KkogKkogItem';
 import KkogKkogList from '@/@components/kkogkkog/KkogKkogList';
 import { getKkogkkog } from '@/apis/kkogkkog';
 import { PATH } from '@/Router';
 import { KkogKkog } from '@/types/domain';
 
-const KkogkkogListPage = () => {
-  const {
-    data: { data: kkogkkogList },
-    isLoading,
-    error,
-  } = useQuery<{ data: KkogKkog[] }>('kkogkkogList', getKkogkkog);
-
+const Loading = () => {
   return (
     <PageTemplate title='꼭꼭 리스트'>
       <StyledRoot>
-        <Link to={PATH.KKOGKKOG_CREATE}>생성해볼래?</Link>
-        <KkogKkogList kkogkkogList={kkogkkogList} />
+        <Link to={PATH.KKOGKKOG_CREATE}>
+          <KkogKkogItem.CreateLinkButton />
+        </Link>
+        <KkogKkogItem.Skeleton />
+        <KkogKkogItem.Skeleton />
+        <KkogKkogItem.Skeleton />
       </StyledRoot>
     </PageTemplate>
+  );
+};
+
+const KkogkkogListPage = () => {
+  const { data } = useQuery<{ data: KkogKkog[] }>('kkogkkogList', getKkogkkog);
+
+  const { data: kkogkkogList } = data;
+
+  return (
+    <Suspense fallback={<Loading />}>
+      <PageTemplate title='꼭꼭 리스트'>
+        <StyledRoot>
+          <Link to={PATH.KKOGKKOG_CREATE}>
+            <KkogKkogItem.CreateLinkButton />
+          </Link>
+          <KkogKkogList kkogkkogList={kkogkkogList} />
+        </StyledRoot>
+      </PageTemplate>
+    </Suspense>
   );
 };
 
@@ -31,4 +50,8 @@ export const StyledRoot = styled.div`
   padding: 20px;
 
   border-radius: 4px;
+
+  & > div {
+    margin-top: 20px;
+  }
 `;
