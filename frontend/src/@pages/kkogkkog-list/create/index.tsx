6b387ba0d7dd3f7ccc93921cb0452b23c80d30e3
junzzi <jjh01027717502@gmail.com>
2022-07-07 21:36:59 +0900

    refactor: form이 완성되지 않으면 꼭꼭을 생성할 수 없다.
    Co-authored-by: 김의진 <gytks4@naver.com>

--- frontend/src/@pages/kkogkkog-list/create/index.tsx
+++ frontend/src/@pages/kkogkkog-list/create/index.tsx
@@ -83,6 +83,13 @@ const KkogkkogCreatePage = () => {
   const onSubmitCreateForm = e => {
     e.preventDefault();
 
+    if (senderName.length === 0 || receiverName.length === 0) {
+      // alert('정보를 모두 입력해주세요 !');
+      alert('정보를 모두 입력해주세요 !');
+
+      return;
+    }
+
     mutate({
       senderName,
       receiverName,
