ae3d0587dec92d7895b4fe0f51763fca024c5dcc
junzzi <jjh01027717502@gmail.com>
2022-07-07 21:32:47 +0900

    feat: Loading 컴포넌트 개발
    Co-authored-by: 김의진 <gytks4@naver.com>

---
+++ frontend/src/@components/@shared/Dimmed/index.tsx
@@ -0,0 +1,18 @@
+import styled from '@emotion/styled';
+
+export const Dimmed = styled.div`
+  width: 100vw;
+  height: 100vh;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: rgba(0, 0, 0, 0.5);
+
+  z-index: 10000;
+`;
