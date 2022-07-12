9ed1a93b478c4e97b556ad80ab90d3279d8848b7
junzzi <jjh01027717502@gmail.com>
2022-07-12 16:12:30 +0900

    refactor: Placeholder typing 컨벤션에 맞게 수정
    Co-authored-by: 김의진 <gytks4@naver.com>

--- frontend/src/@components/@shared/Placeholder/index.tsx
+++ frontend/src/@components/@shared/Placeholder/index.tsx
@@ -2,7 +2,9 @@ import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { CSSProperties } from 'react';
 
-const Placeholder = styled.div`
+type PlaceholderProps = Pick<CSSProperties, 'width' | 'height' | 'aspectRatio'>;
+
+const Placeholder = styled.div<PlaceholderProps>`
   border-radius: 4px;
   background-image: linear-gradient(90deg, #e0e0e0 0px, #ededed 30px, #e0e0e0 60px);
   animation: refresh 2s infinite ease-out;
@@ -17,11 +19,7 @@ const Placeholder = styled.div`
     }
   }
 
-  ${({
-    width = '100%',
-    height = '100%',
-    aspectRatio,
-  }: Pick<CSSProperties, 'width' | 'height' | 'aspectRatio'>) => css`
+  ${({ width = '100%', height = '100%', aspectRatio }) => css`
     width: ${width};
     height: ${height};
 
