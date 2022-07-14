3f5a5951da7171976d75c61502c94a73512a7897
gytks4 <gytks4@naver.com>
2022-07-14 16:42:33 +0900

    refactor: KkogKkogModal 컴포넌트로 분리
    - 프로토타입에서 컴포넌트로 변경

--- frontend/src/@components/kkogkkog/KkogKkogItem/index.tsx
+++ frontend/src/@components/kkogkkog/KkogKkogItem/index.tsx
@@ -1,10 +1,5 @@
-import { css } from '@emotion/react';
-
-import Button from '@/@components/@shared/Button';
-import Modal from '@/@components/@shared/Modal';
 import Placeholder from '@/@components/@shared/Placeholder';
 import { KkogKkog } from '@/types/domain';
-import { THUMBNAIL } from '@/utils/constants/kkogkkog';
 
 import * as Styled from './style';
 
@@ -54,37 +49,6 @@ KkogKkogItem.LinkButton = function LinkButton() {
 };
 KkogKkogItem.Skeleton = function Skeleton() {
   return <Placeholder aspectRatio='3/1' />;
-};
-
-interface KkogKkogItemModalProps {
-  clickedKkogKkog: KkogKkog;
-  handleCloseModal: () => void;
-}
-
-KkogKkogItem.Modal = function KkogKkogItemModal(props: KkogKkogItemModalProps) {
-  const { clickedKkogKkog, handleCloseModal } = props;
-
-  return (
-    <Modal position='bottom' title='쿠폰을 사용하시겠어요?' onCloseModal={handleCloseModal}>
-      <KkogKkogItem
-        key={clickedKkogKkog.id}
-        {...clickedKkogKkog}
-        thumbnail={THUMBNAIL[clickedKkogKkog.couponType]}
-        css={css`
-          margin-bottom: 30px;
-        `}
-      />
-      <Styled.Message>{clickedKkogKkog.message}</Styled.Message>
-      <Styled.ButtonContainer>
-        <Styled.ButtonInner>
-          <Button>사용 완료</Button>
-        </Styled.ButtonInner>
-        <Styled.ButtonInner>
-          <Button>사용 요청</Button>
-        </Styled.ButtonInner>
-      </Styled.ButtonContainer>
-    </Modal>
-  );
 };
 
 export default KkogKkogItem;
---
+++ frontend/src/@components/kkogkkog/KkogKkogModal/index.tsx
@@ -0,0 +1,42 @@
+import { css } from '@emotion/react';
+
+import Button from '@/@components/@shared/Button';
+import Modal from '@/@components/@shared/Modal';
+import { KkogKkog } from '@/types/domain';
+import { THUMBNAIL } from '@/utils/constants/kkogkkog';
+
+import KkogKkogItem from '../KkogKkogItem';
+import * as Styled from './style';
+
+interface KkogKkogItemProps {
+  clickedKkogKkog: KkogKkog;
+  handleCloseModal: () => void;
+}
+
+const KkogKkogModal = (props: KkogKkogItemProps) => {
+  const { clickedKkogKkog, handleCloseModal } = props;
+
+  return (
+    <Modal position='bottom' title='쿠폰을 사용하시겠어요?' onCloseModal={handleCloseModal}>
+      <KkogKkogItem
+        key={clickedKkogKkog.id}
+        {...clickedKkogKkog}
+        thumbnail={THUMBNAIL[clickedKkogKkog.couponType]}
+        css={css`
+          margin-bottom: 30px;
+        `}
+      />
+      <Styled.Message>{clickedKkogKkog.message}</Styled.Message>
+      <Styled.ButtonContainer>
+        <Styled.ButtonInner>
+          <Button>사용 완료</Button>
+        </Styled.ButtonInner>
+        <Styled.ButtonInner>
+          <Button>사용 요청</Button>
+        </Styled.ButtonInner>
+      </Styled.ButtonContainer>
+    </Modal>
+  );
+};
+
+export default KkogKkogModal;
