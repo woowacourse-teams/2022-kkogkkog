da869090ba74baa434326530897869fe3acfd027
junzzi <jjh01027717502@gmail.com>
2022-07-14 13:21:57 +0900

    refactor: Input Component를 두 가지 형태(레이블, 레이블리스)로 분리

--- frontend/src/@components/@shared/Input/index.tsx
+++ frontend/src/@components/@shared/Input/index.tsx
@@ -4,17 +4,27 @@ import * as Styled from './style';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
-  isShowLabel?: boolean;
   description?: string;
 }
 
 const Input = (props: InputProps) => {
-  const { label, isShowLabel = true, description, id, type = 'text', ...rest } = props;
+  const { label, description, id, type = 'text', ...rest } = props;
 
   return (
-    <Styled.Root isShowLabel={isShowLabel}>
+    <Styled.Root>
       <label htmlFor={id}>{label}</label>
       {description && <Styled.Description>{description}</Styled.Description>}
+      <Styled.Input id={id} type={type} {...rest} />
+    </Styled.Root>
+  );
+};
+
+Input.HiddenLabel = function HiddenLabel(props: InputProps) {
+  const { label, id, type = 'text', ...rest } = props;
+
+  return (
+    <Styled.Root isShowLabel={false}>
+      <label htmlFor={id}>{label}</label>
       <Styled.Input id={id} type={type} {...rest} />
     </Styled.Root>
   );
