9c4332588601b4e2154708fecf0a088f00bb0b73
gytks4 <gytks4@naver.com>
2022-07-14 14:41:31 +0900

    feat: 꼭꼭 클릭 시 모달 올라오도록 구현

--- frontend/src/@components/kkogkkog/KkogKkogList/index.tsx
+++ frontend/src/@components/kkogkkog/KkogKkogList/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import KkogKkogItem from '@/@components/kkogkkog/KkogKkogItem';
 import { KkogKkog } from '@/types/domain';
 import { THUMBNAIL } from '@/utils/constants/kkogkkog';
@@ -10,12 +12,32 @@ interface KkogKkogListProps {
 
 const KkogKkogList = (props: KkogKkogListProps) => {
   const { kkogkkogList } = props;
+  const [clickedKkogKkog, setClickedKkogKkog] = useState<KkogKkog | null>(null);
+
+  const handleClickKkogKkog = (kkogkkog: KkogKkog) => () => {
+    setClickedKkogKkog(kkogkkog);
+  };
+
+  const handleCloseModal = () => {
+    setClickedKkogKkog(null);
+  };
 
   return (
     <Styled.Root>
       {kkogkkogList.map(kkogkkog => (
-        <KkogKkogItem key={kkogkkog.id} {...kkogkkog} thumbnail={THUMBNAIL[kkogkkog.couponType]} />
+        <KkogKkogItem
+          key={kkogkkog.id}
+          {...kkogkkog}
+          thumbnail={THUMBNAIL[kkogkkog.couponType]}
+          onClickKkogKkog={handleClickKkogKkog({
+            ...kkogkkog,
+            thumbnail: THUMBNAIL[kkogkkog.couponType],
+          })}
+        />
       ))}
+      {clickedKkogKkog && (
+        <KkogKkogItem.Modal clickedKkogKkog={clickedKkogKkog} handleCloseModal={handleCloseModal} />
+      )}
     </Styled.Root>
   );
 };
