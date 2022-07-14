bc0a0679dbfffce2c11bca4510e3a7b6a279aeb5
Hyunta <mohai2618@naver.com>
2022-07-14 16:45:09 +0900

    refactor: static import로 변경
    Co-authored-by: wishoon <heonga2@gmail.com>

--- backend/src/test/java/com/woowacourse/kkogkkog/application/AuthServiceTest.java
+++ backend/src/test/java/com/woowacourse/kkogkkog/application/AuthServiceTest.java
@@ -1,9 +1,10 @@
 package com.woowacourse.kkogkkog.application;
 
+import static com.woowacourse.kkogkkog.fixture.MemberFixture.ROOKIE;
+
 import com.woowacourse.kkogkkog.application.dto.TokenResponse;
 import com.woowacourse.kkogkkog.domain.Member;
 import com.woowacourse.kkogkkog.domain.repository.MemberRepository;
-import com.woowacourse.kkogkkog.fixture.MemberFixture;
 import com.woowacourse.kkogkkog.presentation.dto.TokenRequest;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -25,7 +26,7 @@ class AuthServiceTest {
     @Test
     @DisplayName("로그인에 성공하면 토큰을 반환한다")
     void login() {
-        Member rookie = MemberFixture.ROOKIE;
+        Member rookie = ROOKIE;
         memberRepository.save(rookie);
 
         TokenRequest tokenRequest = new TokenRequest(rookie.getEmail(), rookie.getPassword());
