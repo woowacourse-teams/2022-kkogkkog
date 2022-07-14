3be09535ab51d874a3e540b66f46f002835aaa2a
Hyunta <mohai2618@naver.com>
2022-07-14 20:56:53 +0900

    refactor: 코드 스타일 수정

--- backend/src/main/java/com/woowacourse/kkogkkog/exception/ForbiddenException.java
+++ backend/src/main/java/com/woowacourse/kkogkkog/exception/ForbiddenException.java
@@ -1,6 +1,6 @@
 package com.woowacourse.kkogkkog.exception;
 
-public class ForbiddenException extends RuntimeException{
+public class ForbiddenException extends RuntimeException {
 
     public ForbiddenException(String message) {
         super(message);
