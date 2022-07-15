b812c59750acde1e5421d573f5730af102bc234c
Wishoon <heonga2@gmail.com>
2022-07-15 16:02:54 +0900

    feat: MemberController의 본인 정보 조회 기능 구현
    - Interceptor URI 추가
    - RestDocs 작성
    
    Co-authored-by: Hyunta <mohai2618@gmail.com>

--- backend/src/main/java/com/woowacourse/kkogkkog/presentation/MemberController.java
+++ backend/src/main/java/com/woowacourse/kkogkkog/presentation/MemberController.java
@@ -1,8 +1,10 @@
 package com.woowacourse.kkogkkog.presentation;
 
 import com.woowacourse.kkogkkog.application.MemberService;
+import com.woowacourse.kkogkkog.application.dto.MemberResponse;
 import com.woowacourse.kkogkkog.presentation.dto.MemberCreateRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,5 +25,12 @@ public class MemberController {
         memberService.save(memberCreateRequest);
 
         return ResponseEntity.created(null).build();
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<MemberResponse> showMe(@LoginMember Long id) {
+        MemberResponse memberResponse = memberService.findById(id);
+
+        return ResponseEntity.ok(memberResponse);
     }
 }
--- backend/src/test/java/com/woowacourse/kkogkkog/documentation/MemberControllerTest.java
+++ backend/src/test/java/com/woowacourse/kkogkkog/documentation/MemberControllerTest.java
@@ -4,13 +4,19 @@ import static com.woowacourse.kkogkkog.documentation.support.ApiDocumentUtils.ge
 import static com.woowacourse.kkogkkog.documentation.support.ApiDocumentUtils.getDocumentResponse;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.woowacourse.kkogkkog.application.dto.MemberResponse;
 import com.woowacourse.kkogkkog.presentation.dto.MemberCreateRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
@@ -43,6 +49,41 @@ public class MemberControllerTest extends Documentation {
                 requestFields(
                     fieldWithPath("email").type(JsonFieldType.STRING).description("이메일"),
                     fieldWithPath("password").type(JsonFieldType.STRING).description("비밀번호"),
+                    fieldWithPath("nickname").type(JsonFieldType.STRING).description("닉네임")
+                ))
+            );
+    }
+
+    @Test
+    void 나의_회원정보를_요청할_수_있다() throws Exception {
+        // given
+        MemberResponse memberResponse = new MemberResponse(1L, "user1@gmail.com", "user1");
+
+        given(jwtTokenProvider.getValidatedPayload(any())).willReturn("1");
+        given(memberService.findById(any())).willReturn(memberResponse);
+
+        // when
+        ResultActions perform = mockMvc.perform(get("/api/members/me")
+            .header("Authorization", "Bearer AccessToken"));
+
+        // then
+        perform.andExpect(status().isOk())
+            .andExpect(jsonPath("$.id").value("1"))
+            .andExpect(jsonPath("$.email").value("user1@gmail.com"))
+            .andExpect(jsonPath("$.nickname").value("user1"));
+
+        // docs
+        perform
+            .andDo(print())
+            .andDo(document("member-showMe",
+                getDocumentRequest(),
+                getDocumentResponse(),
+                requestHeaders(
+                    headerWithName("Authorization").description("Bearer {accessToken}")
+                ),
+                responseFields(
+                    fieldWithPath("id").type(JsonFieldType.NUMBER).description("ID"),
+                    fieldWithPath("email").type(JsonFieldType.STRING).description("이메일"),
                     fieldWithPath("nickname").type(JsonFieldType.STRING).description("닉네임")
                 ))
             );
