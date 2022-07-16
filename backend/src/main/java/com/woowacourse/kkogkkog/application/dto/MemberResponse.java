6a0042814f9da832e4173ba29b85f1a4453f0720
Jihoon Choi <48710213+Wishoon@users.noreply.github.com>
2022-07-16 15:39:39 +0900

    feat: 회원가입 된 회원들의 전체 조회 API 구현 (#57)
    * feat: MemberService의 회원 전체 조회 기능 구현
    
    Co-authored-by: Hyunta <mohai2618@gmail.com>
    
    * feat: MemberController의 회원 전체 조회 기능 구현
    
    - MemberResponse 필드 순서 변경
    - 회원 전체 조회 문서 테스트 작성
    
    Co-authored-by: Hyunta <mohai2618@gmail.com>
    
    * test: 회원 전체 조회 인수테스트 작성
    
    Co-authored-by: Hyunta <mohai2618@gmail.com>
    
    * docs: 회원 전체 조회 API에 대한 adoc 작성
    
    Co-authored-by: Hyunta <mohai2618@gmail.com>
    
    * refactor: MemberResponse 생성을 static 메서드로 변경
    
    * test: 전체 사용자 조회 인수테스트의 검증 값 추가
    
    Co-authored-by: Hyunta <mohai2618@gmail.com>

---
+++ backend/src/main/java/com/woowacourse/kkogkkog/application/dto/MemberResponse.java
@@ -0,0 +1,25 @@
+package com.woowacourse.kkogkkog.application.dto;
+
+import com.woowacourse.kkogkkog.domain.Member;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class MemberResponse {
+
+    private Long id;
+    private String email;
+    private String nickname;
+
+    public MemberResponse(Long id, String email, String nickname) {
+        this.id = id;
+        this.email = email;
+        this.nickname = nickname;
+    }
+
+    public static MemberResponse of(Member member) {
+        return new MemberResponse(member.getId(), member.getEmail(), member.getNickname());
+    }
+}
--- backend/src/test/java/com/woowacourse/kkogkkog/acceptance/MemberAcceptanceTest.java
+++ backend/src/test/java/com/woowacourse/kkogkkog/acceptance/MemberAcceptanceTest.java
@@ -1,11 +1,16 @@
 package com.woowacourse.kkogkkog.acceptance;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
+import com.woowacourse.kkogkkog.application.dto.MemberResponse;
+import com.woowacourse.kkogkkog.application.dto.MembersResponse;
+import com.woowacourse.kkogkkog.domain.Member;
 import com.woowacourse.kkogkkog.presentation.dto.MemberCreateRequest;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -52,5 +57,33 @@ public class MemberAcceptanceTest extends AcceptanceTest {
             .extract();
 
         assertThat(extract.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    void 회원_가입된_전체_사용자_조회를_할_수_있다() {
+        MemberCreateRequest memberCreateRequest1 = new MemberCreateRequest("rookie@gmail.com",
+            "password1234!", "rookie");
+        MemberCreateRequest memberCreateRequest2 = new MemberCreateRequest("arthur@gmail.com",
+            "password1234!", "arthur");
+        회원_가입에_성공한다(memberCreateRequest1);
+        회원_가입에_성공한다(memberCreateRequest2);
+
+        ExtractableResponse<Response> extract = RestAssured.given().log().all()
+            .when()
+            .get("/api/members")
+            .then().log().all()
+            .extract();
+
+        MembersResponse membersResponse = extract.as(MembersResponse.class);
+
+        assertAll(
+            () -> assertThat(extract.statusCode()).isEqualTo(HttpStatus.OK.value()),
+            () -> assertThat(membersResponse.getData()).hasSize(2),
+            () -> assertThat(membersResponse.getData()).usingRecursiveComparison().isEqualTo(
+                List.of(
+                    MemberResponse.of(new Member(1L, "rookie@gmail.com", null, "rookie")),
+                    MemberResponse.of(new Member(2L, "arthur@gmail.com", null, "arthur"))
+                )
+        ));
     }
 }
