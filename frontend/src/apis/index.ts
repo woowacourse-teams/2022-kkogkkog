9656bbfe0c136fb51571d111d8bdef0d6654711b
junzzi <jjh01027717502@gmail.com>
2022-07-16 18:22:36 +0900

    feat: 인증 Remote fetcher 수정

--- frontend/src/apis/index.ts
+++ frontend/src/apis/index.ts
@@ -4,4 +4,7 @@ export const BASE_URL = 'http://localhost:8080/api';
 
 export const client = axios.create({
   baseURL: BASE_URL,
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem('user-token')}`,
+  },
 });
--- frontend/src/apis/kkogkkog.ts
+++ frontend/src/apis/kkogkkog.ts
@@ -17,6 +17,3 @@ export const getKkogkkog = () =>
 
 export const createKkogkkog = (info: Omit<KkogKkog, 'id' | 'thumbnail'>) =>
   client.post('/coupons', info);
-
-export const join = (args: { nickname: string; email: string; password: string }) =>
-  client.post('/members', args);
