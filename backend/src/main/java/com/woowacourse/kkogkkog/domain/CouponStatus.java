f5111b3f36b5a2b9e554b61b54fadacf745224ed
Jeong Jinwoo <jwjeong96@gmail.com>
2022-07-19 15:21:52 +0900

    feat: CouponStatus enum 구현
    - 사용 요청 이벤트에 대한 쿠폰 상태 변화 처리 메서드 구현

--- backend/src/main/java/com/woowacourse/kkogkkog/domain/CouponStatus.java
+++ backend/src/main/java/com/woowacourse/kkogkkog/domain/CouponStatus.java
@@ -1,6 +1,24 @@
 package com.woowacourse.kkogkkog.domain;
 
+import com.woowacourse.kkogkkog.exception.InvalidRequestException;
+
 public enum CouponStatus {
 
-    READY
+    READY,
+    REQUESTED,
+    ;
+
+    public CouponStatus handle(CouponEvent event) {
+        if (event == CouponEvent.REQUEST) {
+            return handleRequest();
+        }
+        throw new InvalidRequestException("처리할 수 없는 요청입니다.");
+    }
+
+    private CouponStatus handleRequest() {
+        if (this != READY) {
+            throw new InvalidRequestException("사용 요청을 보낼 수 없는 상태의 쿠폰입니다.");
+        }
+        return REQUESTED;
+    }
 }
