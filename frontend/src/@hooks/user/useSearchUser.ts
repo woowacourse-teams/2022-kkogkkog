faaad78a55f31b20b3049627f4afce00a1cf80c0
junzzi <jjh01027717502@gmail.com>
2022-07-19 11:09:32 +0900

    feat: SearchUser 기능 useSearchUser로 훅 분리

---
+++ frontend/src/@hooks/user/useSearchUser.ts
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+import useMe from '@/@hooks/user/useMe';
+import useUserList from '@/@hooks/user/useUserList';
+import { UserListResponse } from '@/types/remote/response';
+
+export const useSearchUser = () => {
+  const { me } = useMe();
+
+  // 이 부분은 검색 API 도입시 사라지게됨
+  const { userList } = useUserList();
+
+  const [searchedUserList, setSearchedUserList] = useState<UserListResponse | null>(null);
+
+  const searchUser = (keyword: string) => {
+    const users = userList || [];
+
+    // 이 부분은 검색 API 도입시 비동기 요청하는 코드로 변경됨
+    const findUserList = users.filter(({ id, nickname }) => nickname === keyword && me?.id !== id);
+
+    setSearchedUserList(findUserList);
+  };
+
+  return {
+    searchedUserList,
+    searchUser,
+  };
+};
