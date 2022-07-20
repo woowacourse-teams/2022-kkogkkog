e070bec1d4145ba5f556061e03307f33c486cf41
DWL21 <nggus5@naver.com>
2022-07-20 12:28:47 +0900

    feat: 회원가입 기능 API validation 구현

--- backend/src/main/java/com/woowacourse/kkogkkog/presentation/dto/MemberCreateRequest.java
+++ backend/src/main/java/com/woowacourse/kkogkkog/presentation/dto/MemberCreateRequest.java
@@ -1,6 +1,7 @@
 package com.woowacourse.kkogkkog.presentation.dto;
 
 import com.woowacourse.kkogkkog.domain.Member;
+import javax.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,8 +10,13 @@ import lombok.NoArgsConstructor;
 @Getter
 public class MemberCreateRequest {
 
+    @NotBlank(message = "이메일을 입력해주세요")
     private String email;
+
+    @NotBlank(message = "비밀번호를 입력해주세요")
     private String password;
+
+    @NotBlank(message = "닉네임을 입력해주세요")
     private String nickname;
 
     public MemberCreateRequest(String email, String password, String nickname) {
