83bfda99d30115d3acfd3e0efac2a2d12db59e41
gytks4 <gytks4@naver.com>
2022-07-20 14:29:17 +0900

    feat: PrivateRoute로 인가처리

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
 
 import Loading from '@/@components/@shared/Loading';
 import JoinPage from '@/@pages/join';
@@ -8,6 +8,7 @@ import KkogkkogCreatePage from '@/@pages/kkogkkog-list/create';
 import LandingPage from '@/@pages/landing';
 import ProfilePage from '@/@pages/profile';
 
+import useMe from './@hooks/user/useMe';
 import LoginPage from './@pages/login';
 
 export const PATH = {
@@ -30,27 +31,35 @@ const Router = () => {
           </Suspense>
         }
       />
-      <Route
-        path={PATH.KKOGKKOG_LIST}
-        element={
-          <Suspense fallback={<KkogkkogListPage.Skeleton />}>
-            <KkogkkogListPage />
-          </Suspense>
-        }
-      />
-      <Route
-        path={PATH.KKOGKKOG_CREATE}
-        element={
-          <Suspense fallback={<Loading>👻</Loading>}>
-            <KkogkkogCreatePage />
-          </Suspense>
-        }
-      />
       <Route path={PATH.LOGIN} element={<LoginPage />} />
       <Route path={PATH.JOIN} element={<JoinPage />} />
-      <Route path={PATH.PROFILE} element={<ProfilePage />} />
+      <Route element={<PrivateRoute />}>
+        <Route
+          path={PATH.KKOGKKOG_LIST}
+          element={
+            <Suspense fallback={<KkogkkogListPage.Skeleton />}>
+              <KkogkkogListPage />
+            </Suspense>
+          }
+        />
+        <Route
+          path={PATH.KKOGKKOG_CREATE}
+          element={
+            <Suspense fallback={<Loading>👻</Loading>}>
+              <KkogkkogCreatePage />
+            </Suspense>
+          }
+        />
+        <Route path={PATH.PROFILE} element={<ProfilePage />} />
+      </Route>
     </Routes>
   );
 };
 
 export default Router;
+
+const PrivateRoute = () => {
+  const { me } = useMe();
+
+  return me ? <Outlet /> : <Navigate to='/' replace />;
+};
