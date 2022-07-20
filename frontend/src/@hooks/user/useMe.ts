518be69f1f724db09fc9c28dd001ee570ca62bdf
gytks4 <gytks4@naver.com>
2022-07-20 14:16:20 +0900

    refactor: useMe 훅에서 모든 Query 요소를 반환하도록 수정

--- frontend/src/@hooks/user/useMe.ts
+++ frontend/src/@hooks/user/useMe.ts
@@ -4,15 +4,13 @@ import { getMe } from '@/apis/user';
 import { MeResponse } from '@/types/remote/response';
 
 const useMe = () => {
-  const { data, isLoading, isError, remove } = useQuery<{ data: MeResponse }>(['me'], getMe, {
+  const meQuery = useQuery<{ data: MeResponse }>(['me'], getMe, {
     suspense: false,
   });
 
   return {
-    me: data?.data,
-    isLoading,
-    isError,
-    remove,
+    ...meQuery,
+    me: meQuery.data?.data,
   };
 };
 
