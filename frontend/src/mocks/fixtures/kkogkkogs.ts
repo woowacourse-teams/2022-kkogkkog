151666a9105a32435d1cab0e7815ffb38dcd0d32
junzzi <jjh01027717502@gmail.com>
2022-07-20 15:22:28 +0900

    feat: kkogkkog fixture 데이터 추가

--- frontend/src/mocks/fixtures/kkogkkogs.ts
+++ frontend/src/mocks/fixtures/kkogkkogs.ts
@@ -10,6 +10,7 @@ export default {
       modifier: '한턱쏘는',
       message: 'hi',
       couponType: 'COFFEE',
+      couponStatus: 'READY',
     },
     {
       id: 1,
@@ -19,6 +20,17 @@ export default {
       modifier: '한턱쏘는',
       message: 'hi',
       couponType: 'COFFEE',
+      couponStatus: 'READY',
+    },
+    {
+      id: 2,
+      sender: { id: 0, email: 'wnsgur8397@naver.com', password: '1234', nickname: '준찌' },
+      receiver: { id: 1, email: 'lll@naver.com', password: '1234', nickname: '시지프' },
+      backgroundColor: '#FFFFFF',
+      modifier: '한턱쏘는',
+      message: 'hi',
+      couponType: 'COFFEE',
+      couponStatus: 'REQUESTED',
     },
   ],
 
