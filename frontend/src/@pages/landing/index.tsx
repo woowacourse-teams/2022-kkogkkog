8588ce816d12db8f6fd08010c7427af2bd7059e2
gytks4 <gytks4@naver.com>
2022-07-20 13:50:38 +0900

    refactor: Landing 인증 여부에 따라 분리하여 렌더링

--- frontend/src/@pages/landing/index.tsx
+++ frontend/src/@pages/landing/index.tsx
@@ -11,32 +11,64 @@ import { useKkogKkogList } from '@/@hooks/kkogkkog/useKkogKkogList';
 import useMe from '@/@hooks/user/useMe';
 import { PATH } from '@/Router';
 
-type STATUS_TYPE = 'received' | 'sent';
-
 const LandingPage = () => {
   const { me } = useMe();
 
+  return me ? <AuthorizedLanding /> : <UnAuthorizedLanding />;
+};
+
+export default LandingPage;
+
+LandingPage.Skeleton = function Skeleton() {
+  return (
+    <PageTemplate title='꼭꼭'>
+      <Styled.Root>
+        <Styled.LinkContainer>
+          <Link to={PATH.KKOGKKOG_CREATE}>
+            <KkogKkogItem.LinkButton />
+          </Link>
+        </Styled.LinkContainer>
+        <Styled.ListContainer>
+          <Styled.ListHeaderContainer>
+            <Styled.ListHeaderItem>받은 쿠폰</Styled.ListHeaderItem>
+            <Styled.ListHeaderItem>보낸 쿠폰</Styled.ListHeaderItem>
+          </Styled.ListHeaderContainer>
+          <KkogKkogItem.Skeleton />
+          <KkogKkogItem.Skeleton />
+          <KkogKkogItem.Skeleton />
+          <KkogKkogItem.Skeleton />
+          <KkogKkogItem.Skeleton />
+          <KkogKkogItem.Skeleton />
+        </Styled.ListContainer>
+      </Styled.Root>
+    </PageTemplate>
+  );
+};
+
+const UnAuthorizedLanding = () => {
+  return (
+    <PageTemplate title='꼭꼭'>
+      <Styled.UnAuthorizedRoot>
+        <Button
+          css={css`
+            padding: 15px;
+          `}
+        >
+          꼭꼭 시작하기
+        </Button>
+      </Styled.UnAuthorizedRoot>
+    </PageTemplate>
+  );
+};
+
+type STATUS_TYPE = 'received' | 'sent';
+
+const AuthorizedLanding = () => {
   const { state } = useLocation() as { state: { status: STATUS_TYPE } };
 
   const { kkogkkogList } = useKkogKkogList();
 
   const { status, changeStatus } = useStatus<STATUS_TYPE>(state?.status ?? 'received');
-
-  if (!me) {
-    return (
-      <PageTemplate title='꼭꼭'>
-        <Styled.UnAuthorizedRoot>
-          <Button
-            css={css`
-              padding: 15px;
-            `}
-          >
-            꼭꼭 시작하기
-          </Button>
-        </Styled.UnAuthorizedRoot>
-      </PageTemplate>
-    );
-  }
 
   return (
     <PageTemplate title='꼭꼭'>
@@ -68,34 +100,6 @@ const LandingPage = () => {
     </PageTemplate>
   );
 };
-
-LandingPage.Skeleton = function Skeleton() {
-  return (
-    <PageTemplate title='꼭꼭'>
-      <Styled.Root>
-        <Styled.LinkContainer>
-          <Link to={PATH.KKOGKKOG_CREATE}>
-            <KkogKkogItem.LinkButton />
-          </Link>
-        </Styled.LinkContainer>
-        <Styled.ListContainer>
-          <Styled.ListHeaderContainer>
-            <Styled.ListHeaderItem>받은 쿠폰</Styled.ListHeaderItem>
-            <Styled.ListHeaderItem>보낸 쿠폰</Styled.ListHeaderItem>
-          </Styled.ListHeaderContainer>
-          <KkogKkogItem.Skeleton />
-          <KkogKkogItem.Skeleton />
-          <KkogKkogItem.Skeleton />
-          <KkogKkogItem.Skeleton />
-          <KkogKkogItem.Skeleton />
-          <KkogKkogItem.Skeleton />
-        </Styled.ListContainer>
-      </Styled.Root>
-    </PageTemplate>
-  );
-};
-
-export default LandingPage;
 
 export const Styled = {
   UnAuthorizedRoot: styled.div`
