ca0be5688400d686aa5b493369e79de2b782d424
gytks4 <gytks4@naver.com>
2022-07-21 22:38:21 +0900

    feat: 로그아웃 스타일 수정

--- frontend/src/@pages/profile/index.tsx
+++ frontend/src/@pages/profile/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 import { useNavigate } from 'react-router-dom';
 
+import Button from '@/@components/@shared/Button';
 import PageTemplate from '@/@components/@shared/PageTemplate';
 import useMe from '@/@hooks/user/useMe';
 import { client } from '@/apis';
@@ -23,9 +25,25 @@ const ProfilePage = () => {
 
   return (
     <PageTemplate title='회원정보수정'>
-      <button onClick={onClickLogoutButton}>logout</button>
+      <Styled.Root>
+        <Button
+          onClick={onClickLogoutButton}
+          css={css`
+            margin-top: 20px;
+            width: 50%;
+          `}
+        >
+          로그아웃
+        </Button>
+      </Styled.Root>
     </PageTemplate>
   );
 };
 
 export default ProfilePage;
+
+const Styled = {
+  Root: styled.div`
+    text-align: center;
+  `,
+};
