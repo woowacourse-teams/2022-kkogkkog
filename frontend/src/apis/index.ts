e25e3f2b90f500777e90145e8e47b5185fdc8201
gytks4 <gytks4@naver.com>
2022-07-21 20:45:28 +0900

    chore: baseUrl .env로 설정

--- frontend/src/apis/index.ts
+++ frontend/src/apis/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASE_URL = 'http://localhost:8080/api';
+export const BASE_URL = process.env.PROD_SERVER;
 
 export const client = axios.create({
   baseURL: BASE_URL,
