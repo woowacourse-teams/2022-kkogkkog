c6eb12c61e852d16f1f7faaeb6d769d237edc07b
gytks4 <gytks4@naver.com>
2022-07-22 12:08:54 +0900

    fix: 쿠폰 모달에서 sender/receiver 잘못 뜨는 에러 해결

--- frontend/src/@components/kkogkkog/KkogKkogItem/index.tsx
+++ frontend/src/@components/kkogkkog/KkogKkogItem/index.tsx
@@ -12,7 +12,7 @@ type KkogKkogItemProps = KkogKKogResponse & {
   onClickCoupon: (kkogkkog: KkogKKogResponse) => void;
 };
 
-type KkogKkogItemPreviewProps = Omit<KkogKKogResponse, 'id' | 'sender' | 'couponStatus'> & {
+type KkogKkogItemPreviewProps = Omit<KkogKKogResponse, 'id' | 'couponStatus'> & {
   className?: string;
 };
 
@@ -57,15 +57,27 @@ const KkogKkogItem = (props: KkogKkogItemProps) => {
 
 /* UI에서 보이지 않는 id, couponStatus를 제외한 props만 받는 프로토타입 컴포넌트 */
 KkogKkogItem.Preview = function Preview(props: KkogKkogItemPreviewProps) {
-  const { className, receiver, backgroundColor, modifier, couponType, thumbnail } = {
+  const { className, sender, receiver, backgroundColor, modifier, couponType, thumbnail } = {
     ...props,
     thumbnail: THUMBNAIL[props.couponType],
   };
 
+  const { me } = useMe();
+
   return (
     <Styled.Root className={className} hasCursor={false}>
       <Styled.TextContainer>
-        <div>To. {receiver.nickname}</div>
+        {sender ? (
+          // sender가 있고, 자신이 보낸 사람일 때는 받은 사람을 보여준다.
+          sender.id === me?.id ? (
+            <div>To. {receiver?.nickname}</div>
+          ) : (
+            <div>From. {sender?.nickname}</div>
+          )
+        ) : (
+          // 쿠폰 생성 시에는 sender가 없다. 받은 사람을 보여준다.
+          <div>To. {receiver?.nickname}</div>
+        )}
         <div>
           #{modifier} &nbsp;
           <Styled.TypeText>{KKOGKKOG_TYPE_MAPPER[couponType]}</Styled.TypeText>
