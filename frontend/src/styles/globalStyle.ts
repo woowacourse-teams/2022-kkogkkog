9067be2f213c88575c4643bf1a9895e1ecf5ede6
junzzi <jjh01027717502@gmail.com>
2022-07-27 16:20:35 +0900

    refactor: ExtraBold, SemiBold 추가, theme 색상 추가

--- frontend/src/styles/globalStyle.ts
+++ frontend/src/styles/globalStyle.ts
@@ -48,8 +48,22 @@ const globalStyle = css`
   @font-face {
     font-family: 'Pretendard';
     font-style: normal;
+    font-weight: 600;
+    src: url('/assets/font/Pretendard-SemiBold.woff') format('woff');
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-style: normal;
     font-weight: bold;
     src: url('/assets/font/Pretendard-Bold.woff') format('woff');
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 900;
+    src: url('/assets/font/Pretendard-ExtraBold.woff') format('woff');
   }
 
   @keyframes show-up {
