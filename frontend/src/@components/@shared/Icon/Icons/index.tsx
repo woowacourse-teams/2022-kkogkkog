36f534e88937a792ca83348c759b6ff4b55d2ea1
junzzi <jjh01027717502@gmail.com>
2022-07-27 16:54:44 +0900

    feat: airplane, plus icon 추가

--- frontend/src/@components/@shared/Icon/Icons/index.tsx
+++ frontend/src/@components/@shared/Icon/Icons/index.tsx
@@ -1,9 +1,13 @@
+import Airplane from '@/@components/@shared/Icon/Icons/Airplane';
 import Close from '@/@components/@shared/Icon/Icons/Close';
+import Plus from '@/@components/@shared/Icon/Icons/Plus';
 import Profile from '@/@components/@shared/Icon/Icons/Profile';
 
 const iconName = {
   profile: Profile,
   close: Close,
+  airplane: Airplane,
+  plus: Plus,
 };
 
 export type IconNames = keyof typeof iconName;
