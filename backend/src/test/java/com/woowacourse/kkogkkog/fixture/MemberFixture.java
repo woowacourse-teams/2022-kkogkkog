9623565103b8e6a443c17b5d4b9ca108960ff1c6
Hyunta <mohai2618@naver.com>
2022-07-27 10:48:03 +0900

    test: DCI 패턴을 적용한 CouponTest 재구현
    - MemberFixture 의 메서드를 sourceMethod 로 공유

--- backend/src/test/java/com/woowacourse/kkogkkog/fixture/MemberFixture.java
+++ backend/src/test/java/com/woowacourse/kkogkkog/fixture/MemberFixture.java
@@ -1,6 +1,8 @@
 package com.woowacourse.kkogkkog.fixture;
 
 import com.woowacourse.kkogkkog.domain.Member;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.provider.Arguments;
 
 public class MemberFixture {
 
@@ -10,4 +12,11 @@ public class MemberFixture {
     public static Member LEO = new Member(null, "leo@gmail.com", "password1234!", "레오");
     public static Member NON_EXISTING_MEMBER = new Member(99999L, "no-one@gmail.com",
         "password1234!", "존재하지_않는_사용자");
+
+    public static Stream<Arguments> provideSenderAndReceiver() {
+        return Stream.of(
+            Arguments.of(MemberFixture.ROOKIE, MemberFixture.ARTHUR, MemberFixture.ROOKIE),
+            Arguments.of(MemberFixture.ROOKIE, MemberFixture.ARTHUR, MemberFixture.ARTHUR)
+        );
+    }
 }
