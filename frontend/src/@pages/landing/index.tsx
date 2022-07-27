4b21165cfb54d96b9f02741e2403178a174b4cbb
junzzi <jjh01027717502@gmail.com>
2022-07-27 16:44:55 +0900

    feat: landing page 개발, Style 파일로 분리

--- frontend/src/@pages/landing/index.tsx
+++ frontend/src/@pages/landing/index.tsx
@@ -1,33 +1,60 @@
 import { css } from '@emotion/react';
-import styled from '@emotion/styled';
 import { Link } from 'react-router-dom';
 
 import Button from '@/@components/@shared/Button';
 import CustomSuspense from '@/@components/@shared/CustomSuspense';
+import Icon from '@/@components/@shared/Icon';
 import PageTemplate from '@/@components/@shared/PageTemplate';
-import KkogKkogItem from '@/@components/kkogkkog/KkogKkogItem';
-import KkogKkogListContainer from '@/@components/kkogkkog/KkogKkogListContainer';
-import useRouterState from '@/@hooks/@common/useRouterState';
-import { useStatus } from '@/@hooks/@common/useStatus';
+import Position from '@/@components/@shared/Position';
+import KkogKkogList from '@/@components/kkogkkog/KkogKkogList';
 import { useKkogKkogList } from '@/@hooks/kkogkkog/useKkogKkogList';
 import useMe from '@/@hooks/user/useMe';
 import { PATH } from '@/Router';
 
-type STATUS_TYPE = 'received' | 'sent';
+import * as Styled from './style';
 
 const UnAuthorizedLanding = () => {
   return (
-    <PageTemplate title='꼭꼭'>
+    <PageTemplate title='꼭꼭' hasHeader={false}>
       <Styled.UnAuthorizedRoot>
-        <Link to='/login'>
-          <Button
-            css={css`
-              padding: 15px;
-            `}
-          >
-            꼭꼭 시작하기
-          </Button>
-        </Link>
+        <Styled.UnAuthorizedContainer>
+          <img src='/assets/images/landing_logo.png' alt='로고' width='86' />
+
+          <div>
+            <Styled.ExtraBold>꼭꼭</Styled.ExtraBold>으로
+            <br />
+            당신의 마음을 전달해보세요!
+          </div>
+
+          <Styled.AdditionalExplanation>
+            시간을 보내고 싶어하는 사람들이 있을지 모릅니다.
+          </Styled.AdditionalExplanation>
+        </Styled.UnAuthorizedContainer>
+        <Position
+          position='absolute'
+          bottom='50px'
+          css={css`
+            width: 100%;
+            padding: 30px;
+          `}
+        >
+          <Link to='/login'>
+            <Button
+              css={css`
+                display: flex;
+                padding: 15px;
+                justify-content: center;
+                align-items: center;
+
+                font-size: 16px;
+
+                gap: 15px;
+              `}
+            >
+              꼭꼭 시작하기 <Icon iconName='airplane' />
+            </Button>
+          </Link>
+        </Position>
       </Styled.UnAuthorizedRoot>
     </PageTemplate>
   );
@@ -38,47 +65,64 @@ const UnAuthorizedLanding = () => {
 const AuthorizedLanding = () => {
   const { kkogkkogList, isLoading } = useKkogKkogList();
 
-  const routerState = useRouterState<'create'>('action');
-
-  const { status, changeStatus } = useStatus<STATUS_TYPE>(
-    routerState === 'create' ? 'sent' : 'received'
-  );
-
-  const onClickReceivedCouponButton = () => {
-    changeStatus('received');
-  };
-
-  const onClickSentCouponButton = () => {
-    changeStatus('sent');
-  };
-
   return (
-    <PageTemplate title='꼭꼭'>
+    <PageTemplate.LandingPage title='꼭꼭'>
       <Styled.Root>
-        <Styled.LinkContainer>
-          <Link to={PATH.KKOGKKOG_CREATE}>
-            <KkogKkogItem.LinkButton />
-          </Link>
-        </Styled.LinkContainer>
-        <Styled.ListContainer>
-          <Styled.ListHeaderContainer>
-            <Styled.ListHeaderItem
-              isSelected={status === 'received'}
-              onClick={onClickReceivedCouponButton}
-            >
-              받은 쿠폰
-            </Styled.ListHeaderItem>
-            <Styled.ListHeaderItem isSelected={status === 'sent'} onClick={onClickSentCouponButton}>
-              보낸 쿠폰
-            </Styled.ListHeaderItem>
-          </Styled.ListHeaderContainer>
+        <Styled.CreateCouponContainer>
+          <div>
+            <Styled.ExtraBold>꼭꼭</Styled.ExtraBold>으로
+            <br />
+            당신의 마음을 전달해보세요!
+          </div>
 
-          <CustomSuspense fallback={<KkogKkogListContainer.Skeleton />} isLoading={isLoading}>
-            <KkogKkogListContainer kkogkkogList={kkogkkogList && kkogkkogList[status]} />
-          </CustomSuspense>
-        </Styled.ListContainer>
+          <Styled.AdditionalExplanation>
+            시간을 보내고 싶어하는 사람들이 있을지 모릅니다.
+          </Styled.AdditionalExplanation>
+          <Link to={PATH.KKOGKKOG_CREATE}>
+            <Position>
+              <Button css={Styled.ExtendedButton}>
+                꼭꼭 보내러가기
+                <Position
+                  position='absolute'
+                  css={css`
+                    right: 5px;
+                    bottom: 50%;
+
+                    transform: translateY(50%);
+                  `}
+                >
+                  <Icon iconName='plus' size='32' />
+                </Position>
+              </Button>
+            </Position>
+          </Link>
+        </Styled.CreateCouponContainer>
+
+        <CustomSuspense fallback={<KkogKkogList.Skeleton />} isLoading={isLoading}>
+          <Styled.ListContainer>
+            <div>
+              <Styled.ListTitle>
+                <span>받은 쿠폰</span>
+                <Link to={PATH.KKOGKKOG_LIST} css={Styled.ExtendedLink}>
+                  더보기
+                </Link>
+              </Styled.ListTitle>
+              <KkogKkogList.Slide kkogkkogList={kkogkkogList && kkogkkogList.received} />
+            </div>
+
+            <div>
+              <Styled.ListTitle>
+                <span>보낸 쿠폰</span>
+                <Link to={PATH.KKOGKKOG_LIST} css={Styled.ExtendedLink}>
+                  더보기
+                </Link>
+              </Styled.ListTitle>
+              <KkogKkogList.Slide kkogkkogList={kkogkkogList && kkogkkogList.sent} />
+            </div>
+          </Styled.ListContainer>
+        </CustomSuspense>
       </Styled.Root>
-    </PageTemplate>
+    </PageTemplate.LandingPage>
   );
 };
 
@@ -89,43 +133,3 @@ const LandingPage = () => {
 };
 
 export default LandingPage;
-
-export const Styled = {
-  UnAuthorizedRoot: styled.div`
-    min-height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    padding: 20px;
-  `,
-  Root: styled.div`
-    border-radius: 4px;
-  `,
-  LinkContainer: styled.div`
-    padding: 20px;
-  `,
-  ListContainer: styled.div``,
-  ListHeaderContainer: styled.div`
-    display: flex;
-
-    padding: 0 20px;
-
-    & > div {
-      margin-right: 30px;
-    }
-  `,
-  ListHeaderItem: styled.div<{ isSelected?: boolean }>`
-    padding-bottom: 8px;
-
-    font-size: 14px;
-
-    cursor: pointer;
-
-    ${({ theme, isSelected }) => css`
-      color: ${theme.colors.primary_400};
-
-      border-bottom: ${isSelected ? `1.5px solid ${theme.colors.primary_400}` : 'none'};
-    `}
-  `,
-};
