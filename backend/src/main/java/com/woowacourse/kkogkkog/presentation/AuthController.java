cd77dfc5c444866bd20a1ecffa2731b3675e5796
DWL21 <nggus5@naver.com>
2022-07-28 11:37:19 +0900

    feat: 소셜 로그인 API 구현

--- backend/src/main/java/com/woowacourse/kkogkkog/presentation/AuthController.java
+++ backend/src/main/java/com/woowacourse/kkogkkog/presentation/AuthController.java
@@ -1,13 +1,16 @@
 package com.woowacourse.kkogkkog.presentation;
 
 import com.woowacourse.kkogkkog.application.AuthService;
+import com.woowacourse.kkogkkog.application.dto.TokenOAuthResponse;
 import com.woowacourse.kkogkkog.application.dto.TokenResponse;
 import com.woowacourse.kkogkkog.presentation.dto.TokenRequest;
 import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,6 +26,13 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<TokenResponse> login(@Valid @RequestBody TokenRequest tokenRequest) {
         TokenResponse tokenResponse = authService.login(tokenRequest);
+
+        return ResponseEntity.ok(tokenResponse);
+    }
+
+    @GetMapping("/slack/login")
+    public ResponseEntity<TokenOAuthResponse> loginByOAuth(@RequestParam String code) {
+        TokenOAuthResponse tokenResponse = authService.loginByOAuth(code);
 
         return ResponseEntity.ok(tokenResponse);
     }
