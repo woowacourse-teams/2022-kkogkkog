caa0085d33b196cb33d917f45b250f9082203475
gytks4 <gytks4@naver.com>
2022-07-28 15:44:33 +0900

    refactor: mutation Options 타입 구체적으로 정의

--- frontend/src/@hooks/kkogkkog/useKkogKkogStatusMutation.ts
+++ frontend/src/@hooks/kkogkkog/useKkogKkogStatusMutation.ts
@@ -1,11 +1,22 @@
+import { AxiosResponse } from 'axios';
 import { useMutation, UseMutationOptions, useQueryClient } from 'react-query';
 
 import { changeKkogkkogStatus } from '@/apis/kkogkkog';
+import { ChangeKkogKkogStatusRequest } from '@/types/remote/request';
 
 type kkogkkogActionType = {
   id: number;
   meetingDate?: string;
 };
+
+type changeStatusMutationOptions = Omit<
+  UseMutationOptions<
+    AxiosResponse<any, any>,
+    unknown,
+    { id: number; body: ChangeKkogKkogStatusRequest }
+  >,
+  'mutationFn'
+>;
 
 const useKkogKkogStatusMutation = () => {
   const queryClient = useQueryClient();
@@ -16,31 +27,22 @@ const useKkogKkogStatusMutation = () => {
     },
   });
 
-  const cancelKkogKkog = (
-    { id }: kkogkkogActionType,
-    options: Omit<UseMutationOptions<unknown, unknown, unknown>, 'mutationFn'>
-  ) => {
+  const cancelKkogKkog = ({ id }: kkogkkogActionType, options: changeStatusMutationOptions) => {
     changeStatusMutate.mutate({ id, body: { couponEvent: 'CANCEL' } }, options);
   };
 
   const requestKkogKKog = (
     { id, meetingDate }: kkogkkogActionType,
-    options: Omit<UseMutationOptions<unknown, unknown, unknown>, 'mutationFn'>
+    options: changeStatusMutationOptions
   ) => {
     changeStatusMutate.mutate({ id, body: { couponEvent: 'REQUEST', meetingDate } }, options);
   };
 
-  const finishKkogKkog = (
-    { id }: kkogkkogActionType,
-    options: Omit<UseMutationOptions<unknown, unknown, unknown>, 'mutationFn'>
-  ) => {
+  const finishKkogKkog = ({ id }: kkogkkogActionType, options: changeStatusMutationOptions) => {
     changeStatusMutate.mutate({ id, body: { couponEvent: 'FINISH' } }, options);
   };
 
-  const acceptKkogKkog = (
-    { id }: kkogkkogActionType,
-    options: Omit<UseMutationOptions<unknown, unknown, unknown>, 'mutationFn'>
-  ) => {
+  const acceptKkogKkog = ({ id }: kkogkkogActionType, options: changeStatusMutationOptions) => {
     changeStatusMutate.mutate({ id, body: { couponEvent: 'ACCEPT' } }, options);
   };
 
