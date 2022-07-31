dc3bec78a99b1a3dee5782e35a1100fdeac9e4e9
DWL21 <nggus5@naver.com>
2022-08-01 00:38:56 +0900

    docs: 소셜 로그인 적용으로 인한 로그인 API adoc 변경

--- backend/src/test/java/com/woowacourse/kkogkkog/documentation/AuthControllerTest.java
+++ backend/src/test/java/com/woowacourse/kkogkkog/documentation/AuthControllerTest.java
@@ -5,20 +5,15 @@ import static com.woowacourse.kkogkkog.documentation.support.ApiDocumentUtils.ge
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.woowacourse.kkogkkog.application.dto.TokenResponse;
-import com.woowacourse.kkogkkog.domain.Member;
-import com.woowacourse.kkogkkog.fixture.MemberFixture;
-import com.woowacourse.kkogkkog.presentation.dto.TokenRequest;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -26,22 +21,20 @@ import org.springframework.test.web.servlet.ResultActions;
 class AuthControllerTest extends Documentation {
 
     @Test
-    void 로그인을_요청한다() throws Exception {
+    void 회원가입_또는_로그인을_요청한다() throws Exception {
         // given
-        Member rookie = MemberFixture.ROOKIE;
-        TokenRequest tokenRequest = new TokenRequest(rookie.getEmail(), rookie.getPassword());
-        TokenResponse tokenResponse = new TokenResponse("accessToken");
+        TokenResponse tokenResponse = new TokenResponse("accessToken", true);
         given(authService.login(any())).willReturn(tokenResponse);
 
         // when
-        ResultActions perform = mockMvc.perform(post("/api/login")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(objectMapper.writeValueAsString(tokenRequest)));
+        ResultActions perform = mockMvc.perform(get("/api/login/token")
+            .param("code", "code_here"));
 
         // then
         perform
             .andExpect(status().isOk())
-            .andExpect(jsonPath("$.accessToken").value("accessToken"));
+            .andExpect(jsonPath("$.accessToken").value("accessToken"))
+            .andExpect(jsonPath("$.isCreated").value("true"));
 
         // docs
         perform
@@ -49,12 +42,9 @@ class AuthControllerTest extends Documentation {
             .andDo(document("auth-login",
                 getDocumentRequest(),
                 getDocumentResponse(),
-                requestFields(
-                    fieldWithPath("email").type(JsonFieldType.STRING).description("이메일"),
-                    fieldWithPath("password").type(JsonFieldType.STRING).description("비밀번호")
-                ),
                 responseFields(
-                    fieldWithPath("accessToken").type(JsonFieldType.STRING).description("토큰")
+                    fieldWithPath("accessToken").type(JsonFieldType.STRING).description("토큰"),
+                    fieldWithPath("isCreated").type(JsonFieldType.BOOLEAN).description("회원가입 여부")
                 )
             ));
     }
