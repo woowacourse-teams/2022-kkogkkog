91789a0e0d1448613241ba5fb350acca93eb8664
DWL21 <nggus5@naver.com>
2022-08-01 00:31:10 +0900

    test: AcceptanceTest 내 slackRequester mockBean 등록

--- backend/src/test/java/com/woowacourse/kkogkkog/acceptance/AcceptanceTest.java
+++ backend/src/test/java/com/woowacourse/kkogkkog/acceptance/AcceptanceTest.java
@@ -1,11 +1,13 @@
 package com.woowacourse.kkogkkog.acceptance;
 
+import com.woowacourse.kkogkkog.infrastructure.SlackRequester;
 import com.woowacourse.kkogkkog.support.DatabaseCleaner;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -16,6 +18,9 @@ public class AcceptanceTest {
 
     @Autowired
     private DatabaseCleaner databaseCleaner;
+
+    @MockBean
+    protected static SlackRequester slackRequester;
 
     @BeforeEach
     void setUp() {
