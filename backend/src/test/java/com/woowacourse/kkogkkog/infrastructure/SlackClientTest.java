c70e47df6f5a749d46a9a08cc422fd2c60360391
Jeong Jinwoo <jwjeong96@gmail.com>
2022-08-01 15:41:55 +0900

    test: SlackClientTest 코드 정리

--- backend/src/test/java/com/woowacourse/kkogkkog/infrastructure/SlackClientTest.java
+++ backend/src/test/java/com/woowacourse/kkogkkog/infrastructure/SlackClientTest.java
@@ -15,74 +15,61 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 class SlackClientTest {
 
-    private static final String USER_ID = "U0R7JM";
-
+    private static final String USER_ID = "ABC123";
+    private static final String TEAM_ID = "TEAM12";
     private static final String SLACK_TOKEN_RESPONSE = "{\n" +
         "    \"ok\": true,\n" +
         "    \"access_token\": \"ACCESS_TOKEN\",\n" +
         "    \"token_type\": \"Bearer\",\n" +
         "    \"id_token\": \"ID_TOKEN\"\n" +
         "}";
-
     private static final String SLACK_USER_INFO_RESPONSE = "{\n" +
         "    \"ok\": true,\n" +
         "    \"sub\": \"" + USER_ID + "\",\n" +
-        "    \"https://slack.com/user_id\": \"U0R7JM\",\n" +
-        "    \"https://slack.com/team_id\": \"T0R7GR\",\n" +
+        "    \"https://slack.com/user_id\": \"" + USER_ID + "\",\n" +
+        "    \"https://slack.com/team_id\": \"" + TEAM_ID + "\",\n" +
         "    \"email\": \"kkogkkog@gmail.com\",\n" +
-        "    \"name\": \"krane\",\n" +
+        "    \"name\": \"kkogkkog\",\n" +
         "    \"picture\": \"IMAGE_URL\"\n" +
         "}";
 
     @Test
     @DisplayName("인증 서버로 코드를 보내 엑세스 토큰을 받아와 정보를 조회한다.")
     void getUserInfoByCode() throws IOException {
-            MockWebServer mockWebServer = new MockWebServer();
-            mockWebServer.start();
+        MockWebServer mockWebServer = new MockWebServer();
+        mockWebServer.start();
+        setUpResponse(mockWebServer, SLACK_TOKEN_RESPONSE);
+        setUpResponse(mockWebServer, SLACK_USER_INFO_RESPONSE);
+        SlackClient slackClient = buildMockSlackClient(mockWebServer);
 
-            setUpResponse(mockWebServer, SLACK_TOKEN_RESPONSE);
-            setUpResponse(mockWebServer, SLACK_USER_INFO_RESPONSE);
+        SlackUserInfo slackUserInfo = slackClient.getUserInfoByCode("code");
+        String userId = slackUserInfo.getUserId();
 
-            SlackClient slackClient = new SlackClient(
-                "clientId",
-                "secretId",
-                String.format("http://%s:%s", mockWebServer.getHostName(), mockWebServer.getPort()),
-                String.format("http://%s:%s", mockWebServer.getHostName(), mockWebServer.getPort()),
-                WebClient.create()
-            );
-
-            SlackUserInfo slackUserInfo = slackClient.getUserInfoByCode("code");
-            String userId = slackUserInfo.getUserId();
-
-            assertThat(userId).isEqualTo(USER_ID);
-            mockWebServer.shutdown();
+        assertThat(userId).isEqualTo(USER_ID);
+        mockWebServer.shutdown();
     }
 
     @Test
-    @DisplayName("요청 과정에서 오류가 발생하면 ErrorResponseToGetAccessTokenException을 응답한다.")
-    void getTokenException() {
+    @DisplayName("요청 과정에서 오류가 발생하면 예외가 발생한다.")
+    void getTokenException() throws IOException {
         String getTokenErrorResponse = "{\n" +
             "    \"error\": \"invalid_code\"\n" +
             "}";
+        MockWebServer mockWebServer = new MockWebServer();
+        mockWebServer.start();
+        setUpResponse(mockWebServer, getTokenErrorResponse);
+        setUpResponse(mockWebServer, SLACK_USER_INFO_RESPONSE);
+        SlackClient slackClient = buildMockSlackClient(mockWebServer);
 
-        try (MockWebServer mockWebServer = new MockWebServer()) {
-            mockWebServer.start();
+        assertThatThrownBy(() -> slackClient.getUserInfoByCode("code"))
+            .isInstanceOf(ErrorResponseToGetAccessTokenException.class);
+    }
 
-            setUpResponse(mockWebServer, getTokenErrorResponse);
-            setUpResponse(mockWebServer, SLACK_USER_INFO_RESPONSE);
-
-            SlackClient slackClient = new SlackClient(
-                "clientId",
-                "secretId",
-                String.format("http://%s:%s", mockWebServer.getHostName(), mockWebServer.getPort()),
-                String.format("http://%s:%s", mockWebServer.getHostName(), mockWebServer.getPort()),
-                WebClient.create()
-            );
-
-            assertThatThrownBy(() -> slackClient.getUserInfoByCode("code"))
-                .isInstanceOf(ErrorResponseToGetAccessTokenException.class);
-        } catch (IOException ignored) {
-        }
+    private SlackClient buildMockSlackClient(MockWebServer mockWebServer) {
+        String mockWebClientURI = String.format("http://%s:%s",
+            mockWebServer.getHostName(), mockWebServer.getPort());
+        return new SlackClient("clientId", "secretId",
+            mockWebClientURI, mockWebClientURI, WebClient.create());
     }
 
     private void setUpResponse(MockWebServer mockWebServer, String responseBody) {
