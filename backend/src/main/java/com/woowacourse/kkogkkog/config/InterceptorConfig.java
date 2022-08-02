49b28de1a90bb4336ee467581f23e1f2dfa3c8f9
Wishoon <heonga2@gmail.com>
2022-08-02 13:53:33 +0900

    feat: 회원의 History 목록을 조회하는 Controller 기능 구현

--- backend/src/main/java/com/woowacourse/kkogkkog/config/InterceptorConfig.java
+++ backend/src/main/java/com/woowacourse/kkogkkog/config/InterceptorConfig.java
@@ -19,6 +19,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor(jwtTokenProvider))
             .addPathPatterns("/api/members/me")
+            .addPathPatterns("/api/members/me/history")
             .addPathPatterns("/api/coupons")
             .addPathPatterns("/api/coupons/*/event")
             .addPathPatterns("/api/coupons/*/event/*");
--- backend/src/main/java/com/woowacourse/kkogkkog/presentation/MemberController.java
+++ backend/src/main/java/com/woowacourse/kkogkkog/presentation/MemberController.java
@@ -3,6 +3,7 @@ package com.woowacourse.kkogkkog.presentation;
 import com.woowacourse.kkogkkog.application.MemberService;
 import com.woowacourse.kkogkkog.application.dto.MemberResponse;
 import com.woowacourse.kkogkkog.presentation.dto.SuccessResponse;
+import com.woowacourse.kkogkkog.presentation.dto.MemberHistoriesResponse;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,5 +30,10 @@ public class MemberController {
     @GetMapping
     public ResponseEntity<SuccessResponse<List<MemberResponse>>> showAll() {
         return ResponseEntity.ok(new SuccessResponse<>(memberService.findAll()));
+    }
+
+    @GetMapping("/me/history")
+    public ResponseEntity<MemberHistoriesResponse> showHistory(@LoginMember Long id) {
+        return ResponseEntity.ok(new MemberHistoriesResponse(memberService.findHistoryById(id)));
     }
 }
