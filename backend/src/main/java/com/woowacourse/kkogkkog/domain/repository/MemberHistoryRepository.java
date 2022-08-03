2bf41b85eb18897d54c35dd598025548a1754330
Jeong Jinwoo <jwjeong96@gmail.com>
2022-08-03 18:51:28 +0900

    feat: 회원의 읽지 않은 기록의 개수를 반환하는 JPA 메서드 정의

--- backend/src/main/java/com/woowacourse/kkogkkog/domain/repository/MemberHistoryRepository.java
+++ backend/src/main/java/com/woowacourse/kkogkkog/domain/repository/MemberHistoryRepository.java
@@ -8,4 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface MemberHistoryRepository extends JpaRepository<MemberHistory, Long> {
 
     List<MemberHistory> findAllByHostMember(Member member);
+
+    long countByHostMemberAndIsReadFalse(Member member);
 }
