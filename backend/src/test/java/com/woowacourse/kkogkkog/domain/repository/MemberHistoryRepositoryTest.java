157df2cd9412b5c4996e62cffbc7be48ca3c6752
Jeong Jinwoo <jwjeong96@gmail.com>
2022-08-04 14:05:39 +0900

    feat: 본인 정보 조회 API에 읽지 않은 알림 개수 정보 포함 (#167)
    * feat: 회원의 읽지 않은 기록의 개수를 반환하는 JPA 메서드 정의
    
    * feat: 읽지 않은 기록의 개수가 포함된 회원 프로필 조회 서비스 구현
    
    * feat: 본인 정보 조회 API에 읽지 않은 기록의 개수 정보 추가
    
    * test: 누락된 @DisplayName 추가
    
    * chore: prod 프로파일로 실행시 스키마 검증 과정 제거

---
+++ backend/src/test/java/com/woowacourse/kkogkkog/domain/repository/MemberHistoryRepositoryTest.java
@@ -0,0 +1,56 @@
+package com.woowacourse.kkogkkog.domain.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.woowacourse.kkogkkog.domain.Coupon;
+import com.woowacourse.kkogkkog.domain.CouponEvent;
+import com.woowacourse.kkogkkog.domain.CouponStatus;
+import com.woowacourse.kkogkkog.domain.CouponType;
+import com.woowacourse.kkogkkog.domain.Member;
+import com.woowacourse.kkogkkog.domain.MemberHistory;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class MemberHistoryRepositoryTest {
+
+    @Autowired
+    private CouponRepository coupons;
+
+    @Autowired
+    private MemberRepository members;
+
+    @Autowired
+    private MemberHistoryRepository memberHistories;
+
+    @Test
+    @DisplayName("countByHostMemberAndIsReadFalse 메서드는 기록의 주인이 읽지 안은 기록들의 개수를 반환한다.")
+    void countByHostMemberAndIsReadFalse() {
+        Member sender = members.save(new Member(null, "UJeong", "T03LX3C5540",
+            "정", "jeong@gmail.com", "image"));
+        Member receiver = members.save(new Member(null, "ULeo", "T03LX3C5540",
+            "레오", "leothelion@gmail.com", "image"));
+        Coupon coupon = coupons.save(new Coupon(null, sender, receiver, "한턱쏘는",
+            "추가 메세지", "#241223", CouponType.COFFEE, CouponStatus.READY));
+
+        MemberHistory initHistory = toMemberHistory(receiver, sender, coupon, CouponEvent.INIT);
+        initHistory.updateIsRead();
+        memberHistories.save(initHistory);
+        memberHistories.save(toMemberHistory(sender, receiver, coupon, CouponEvent.REQUEST));
+        memberHistories.save(toMemberHistory(receiver, sender, coupon, CouponEvent.DECLINE));
+        memberHistories.flush();
+
+        long actual = memberHistories.countByHostMemberAndIsReadFalse(receiver);
+        assertThat(actual).isEqualTo(1L);
+    }
+
+    private MemberHistory toMemberHistory(Member hostMember, Member targetMember, Coupon coupon,
+                                          CouponEvent event) {
+        return new MemberHistory(null, hostMember, targetMember, coupon.getId(),
+            coupon.getCouponType(), event, coupon.getMeetingDate());
+    }
+}
