899d600e5fd4ce1de6e00904c4f173f95c20f77d
gytks4 <gytks4@naver.com>
2022-08-04 21:14:32 +0900

    feat: 승낙 -> 승인 워딩 변경

--- frontend/src/@components/user/UserHistoryItem/index.tsx
+++ frontend/src/@components/user/UserHistoryItem/index.tsx
@@ -23,7 +23,7 @@ const couponEventTextMapper: Record<COUPON_EVENT, string> = {
   REQUEST: '쿠폰 사용을 요청했어요.',
   CANCEL: '쿠폰 사용을 취소했어요.',
   DECLINE: '쿠폰 사용을 거절했어요.',
-  ACCEPT: '쿠폰 사용을 승낙했어요.',
+  ACCEPT: '쿠폰 사용을 승인했어요.',
   FINISH: '쿠폰 사용을 완료했어요.',
 };
 
