9f8eb86f58870319b5c13c06353525855788ed41
gytks4 <gytks4@naver.com>
2022-08-04 20:12:08 +0900

    chore: 사용하지 않는 변수 제거

--- frontend/src/@pages/login/index.tsx
+++ frontend/src/@pages/login/index.tsx
@@ -55,40 +55,6 @@ const Styled = {
       color: ${theme.colors.primary_400};
     `}
   `,
-  LoginForm: styled.form`
-    width: 100%;
-
-    & input {
-      width: 100%;
-      height: 50px;
-      font-size: 14px;
-      border: 1px solid ${({ theme }) => theme.colors.light_grey_100};
-
-      padding: 0 10px;
-    }
-
-    & input::placeholder {
-      ${({ theme }) => css`
-        color: ${theme.colors.light_grey_100};
-      `}
-    }
-
-    & > button {
-      width: 100%;
-      height: 50px;
-
-      font-weight: bold;
-
-      border-radius: 20px;
-
-      margin-bottom: 10px;
-
-      ${({ theme }) => css`
-        background-color: ${theme.colors.primary_400};
-        color: ${theme.colors.white_100};
-      `}
-    }
-  `,
   SlackLink: styled.a`
     display: inline-flex;
     align-items: center;
