2480b9d762c002acdb099bde52ceebdaa863644d
gytks4 <gytks4@naver.com>
2022-08-10 16:21:49 +0900

    refactor: couponTextMapper 상수 분리

--- frontend/src/@components/user/UserHistoryItem/index.tsx
+++ frontend/src/@components/user/UserHistoryItem/index.tsx
@@ -1,6 +1,6 @@
 import { MouseEventHandler } from 'react';
 
-import { COUPON_ENG_TYPE, COUPON_EVENT, COUPON_KOREAN_TYPE } from '@/types/client/coupon';
+import { couponEventTextMapper, couponTypeTextMapper } from '@/constants/coupon';
 import { UserHistory } from '@/types/client/user';
 import { generateDateText } from '@/utils';
 
@@ -10,22 +10,6 @@ interface UserHistoryItemProps {
   history: UserHistory;
   onClick: MouseEventHandler<HTMLDivElement>;
 }
-
-/** Typing은 어려워 */
-const couponTypeTextMapper: Record<COUPON_ENG_TYPE, COUPON_KOREAN_TYPE> = {
-  COFFEE: '커피',
-  DRINK: '술',
-  MEAL: '식사',
-};
-
-const couponEventTextMapper: Record<COUPON_EVENT, string> = {
-  INIT: '쿠폰을 보냈어요.',
-  REQUEST: '쿠폰 사용을 요청했어요.',
-  CANCEL: '쿠폰 사용을 취소했어요.',
-  DECLINE: '쿠폰 사용을 거절했어요.',
-  ACCEPT: '쿠폰 사용을 승인했어요.',
-  FINISH: '쿠폰 사용을 완료했어요.',
-};
 
 const UserHistoryItem = (props: UserHistoryItemProps) => {
   const { history, onClick } = props;
