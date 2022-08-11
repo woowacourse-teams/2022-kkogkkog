f27b84ad13b1972866b9e9651994f3dbe405a2f7
gytks4 <gytks4@naver.com>
2022-08-12 00:28:57 +0900

    feat: 쿠폰 상세 페이지 버튼 클릭 이벤트 구현

--- frontend/src/@pages/coupon-detail/index.tsx
+++ frontend/src/@pages/coupon-detail/index.tsx
@@ -12,6 +12,7 @@ import { useFetchCoupon } from '@/@hooks/@queries/coupon';
 import { useFetchMe } from '@/@hooks/@queries/user';
 import useChangeCouponStatus from '@/@hooks/coupon/useChangeCouponStatus';
 import { couponTypeTextMapper } from '@/constants/coupon';
+import { PATH } from '@/Router';
 import theme from '@/styles/theme';
 import { COUPON_STATUS } from '@/types/client/coupon';
 
@@ -68,8 +69,7 @@ const CouponDetail = () => {
 
   const { coupon } = useFetchCoupon(Number(couponId));
   const { me } = useFetchMe();
-  const { cancelCoupon, requestCoupon, finishCoupon, acceptCoupon, declineCoupon } =
-    useChangeCouponStatus(Number(couponId));
+  const { cancelCoupon, finishCoupon } = useChangeCouponStatus(Number(couponId));
 
   // Suspense 처리 후 NotFound 띄우기
   if (!coupon) {
@@ -91,9 +91,9 @@ const CouponDetail = () => {
   };
 
   const onClickRequestButton = () => {
-    if (window.confirm(confirmMessage)) {
-      // 메시지, 날짜 입력창으로 라우팅
-    }
+    navigate(PATH.COUPON_REQUEST, {
+      state: { coupon, isSent, couponId: Number(couponId) },
+    });
   };
 
   const onClickFinishButton = () => {
@@ -103,100 +103,89 @@ const CouponDetail = () => {
   };
 
   const onClickAcceptButton = () => {
-    if (window.confirm(confirmMessage)) {
-      acceptCoupon({
-        onSuccessCallback() {
-          // 메세지 입력 창으로 라우팅
-        },
-      });
-    }
+    navigate(PATH.COUPON_ACCEPT);
   };
 
   const onClickDeclineButton = () => {
-    if (window.confirm(confirmMessage)) {
-      declineCoupon({
-        onSuccessCallback() {
-          // 메세지 입력 창으로 라우팅
-        },
-      });
-    }
+    navigate(PATH.COUPON_DECLINE);
   };
 
   return (
     <PageTemplate title='쿠폰' hasHeader={false}>
-      <Styled.Top>
-        <Position position='absolute' top='20px' left='20px'>
-          <Icon
-            iconName='arrow'
-            size='20'
-            color={theme.colors.primary_400}
-            onClick={() => navigate(-1)}
-          />
-        </Position>
-        <Styled.ProfileImage src={isSent ? receiver.imageUrl : sender.imageUrl} alt='' />
-        <Styled.SummaryMessage>
-          {isSent ? `${receiver.nickname}님에게 ` : `${sender.nickname}님이 `}보낸&nbsp;
-          {couponTypeTextMapper[couponType]} 쿠폰
-        </Styled.SummaryMessage>
-        <Styled.CouponSummary />
-      </Styled.Top>
-      <Styled.Main>
-        <Styled.CouponInner>
-          <BigCouponItem {...coupon} />
-        </Styled.CouponInner>
-        <Styled.HistorySection>
-          <Styled.HistoryTitle>소통 히스토리</Styled.HistoryTitle>
-          <CouponHistoryList historyList={couponHistories} />
-        </Styled.HistorySection>
-        <Styled.FinishButtonInner>
-          {(couponStatus === 'READY' || couponStatus === 'REQUESTED') && (
-            <button onClick={onClickFinishButton}>혹시 쿠폰을 사용하셨나요?</button>
-          )}
-        </Styled.FinishButtonInner>
-        <Position
-          position='fixed'
-          bottom='0'
-          right='0'
-          css={theme => css`
-            width: 100%;
-            display: flex;
+      <Styled.Root>
+        <Styled.Top>
+          <Position position='absolute' top='20px' left='20px'>
+            <Icon
+              iconName='arrow'
+              size='20'
+              color={theme.colors.primary_400}
+              onClick={() => navigate(-1)}
+            />
+          </Position>
+          <Styled.ProfileImage src={isSent ? receiver.imageUrl : sender.imageUrl} alt='' />
+          <Styled.SummaryMessage>
+            {isSent ? `${receiver.nickname}님에게 ` : `${sender.nickname}님이 `}보낸&nbsp;
+            {couponTypeTextMapper[couponType]} 쿠폰
+          </Styled.SummaryMessage>
+        </Styled.Top>
+        <Styled.Main>
+          <Styled.CouponInner>
+            <BigCouponItem {...coupon} />
+          </Styled.CouponInner>
+          <Styled.HistorySection>
+            <Styled.HistoryTitle>소통 히스토리</Styled.HistoryTitle>
+            <CouponHistoryList historyList={couponHistories} />
+          </Styled.HistorySection>
+          <Styled.FinishButtonInner>
+            {(couponStatus === 'READY' || couponStatus === 'REQUESTED') && (
+              <button onClick={onClickFinishButton}>혹시 쿠폰을 사용하셨나요?</button>
+            )}
+          </Styled.FinishButtonInner>
+          <Position
+            position='fixed'
+            bottom='0'
+            right='0'
+            css={theme => css`
+              width: 100%;
+              display: flex;
 
-            & > button + button {
-              border-left: 1px solid ${theme.colors.grey_100};
-            }
-          `}
-        >
-          {buttons.map(buttonType => (
-            <Fragment key={buttonType}>
-              {buttonType === '취소' && (
-                <Button onClick={onClickCancelButton} css={Styled.ExtendedButton}>
-                  사용 취소
-                </Button>
-              )}
-              {buttonType === '완료' && (
-                <Button onClick={onClickFinishButton} css={Styled.ExtendedButton}>
-                  사용 완료
-                </Button>
-              )}
-              {buttonType === '요청' && (
-                <Button onClick={onClickRequestButton} css={Styled.ExtendedButton}>
-                  사용 요청
-                </Button>
-              )}
-              {buttonType === '승인' && (
-                <Button onClick={onClickAcceptButton} css={Styled.ExtendedButton}>
-                  사용 승인
-                </Button>
-              )}
-              {buttonType === '거절' && (
-                <Button onClick={onClickDeclineButton} css={Styled.ExtendedButton}>
-                  사용 거절
-                </Button>
-              )}
-            </Fragment>
-          ))}
-        </Position>
-      </Styled.Main>
+              & > button + button {
+                border-left: 1px solid ${theme.colors.grey_100};
+              }
+            `}
+          >
+            {buttons.map(buttonType => (
+              <Fragment key={buttonType}>
+                {buttonType === '취소' && (
+                  <Button onClick={onClickCancelButton} css={Styled.ExtendedButton}>
+                    사용 취소
+                  </Button>
+                )}
+                {buttonType === '완료' && (
+                  <Button onClick={onClickFinishButton} css={Styled.ExtendedButton}>
+                    사용 완료
+                  </Button>
+                )}
+                {buttonType === '요청' && (
+                  <Button onClick={onClickRequestButton} css={Styled.ExtendedButton}>
+                    사용 요청
+                  </Button>
+                )}
+                {buttonType === '승인' && (
+                  <Button onClick={onClickAcceptButton} css={Styled.ExtendedButton}>
+                    사용 승인
+                  </Button>
+                )}
+                {buttonType === '거절' && (
+                  <Button onClick={onClickDeclineButton} css={Styled.ExtendedButton}>
+                    사용 거절
+                  </Button>
+                )}
+              </Fragment>
+            ))}
+          </Position>
+        </Styled.Main>
+      </Styled.Root>
     </PageTemplate>
   );
 };
