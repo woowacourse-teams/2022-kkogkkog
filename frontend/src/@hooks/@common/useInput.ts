233504902c218ed82bfe8f66c28a84fa85177f00
gytks4 <gytks4@naver.com>
2022-08-12 00:28:12 +0900

    feat: useInput에서 Validations 받아오기

--- frontend/src/@hooks/@common/useInput.ts
+++ frontend/src/@hooks/@common/useInput.ts
@@ -1,14 +1,26 @@
 import { ChangeEventHandler, Dispatch, SetStateAction, useState } from 'react';
 
+type Validation = (value: string) => boolean;
+
 const useInput = (
-  defaultValue: string
-): [string, ChangeEventHandler<HTMLInputElement>, Dispatch<SetStateAction<string>>] => {
+  defaultValue: string,
+  validations?: Validation[]
+): [
+  string,
+  ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>,
+  Dispatch<SetStateAction<string>>
+] => {
   const [value, setValue] = useState(defaultValue);
 
-  const onChange: ChangeEventHandler<HTMLInputElement> = e => {
-    const { target } = e;
+  const onChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> = e => {
+    const {
+      target: { value },
+    } = e;
 
-    setValue(target.value);
+    if (validations?.some(validation => validation(value))) {
+      return;
+    }
+    setValue(value);
   };
 
   return [value, onChange, setValue];
