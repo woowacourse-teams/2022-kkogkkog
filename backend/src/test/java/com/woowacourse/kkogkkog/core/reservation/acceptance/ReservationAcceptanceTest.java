5af773f02ecd6fccea2e18d9a3b5bfa80bc85d10
Jeong Jinwoo <jwjeong96@gmail.com>
2022-08-12 15:33:48 +0900

    test: 중복되는 회원가입 및 로그인 인수테스트 메서드 통일화

--- backend/src/test/java/com/woowacourse/kkogkkog/core/reservation/acceptance/ReservationAcceptanceTest.java
+++ backend/src/test/java/com/woowacourse/kkogkkog/core/reservation/acceptance/ReservationAcceptanceTest.java
@@ -2,6 +2,7 @@ package com.woowacourse.kkogkkog.core.reservation.acceptance;
 
 import static com.woowacourse.kkogkkog.acceptance.AcceptanceContext.invokePostWithToken;
 import static com.woowacourse.kkogkkog.acceptance.AcceptanceContext.invokePutWithToken;
+import static com.woowacourse.kkogkkog.acceptance.AuthAcceptanceTest.회원가입을_하고;
 import static com.woowacourse.kkogkkog.common.fixture.domain.MemberFixture.LEO;
 import static com.woowacourse.kkogkkog.common.fixture.domain.MemberFixture.ROOKIE;
 import static com.woowacourse.kkogkkog.common.fixture.dto.CouponDtoFixture.COFFEE_쿠폰_생성_요청;
@@ -10,36 +11,25 @@ import static com.woowacourse.kkogkkog.common.fixture.dto.ReservationDtoFixture.
 import static com.woowacourse.kkogkkog.core.coupon.acceptance.CouponAcceptanceTest.쿠폰_생성을_요청하고;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.BDDMockito.given;
 
 import com.woowacourse.kkogkkog.acceptance.AcceptanceTest;
-import com.woowacourse.kkogkkog.application.dto.MemberResponse;
-import com.woowacourse.kkogkkog.application.dto.TokenResponse;
-import com.woowacourse.kkogkkog.infrastructure.SlackUserInfo;
 import com.woowacourse.kkogkkog.reservation.presentation.dto.ReservationCreateRequest;
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import java.time.LocalDate;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 
 public class ReservationAcceptanceTest extends AcceptanceTest {
 
-    public static ExtractableResponse<Response> 예약을_신청한디(String token, Object data) {
-        return invokePostWithToken("/api/reservations", token, data);
-    }
-
     @Test
     void 예약을_신청할_수_있다() {
-        String rookieToken = 로그인을_하고(MemberResponse.of(ROOKIE.getMember()));
-        String leoToken = 로그인을_하고(MemberResponse.of(LEO.getMember()));
-
+        String rookieToken = 회원가입을_하고(ROOKIE.getMember());
+        String leoToken = 회원가입을_하고(LEO.getMember());
         쿠폰_생성을_요청하고(rookieToken, COFFEE_쿠폰_생성_요청(List.of(2L)));
 
-        ExtractableResponse<Response> extract = 예약을_신청한디(
+        ExtractableResponse<Response> extract = 예약을_신청한다(
             leoToken, 예약_생성_요청(1L, LocalDate.now()));
 
         assertAll(
@@ -50,8 +40,8 @@ public class ReservationAcceptanceTest extends AcceptanceTest {
 
     @Test
     void 예약_상태를_변경할_수_있다() {
-        String rookieToken = 로그인을_하고(MemberResponse.of(ROOKIE.getMember()));
-        String leoToken = 로그인을_하고(MemberResponse.of(LEO.getMember()));
+        String rookieToken = 회원가입을_하고(ROOKIE.getMember());
+        String leoToken = 회원가입을_하고(LEO.getMember());
         쿠폰_생성을_요청하고(rookieToken, COFFEE_쿠폰_생성_요청(List.of(2L)));
         예약을_신청하고(leoToken, 예약_생성_요청(1L, LocalDate.now()));
 
@@ -71,27 +61,7 @@ public class ReservationAcceptanceTest extends AcceptanceTest {
         invokePostWithToken("/api/reservations", token, request);
     }
 
-    // 임시로 다음위치에 이동함 (리팩토링 대상)
-    private String 로그인을_하고(MemberResponse memberResponse) {
-        given(slackClient.getUserInfoByCode("CODE"))
-            .willReturn(
-                new SlackUserInfo(
-                    memberResponse.getUserId(),
-                    null,
-                    null,
-                    memberResponse.getNickname(),
-                    memberResponse.getEmail(),
-                    memberResponse.getImageUrl()));
-
-        ExtractableResponse<Response> extract = RestAssured.given().log().all()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .queryParam("code", "CODE")
-            .get("/api/login/token")
-            .then().log().all()
-            .extract();
-
-        assertThat(extract.statusCode()).isEqualTo(HttpStatus.OK.value());
-        return extract.as(TokenResponse.class).getAccessToken();
+    public static ExtractableResponse<Response> 예약을_신청한다(String token, Object data) {
+        return invokePostWithToken("/api/reservations", token, data);
     }
 }
