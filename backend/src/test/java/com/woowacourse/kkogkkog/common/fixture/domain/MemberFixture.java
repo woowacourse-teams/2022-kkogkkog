c755d3303894f2ea8e9e0d8b07301a5f7b3fb3c6
Jeong Jinwoo <jwjeong96@gmail.com>
2022-08-12 14:05:25 +0900

    test: MemberFixture에서 연관관계로 대체된 컬럼 제거

--- backend/src/test/java/com/woowacourse/kkogkkog/common/fixture/domain/MemberFixture.java
+++ backend/src/test/java/com/woowacourse/kkogkkog/common/fixture/domain/MemberFixture.java
@@ -7,29 +7,26 @@ import com.woowacourse.kkogkkog.domain.Workspace;
 
 public enum MemberFixture {
 
-    ROOKIE("rookieId1", "workspaceId", "루키", "rookie@gmail.com", "https://slack"),
-    AUTHOR("authorId2", "workspaceId", "아서", "author@gmail.com", "https://slack"),
-    JEONG("jeongId3", "workspaceId", "정", "jeong@gmail.com", "https://slack"),
-    LEO("leoId4", "workspaceId", "레오", "leo@gmail.com", "https://slack"),
+    ROOKIE("rookieId1", "루키", "rookie@gmail.com", "https://slack"),
+    AUTHOR("authorId2", "아서", "author@gmail.com", "https://slack"),
+    JEONG("jeongId3", "정", "jeong@gmail.com", "https://slack"),
+    LEO("leoId4", "레오", "leo@gmail.com", "https://slack"),
 
-    SENDER("senderId1", "workspaceId", "루키", "sender@gmail.com", "https://slack"),
-    RECEIVER("receiverId1", "workspaceId", "아서", "receiver@gmail.com", "https://slack"),
-    RECEIVER2("receiverId2", "workspaceId", "정", "receiver@gmail.com", "https://slack"),
+    SENDER("senderId1", "루키", "sender@gmail.com", "https://slack"),
+    RECEIVER("receiverId1", "아서", "receiver@gmail.com", "https://slack"),
+    RECEIVER2("receiverId2", "정", "receiver@gmail.com", "https://slack"),
     ;
 
     private final String userId;
-    private final String workspaceId;
     private final String nickname;
     private final String email;
     private final String imageUrl;
 
     MemberFixture(String userId,
-                  String workspaceId,
                   String nickname,
                   String email,
                   String imageUrl) {
         this.userId = userId;
-        this.workspaceId = workspaceId;
         this.nickname = nickname;
         this.email = email;
         this.imageUrl = imageUrl;
