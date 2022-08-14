a405b799887fff42e01b8c80aea82d1825e6dfa9
Jeong Jinwoo <jwjeong96@gmail.com>
2022-08-14 11:24:51 +0900

    fix: @EnableJpaAuditing 설정을 위한 빈 분리. @DataJpaTest를 사용하는 부분에서 직접 @Import하도록 셋업

--- backend/src/test/java/com/woowacourse/kkogkkog/common/annotaion/RepositoryTest.java
+++ backend/src/test/java/com/woowacourse/kkogkkog/common/annotaion/RepositoryTest.java
@@ -1,15 +1,18 @@
 package com.woowacourse.kkogkkog.common.annotaion;
 
+import com.woowacourse.kkogkkog.config.DatabaseConfig;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(DatabaseConfig.class)
 public @interface RepositoryTest {
 }
