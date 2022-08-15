3e31dd33ba0bd7fe3fd94d792ac8b5688b6e1694
gytks4 <gytks4@naver.com>
2022-08-15 17:49:04 +0900

    refactor: 배포 환경에 따라 slack redirect uri 구분하기

--- frontend/src/@pages/download/index.tsx
+++ frontend/src/@pages/download/index.tsx
@@ -10,15 +10,27 @@ const DownloadPage = () => {
         쿠폰 알림을 받을 수 있어요.
       </Styled.Description>
       <Styled.SlackAddButtonInner>
-        <a href='https://slack.com/oauth/v2/authorize?client_id=3711114175136.3863202543751&scope=chat:write,chat:write.public,users:read.email,users:read&user_scope=email,openid,profile&redirect_uri=https://kkogkkog.com/download/redirect'>
-          <img
-            alt='Add to Slack'
-            height='40'
-            width='139'
-            src='https://platform.slack-edge.com/img/add_to_slack.png'
-            srcSet='https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x'
-          />
-        </a>
+        {process.env.NODE_ENV === 'production' ? (
+          <a href='https://slack.com/oauth/v2/authorize?client_id=3711114175136.3863202543751&scope=chat:write,chat:write.public,users:read.email,users:read&user_scope=email,openid,profile&redirect_uri=https://kkogkkog.com/download/redirect'>
+            <img
+              alt='Add to Slack'
+              height='40'
+              width='139'
+              src='https://platform.slack-edge.com/img/add_to_slack.png'
+              srcSet='https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x'
+            />
+          </a>
+        ) : (
+          <a href='https://slack.com/oauth/v2/authorize?client_id=3711114175136.3863202543751&scope=chat:write,chat:write.public,users:read.email,users:read&user_scope=email,openid,profile&redirect_uri=https://dev.kkogkkog.com/download/redirect'>
+            <img
+              alt='Add to Slack'
+              height='40'
+              width='139'
+              src='https://platform.slack-edge.com/img/add_to_slack.png'
+              srcSet='https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x'
+            />
+          </a>
+        )}
       </Styled.SlackAddButtonInner>
     </Styled.Root>
   );
