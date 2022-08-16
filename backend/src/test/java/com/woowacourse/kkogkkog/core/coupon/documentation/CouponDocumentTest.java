2778721816f200719d2d5a0ec0207eabfd1abcfa
Wishoon <heonga2@gmail.com>
2022-08-16 17:18:50 +0900

    feat: docs에 parameter 값 제거

--- backend/src/test/java/com/woowacourse/kkogkkog/core/coupon/documentation/CouponDocumentTest.java
+++ backend/src/test/java/com/woowacourse/kkogkkog/core/coupon/documentation/CouponDocumentTest.java
@@ -60,41 +60,7 @@ class CouponDocumentTest extends Documentation {
             .andDo(print())
             .andDo(document("coupon-create",
                 getDocumentRequest(),
-                getDocumentResponse(),
-                requestHeaders(
-                    headerWithName(HttpHeaders.AUTHORIZATION).description("Access Token")
-                ),
-                requestFields(
-                    fieldWithPath("receiverIds").type(JsonFieldType.ARRAY)
-                        .description("쿠폰 받을 사람들의 ID 리스트"),
-                    fieldWithPath("hashtag").type(JsonFieldType.STRING)
-                        .description("쿠폰 해시태그"),
-                    fieldWithPath("description").type(JsonFieldType.STRING)
-                        .description("쿠폰 추가 설명"),
-                    fieldWithPath("couponType").type(JsonFieldType.STRING)
-                        .description("쿠폰 타입")
-                ),
-                responseFields(
-                    fieldWithPath("data.[].id").type(JsonFieldType.NUMBER)
-                        .description("쿠폰 ID"),
-                    fieldWithPath("data.[].senderId").type(JsonFieldType.NUMBER)
-                        .description("쿠폰을 보낸 사람의 ID"),
-                    fieldWithPath("data.[].senderNickname").type(JsonFieldType.STRING)
-                        .description("쿠폰을 보낸 사람의 닉네임"),
-                    fieldWithPath("data.[].receiverId").type(JsonFieldType.NUMBER)
-                        .description("쿠폰을 받을 사람의 ID"),
-                    fieldWithPath("data.[].receiverNickname").type(JsonFieldType.STRING)
-                        .description("쿠폰을 받을 사람의 닉네임"),
-                    fieldWithPath("data.[].hashtag").type(JsonFieldType.STRING)
-                        .description("쿠폰 해시태그"),
-                    fieldWithPath("data.[].description").type(JsonFieldType.STRING)
-                        .description("쿠폰 추가 설명"),
-                    fieldWithPath("data.[].couponType").type(JsonFieldType.STRING)
-                        .description("쿠폰 타입"),
-                    fieldWithPath("data.[].couponStatus").type(JsonFieldType.STRING)
-                        .description("쿠폰 상태")
-                )
-            ));
+                getDocumentResponse()));
     }
 
     @Test
@@ -121,57 +87,7 @@ class CouponDocumentTest extends Documentation {
             .andDo(print())
             .andDo(document("coupon-showAll-me",
                 getDocumentRequest(),
-                getDocumentResponse(),
-                requestHeaders(
-                    headerWithName(HttpHeaders.AUTHORIZATION).description("Access Token")
-                ),
-                responseFields(
-                    fieldWithPath("data.received.[].couponId").type(JsonFieldType.NUMBER)
-                        .description("받은 쿠폰 ID"),
-                    fieldWithPath("data.received.[].reservationId").type(JsonFieldType.NUMBER)
-                        .description("예약 ID"),
-                    fieldWithPath("data.received.[].memberId").type(JsonFieldType.NUMBER)
-                        .description("받은 사람 ID"),
-                    fieldWithPath("data.received.[].nickname").type(JsonFieldType.STRING)
-                        .description("받은 사람 닉네임"),
-                    fieldWithPath("data.received.[].imageUrl").type(JsonFieldType.STRING)
-                        .description("받은 사람 이미지"),
-                    fieldWithPath("data.received.[].hashtag").type(JsonFieldType.STRING)
-                        .description("받은 쿠폰 해시태그"),
-                    fieldWithPath("data.received.[].description").type(JsonFieldType.STRING)
-                        .description("받은 쿠폰 설명"),
-                    fieldWithPath("data.received.[].couponType").type(JsonFieldType.STRING)
-                        .description("받은 쿠폰 타입"),
-                    fieldWithPath("data.received.[].couponStatus").type(JsonFieldType.STRING)
-                        .description("받은 쿠폰 상태"),
-                    fieldWithPath("data.received.[].message").type(JsonFieldType.STRING)
-                        .description("예약 메시지"),
-                    fieldWithPath("data.received.[].meetingDate").type(JsonFieldType.STRING)
-                        .description("예약 시간"),
-
-                    fieldWithPath("data.sent.[].couponId").type(JsonFieldType.NUMBER)
-                        .description("보낸 쿠폰 ID"),
-                    fieldWithPath("data.sent.[].reservationId").type(JsonFieldType.NUMBER)
-                        .description("예약 ID"),
-                    fieldWithPath("data.sent.[].memberId").type(JsonFieldType.NUMBER)
-                        .description("보낸 사람 ID"),
-                    fieldWithPath("data.sent.[].nickname").type(JsonFieldType.STRING)
-                        .description("보낸 사람 닉네임"),
-                    fieldWithPath("data.sent.[].imageUrl").type(JsonFieldType.STRING)
-                        .description("받은 사람 이미지"),
-                    fieldWithPath("data.sent.[].hashtag").type(JsonFieldType.STRING)
-                        .description("보낸 쿠폰 해시태그"),
-                    fieldWithPath("data.sent.[].description").type(JsonFieldType.STRING)
-                        .description("보낸 쿠폰 설명"),
-                    fieldWithPath("data.sent.[].couponType").type(JsonFieldType.STRING)
-                        .description("보낸 쿠폰 타입"),
-                    fieldWithPath("data.sent.[].couponStatus").type(JsonFieldType.STRING)
-                        .description("보낸 쿠폰 상태"),
-                    fieldWithPath("data.sent.[].message").type(JsonFieldType.STRING)
-                        .description("예약 메시지"),
-                    fieldWithPath("data.sent.[].meetingDate").type(JsonFieldType.STRING)
-                        .description("예약 시간")
-                ))
+                getDocumentResponse())
             );
     }
 
@@ -190,6 +106,9 @@ class CouponDocumentTest extends Documentation {
 
         perform
             .andDo(print())
-            .andDo(document("coupon-show"));
+            .andDo(document("coupon-show",
+                getDocumentRequest(),
+                getDocumentResponse())
+            );
     }
 }
