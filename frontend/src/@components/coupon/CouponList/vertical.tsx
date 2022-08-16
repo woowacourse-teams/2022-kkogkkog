d5fcc52e1d3b8a65228e9b02f9ff0de51f87ee51
junzzi <jjh01027717502@gmail.com>
2022-08-16 12:14:53 +0900

    feat: skeleton_coupon_big.png 이미지 변경

--- frontend/src/@components/coupon/CouponList/vertical.tsx
+++ frontend/src/@components/coupon/CouponList/vertical.tsx
@@ -19,7 +19,7 @@ const VerticalCouponList = (props: VerticalCouponListProps) => {
       <Styled.Root>
         <Styled.TextContainer>
           <div>
-            <img src='/assets/images/skeleton_coupon_big.png' alt='쿠폰' width={100} />
+            <img src='/assets/images/skeleton_coupon_big.png' alt='쿠폰' width={130} />
           </div>
           <h2>아직 쿠폰이 존재하지 않아요.</h2>
           <h3>쿠폰을 생성해보세요!</h3>
