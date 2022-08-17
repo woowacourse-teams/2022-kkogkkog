59b3f3b6eccdaf1a2f3d34462285213a012e86e7
gytks4 <gytks4@naver.com>
2022-08-17 14:41:30 +0900

    feat: useTokenMutation 정의

--- frontend/src/@hooks/@common/useTokenQuery.ts
+++ frontend/src/@hooks/@common/useTokenQuery.ts
@@ -1,5 +1,13 @@
 import { AxiosError } from 'axios';
-import { QueryFunction, QueryKey, useQuery, UseQueryOptions } from 'react-query';
+import {
+  MutationFunction,
+  QueryFunction,
+  QueryKey,
+  useMutation,
+  UseMutationOptions,
+  useQuery,
+  UseQueryOptions,
+} from 'react-query';
 import { useNavigate } from 'react-router-dom';
 
 import { PATH } from '@/Router';
@@ -31,6 +39,34 @@ export const useTokenQuery = <
       }
 
       options?.onError?.(error);
+    },
+  });
+};
+
+export const useTokenMutation = <
+  TData = unknown,
+  TError = unknown,
+  TVariables = void,
+  TContext = unknown
+>(
+  mutationFn: MutationFunction<TData, TVariables>,
+  options?: Omit<UseMutationOptions<TData, TError, TVariables, TContext>, 'mutationFn'>
+) => {
+  const { displayMessage } = useToast();
+  const navigate = useNavigate();
+
+  return useMutation(mutationFn, {
+    ...options,
+    onError(error, variables, context) {
+      if (error instanceof AxiosError && error.response?.status === 401) {
+        localStorage.removeItem('user-token');
+        displayMessage('다시 로그인해주세요', true);
+        navigate(PATH.LOGIN);
+
+        return;
+      }
+
+      options?.onError?.(error, variables, context);
     },
   });
 };
