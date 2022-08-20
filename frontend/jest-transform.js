6e034d8344f546f83cadce3d8a4ce138b64b7406
gytks4 <gytks4@naver.com>
2022-08-20 16:25:28 +0900

    chore: jest transform 파일 주석처리

--- frontend/jest-transform.js
+++ frontend/jest-transform.js
@@ -1,6 +1,6 @@
-const config = {
-  babelrc: false,
-  presets: ['@babel/react'],
-};
+// const config = {
+//   babelrc: false,
+//   presets: ['@babel/react'],
+// };
 
-module.exports = require('babel-jest').createTransformer(config);
+// module.exports = require('babel-jest').createTransformer(config);
