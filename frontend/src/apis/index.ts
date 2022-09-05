934eeb2041a70f76d93b3b4d4a31f6ffcb6438ab
juunzzi <78349600+juunzzi@users.noreply.github.com>
2022-09-05 14:55:32 +0900

    refactor: process.env.NODE_ENV는 웹팩 빌딩 서버 환경 변수이다. 프론트엔드 환경 변수는 PRODUCT_ENV (#301)

--- frontend/src/apis/index.ts
+++ frontend/src/apis/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const BASE_URL =
-  process.env.NODE_ENV === 'production' ? process.env.PROD_SERVER : process.env.DEV_SERVER;
+  PRODUCT_ENV === 'production' ? process.env.PROD_SERVER : process.env.DEV_SERVER;
 
 export const client = axios.create({
   baseURL: BASE_URL,
