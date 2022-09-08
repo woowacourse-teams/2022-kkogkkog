a482c7403ce25acbdaf7b5d4d4bf96d0d9b48f2b
gytks4 <gytks4@naver.com>
2022-09-08 15:53:40 +0900

    chore: test 환경 변수 삽입을 코드로 대체
    - jest에 환경 변수 삽입 해결 못 했음

--- frontend/src/apis/index.ts
+++ frontend/src/apis/index.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 export const BASE_URL =
-  PRODUCT_ENV === 'production' ? process.env.PROD_SERVER : process.env.DEV_SERVER;
+  PRODUCT_ENV === 'production'
+    ? process.env.PROD_SERVER
+    : process.env.DEV_SERVER ?? 'http://localhost:8080';
 
 export const client = axios.create({
   baseURL: BASE_URL,
