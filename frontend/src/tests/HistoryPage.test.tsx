5a892e69d099201171fa6cacca551b611b3d0392
gytks4 <gytks4@naver.com>
2022-09-08 18:14:37 +0900

    test: HistoryPage 테스트

--- frontend/src/tests/HistoryPage.test.tsx
+++ frontend/src/tests/HistoryPage.test.tsx
@@ -1,19 +1,27 @@
 import { screen } from '@testing-library/react';
 
 import UserHistoryPage from '@/@pages/history';
+import theme from '@/styles/theme';
+import { rgb2hex } from '@/utils/rgba';
 
 import { testUser } from '../setupTests';
 import { render } from './test-utils';
 
 describe('<UserHistoryPage />', () => {
-  it('알림을 확인할 수 있다.', async () => {
+  it('알림이 온 개수만큼 알림 히스토리를 확인할 수 있다.', async () => {
     render(<UserHistoryPage />);
 
-    expect(await screen.findByText(testUser.nickname)).toBeInTheDocument();
+    expect(await screen.findAllByRole('listitem')).toHaveLength(testUser.histories.length);
   });
-  // 히스토리를 확인할 수 있다.
-  // 안 읽은 알림은 주황색 배경을 가진다.
 
-  // 이것들은 테스트가 가능할까? + mock 보충 필요
-  // 하나 읽고 돌아오면 하나만 읽음 처리 된다.
+  it('읽지 않은 알림은 주황색 배경을 가진다.', async () => {
+    render(<UserHistoryPage />);
+
+    const unReadCount = testUser.histories.filter(history => !history.isRead).length;
+    const unReadHistories = (await screen.findAllByRole('listitem')).filter(
+      li => `${theme.colors.primary_100}20` === rgb2hex(getComputedStyle(li).backgroundColor)
+    );
+
+    expect(unReadHistories).toHaveLength(unReadCount);
+  });
 });
