dd30e11bad76d89bf267bb488f18c7a941fed43e
Jeong Jinwoo <jwjeong96@gmail.com>
2022-09-11 15:09:03 +0900

    refactor: V1 스키마 가독성 개선
    - UNIQUE 키는 해당 컬럼에 명시
    - nullable한 컬럼들의 DEFAULT NULL 제거
    - 타입은 전부 대문자로 통일

--- backend/src/main/resources/db/migration/V1__init.sql
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -1,68 +1,65 @@
 CREATE TABLE IF NOT EXISTS `coupon` (
-    `id` bigint NOT NULL AUTO_INCREMENT
-    ,`sender_member_id` bigint NOT NULL
-    ,`receiver_member_id` bigint NOT NULL
-    ,`description` varchar(255) NOT NULL
-    ,`hashtag` varchar(255) NOT NULL
-    ,`coupon_type` varchar(255) NOT NULL
-    ,`coupon_status` varchar(255) NOT NULL
-    ,`created_time` datetime NOT NULL
-    ,`updated_time` datetime NOT NULL
+    `id` BIGINT NOT NULL AUTO_INCREMENT
+    ,`sender_member_id` BIGINT NOT NULL
+    ,`receiver_member_id` BIGINT NOT NULL
+    ,`description` VARCHAR(255) NOT NULL
+    ,`hashtag` VARCHAR(255) NOT NULL
+    ,`coupon_type` VARCHAR(255) NOT NULL
+    ,`coupon_status` VARCHAR(255) NOT NULL
+    ,`created_time` DATETIME NOT NULL
+    ,`updated_time` DATETIME NOT NULL
     ,PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `member` (
-    `id` bigint NOT NULL AUTO_INCREMENT
-    ,`email` varchar(255) NOT NULL
-    ,`user_id` varchar(255) NOT NULL
-    ,`workspace_id` varchar(255) NOT NULL
-    ,`nickname` varchar(255) NOT NULL
-    ,`image_url` varchar(255) NOT NULL
+    `id` BIGINT NOT NULL AUTO_INCREMENT
+    ,`email` VARCHAR(255) NOT NULL UNIQUE
+    ,`user_id` VARCHAR(255) NOT NULL
+    ,`workspace_id` VARCHAR(255) NOT NULL
+    ,`nickname` VARCHAR(255) NOT NULL
+    ,`image_url` VARCHAR(255) NOT NULL
     ,PRIMARY KEY (`id`)
-    ,UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `member_history` (
-    `id` bigint NOT NULL AUTO_INCREMENT
-    ,`host_member_id` bigint NOT NULL
-    ,`target_member_id` bigint DEFAULT NULL
-    ,`coupon_id` bigint DEFAULT NULL
-    ,`coupon_type` varchar(255) DEFAULT NULL
-    ,`coupon_event` varchar(255) DEFAULT NULL
-    ,`meeting_date` datetime DEFAULT NULL
-    ,`message` varchar(255) DEFAULT NULL
-    ,`is_read` tinyint(1) NOT NULL
-    ,`created_time` datetime NOT NULL
+    `id` BIGINT NOT NULL AUTO_INCREMENT
+    ,`host_member_id` BIGINT NOT NULL
+    ,`target_member_id` BIGINT
+    ,`coupon_id` BIGINT
+    ,`coupon_type` VARCHAR(255)
+    ,`coupon_event` VARCHAR(255)
+    ,`meeting_date` DATETIME
+    ,`message` VARCHAR(255)
+    ,`is_read` BOOLEAN NOT NULL
+    ,`created_time` DATETIME NOT NULL
     ,PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `reservation` (
-    `id` bigint NOT NULL AUTO_INCREMENT
-    ,`meeting_date` datetime NOT NULL
-    ,`message` varchar(255) NOT NULL
-    ,`coupon_id` bigint NOT NULL
-    ,`created_time` datetime NOT NULL
-    ,`updated_time` datetime NOT NULL
+    `id` BIGINT NOT NULL AUTO_INCREMENT
+    ,`meeting_date` DATETIME NOT NULL
+    ,`message` VARCHAR(255) NOT NULL
+    ,`coupon_id` BIGINT NOT NULL
+    ,`created_time` DATETIME NOT NULL
+    ,`updated_time` DATETIME NOT NULL
     ,PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `workspace` (
-    `id` bigint NOT NULL AUTO_INCREMENT
-    ,`workspace_id` varchar(255) NOT NULL
-    ,`name` varchar(255) NOT NULL
-    ,`access_token` varchar(255) DEFAULT NULL
+    `id` BIGINT NOT NULL AUTO_INCREMENT
+    ,`workspace_id` VARCHAR(255) NOT NULL UNIQUE
+    ,`name` VARCHAR(255) NOT NULL
+    ,`access_token` VARCHAR(255)
     ,PRIMARY KEY (`id`)
-    ,UNIQUE KEY `workspace_id` (`workspace_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE IF NOT EXISTS `workspace_user` (
-    `id` bigint NOT NULL AUTO_INCREMENT
-    ,`user_id` varchar(255) NOT NULL
-    ,`workspace_id` varchar(255) NOT NULL
-    ,`display_name` varchar(255) NOT NULL
-    ,`email` varchar(255) NOT NULL
-    ,`image_url` varchar(255) NOT NULL
-    ,`master_member_id` bigint NOT NULL
+    `id` BIGINT NOT NULL AUTO_INCREMENT
+    ,`user_id` VARCHAR(255) NOT NULL UNIQUE
+    ,`workspace_id` VARCHAR(255) NOT NULL
+    ,`display_name` VARCHAR(255) NOT NULL
+    ,`email` VARCHAR(255) NOT NULL
+    ,`image_url` VARCHAR(255) NOT NULL
+    ,`master_member_id` BIGINT NOT NULL
     ,PRIMARY KEY (`id`)
-    ,UNIQUE KEY `user_id` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
