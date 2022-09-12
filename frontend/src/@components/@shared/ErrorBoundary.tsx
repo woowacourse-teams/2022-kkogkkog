df4f91479afa87bdf689f9c89303b940f36fb601
gytks4 <gytks4@naver.com>
2022-09-12 11:23:32 +0900

    feat: 에러 바운더리에 401 에러 케이스 추가

--- frontend/src/@components/@shared/ErrorBoundary.tsx
+++ frontend/src/@components/@shared/ErrorBoundary.tsx
@@ -1,7 +1,9 @@
 import { AxiosError } from 'axios';
 import React, { Component, PropsWithChildren } from 'react';
 
-import { NavigationProps } from '../helper/withNavigation';
+import { PATH } from '@/Router';
+
+import withNavigation, { NavigationProps } from '../helper/withNavigation';
 import { ErrorFallbackProps } from './ErrorFallback';
 import { ToastContext, ToastContextType } from './ToastProvider';
 
@@ -38,13 +40,22 @@ class ErrorBoundary extends Component<
   static contextType = ToastContext;
 
   render() {
-    const { fallback: FallbackComponent, children } = this.props;
+    const { fallback: FallbackComponent, children, navigate } = this.props;
 
     const { error } = this.state;
 
     const { displayMessage } = this.context as ToastContextType;
 
     if (error) {
+      /** unauthorized 에러 처리 */
+      if (error.response?.status === 401) {
+        localStorage.removeItem('user-token');
+        displayMessage('다시 로그인해주세요', true);
+        navigate(PATH.LOGIN);
+
+        return children;
+      }
+
       /** get 메소드일때만 fallback을 띄운다. */
       if (error.response?.config.method === 'get') {
         return <FallbackComponent error={error} resetErrorBoundary={this.resetErrorBoundary} />;
@@ -58,4 +69,4 @@ class ErrorBoundary extends Component<
   }
 }
 
-export default ErrorBoundary;
+export default withNavigation(ErrorBoundary);
