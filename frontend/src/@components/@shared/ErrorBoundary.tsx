518b91838e799d1e53798d017d692be833034b08
gytks4 <gytks4@naver.com>
2022-09-12 11:22:25 +0900

    feat: 에러 바운더리에 toast 에러 케이스 추가

--- frontend/src/@components/@shared/ErrorBoundary.tsx
+++ frontend/src/@components/@shared/ErrorBoundary.tsx
@@ -3,6 +3,7 @@ import React, { Component, PropsWithChildren } from 'react';
 
 import { NavigationProps } from '../helper/withNavigation';
 import { ErrorFallbackProps } from './ErrorFallback';
+import { ToastContext, ToastContextType } from './ToastProvider';
 
 interface ErrorBoundaryProps {
   fallback: React.ComponentType<ErrorFallbackProps>;
@@ -34,13 +35,23 @@ class ErrorBoundary extends Component<
     return { error };
   }
 
+  static contextType = ToastContext;
+
   render() {
     const { fallback: FallbackComponent, children } = this.props;
 
     const { error } = this.state;
 
+    const { displayMessage } = this.context as ToastContextType;
+
     if (error) {
-      return <FallbackComponent error={error} resetErrorBoundary={this.resetErrorBoundary} />;
+      /** get 메소드일때만 fallback을 띄운다. */
+      if (error.response?.config.method === 'get') {
+        return <FallbackComponent error={error} resetErrorBoundary={this.resetErrorBoundary} />;
+      }
+
+      /** toast 에러 처리 */
+      displayMessage((error.response?.data as any).message, true);
     }
 
     return children;
