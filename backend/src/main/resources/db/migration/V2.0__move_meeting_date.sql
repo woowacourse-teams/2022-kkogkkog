a782f5c45410b6ac06b97ec6a657131d7c24b5a4
Jeong Jinwoo <jwjeong96@gmail.com>
2022-09-12 21:27:11 +0900

    feat: 기존 데이터에 대한 마이그레이션 작업 분리

--- backend/src/main/resources/db/migration/V2.0__move_meeting_date.sql
+++ backend/src/main/resources/db/migration/V2.0__move_meeting_date.sql
@@ -1,7 +1,2 @@
 ALTER TABLE `coupon`
     ADD `meeting_date` DATETIME;
-
-UPDATE coupon
-    INNER JOIN reservation
-    ON reservation.coupon_id = coupon.id
-SET coupon.meeting_date = reservation.meeting_date;
