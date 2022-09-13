e13aa8e9e907d514ff3980ccf76895b7c4f3ab55
dwl21 <nggus5@naver.com>
2022-09-13 14:22:35 +0900

    feat: Woowacourse 워크스페이스에 푸쉬 알림을 보내는 이벤트 객체 구현

---
+++ backend/src/main/java/com/woowacourse/kkogkkog/infrastructure/event/WoowacoursePushAlarmEvent.java
@@ -0,0 +1,30 @@
+package com.woowacourse.kkogkkog.infrastructure.event;
+
+import com.woowacourse.kkogkkog.coupon.domain.CouponEvent;
+import com.woowacourse.kkogkkog.member.domain.Member;
+import com.woowacourse.kkogkkog.member.domain.MemberHistory;
+import lombok.Getter;
+
+@Getter
+public class WoowacoursePushAlarmEvent {
+
+    private final String hostMemberId;
+    private final String message;
+    private final CouponEvent couponEvent;
+
+    public WoowacoursePushAlarmEvent(String hostMemberId, String message, CouponEvent couponEvent) {
+        this.hostMemberId = hostMemberId;
+        this.message = message;
+        this.couponEvent = couponEvent;
+    }
+
+    public static WoowacoursePushAlarmEvent of(MemberHistory memberHistory) {
+        Member hostMember = memberHistory.getHostMember();
+        return new WoowacoursePushAlarmEvent(hostMember.getUserId(),
+            memberHistory.toNoticeMessage(), memberHistory.getCouponEvent());
+    }
+
+    public boolean shouldNotSendPushAlarm() {
+        return couponEvent == CouponEvent.FINISH;
+    }
+}
