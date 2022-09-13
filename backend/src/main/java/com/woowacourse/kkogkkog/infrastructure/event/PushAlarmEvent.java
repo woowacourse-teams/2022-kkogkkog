8a27a94033fa87088bf5566f73aa2c33d3120d62
dwl21 <nggus5@naver.com>
2022-09-13 15:30:50 +0900

    feat: 푸시 알림 이벤트들을 추상 클래스로 상속

--- backend/src/main/java/com/woowacourse/kkogkkog/infrastructure/event/PushAlarmEvent.java
+++ backend/src/main/java/com/woowacourse/kkogkkog/infrastructure/event/PushAlarmEvent.java
@@ -7,7 +7,7 @@ import com.woowacourse.kkogkkog.member.domain.Workspace;
 import lombok.Getter;
 
 @Getter
-public class PushAlarmEvent {
+public class PushAlarmEvent extends SimplePushAlarmEvent{
 
     private final String botAccessToken;
     private final String hostMemberId;
