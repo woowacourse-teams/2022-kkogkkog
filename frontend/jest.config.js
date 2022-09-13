2137fa5d0e53ce5d7e8dae7eb7542bd323f81eee
gytks4 <gytks4@naver.com>
2022-09-13 22:30:46 +0900

    chore: 주석 제거

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,28 +1,14 @@
 module.exports = {
-  // Automatically clear mock calls, instances, contexts and results before every test
   clearMocks: true,
-
-  // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: false,
-
-  // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
-
-  // Indicates which provider should be used to instrument code for coverage
   coverageProvider: 'v8',
-
-  // An array of file extensions your modules use
   moduleFileExtensions: ['js', 'mjs', 'cjs', 'jsx', 'ts', 'tsx', 'json', 'node'],
-
   testEnvironment: 'jsdom',
-
-  // The glob patterns Jest uses to detect test files
   testMatch: [
     '<rootDir>/**/*.test.(js|jsx|ts|tsx)',
     '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))',
   ],
-
-  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: ['<rootDir>/node_modules/'],
   moduleDirectories: ['node_modules', 'src'],
   moduleNameMapper: {
@@ -30,9 +16,7 @@ module.exports = {
   },
   resetMocks: false,
   setupFiles: ['dotenv/config'],
-  // 서버를 실행하기 위한 세팅 추가
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect', './src/setupTests.ts'],
-  // webpack에 의해 생성되는 전역 변수 개별 설정
   globals: {
     PRODUCT_ENV: 'test',
   },
