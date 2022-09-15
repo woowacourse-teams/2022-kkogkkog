ddaed499bf70af3a154955f8e1532695044d148f
Hyunta <mohai2618@gmail.com>
2022-09-15 16:59:42 +0900

    feat: CouponRepository에서 상태와 역할별로 쿠폰을 전체 조회하는 기능 구현

--- backend/src/main/java/com/woowacourse/kkogkkog/coupon/domain/repository/CouponRepository.java
+++ backend/src/main/java/com/woowacourse/kkogkkog/coupon/domain/repository/CouponRepository.java
@@ -1,6 +1,8 @@
 package com.woowacourse.kkogkkog.coupon.domain.repository;
 
+import com.woowacourse.kkogkkog.coupon.application.dto.CouponResponse;
 import com.woowacourse.kkogkkog.coupon.domain.Coupon;
+import com.woowacourse.kkogkkog.coupon.domain.CouponStatus;
 import com.woowacourse.kkogkkog.member.domain.Member;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -19,10 +21,26 @@ public interface CouponRepository extends JpaRepository<Coupon, Long> {
 
     @Query("SELECT c "
         + "FROM Coupon c "
+        + "JOIN FETCH c.sender "
+        + "WHERE c.sender = :member AND c.couponState.couponStatus = :status "
+        + "ORDER BY c.couponState.meetingDate DESC")
+    List<CouponResponse> findAllBySender(@Param("member") Member member,
+                                         @Param("status") CouponStatus couponStatus);
+
+    @Query("SELECT c "
+        + "FROM Coupon c "
         + "JOIN FETCH c.receiver "
         + "WHERE c.receiver = :member "
         + "ORDER BY c.couponState.meetingDate DESC")
     List<Coupon> findAllByReceiver(@Param("member") Member member);
+
+    @Query("SELECT c "
+        + "FROM Coupon c "
+        + "JOIN FETCH c.receiver "
+        + "WHERE c.receiver = :member AND c.couponState.couponStatus = :status "
+        + "ORDER BY c.couponState.meetingDate DESC")
+    List<CouponResponse> findAllByReceiver(@Param("member") Member member,
+                                           @Param("status") CouponStatus couponStatus);
 
     @Query("SELECT c "
         + "FROM Coupon c "
