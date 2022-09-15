218ffc226ae5f4c700527cb0a6df9e06d342098a
junzzi <jjh01027717502@gmail.com>
2022-09-15 20:18:04 +0900

    refactor: 404 페이지에 메인 버튼 추가

--- frontend/src/@pages/404/index.tsx
+++ frontend/src/@pages/404/index.tsx
@@ -1,4 +1,7 @@
+import { Link } from 'react-router-dom';
+
 import PageTemplate from '@/@components/@shared/PageTemplate';
+import { PATH } from '@/Router';
 
 import * as Styled from './style';
 
@@ -9,6 +12,9 @@ const NotFoundPage = () => {
         <img src='/assets/images/404.png' alt='로고' />
         <h2>찾을 수 없는 페이지에요.</h2>
         <h3>요청하신 페이지가 사라졌거나, 잘못된 경로로 이동하셨어요 :)</h3>
+        <Link to={PATH.LANDING} css={Styled.LinkButton} replace>
+          메인으로
+        </Link>
       </Styled.Root>
     </PageTemplate>
   );
