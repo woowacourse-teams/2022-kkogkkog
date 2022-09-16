ef0af927119b373ac40f0ca6654a90f31c5b8a0f
dwl21 <nggus5@naver.com>
2022-09-16 21:50:13 +0900

    refactor: hotfix 하려고 만들었지만 쓸모가 없어진 예외 생성자 제거

--- backend/src/main/java/com/woowacourse/kkogkkog/infrastructure/exception/WoowacoursePostMessageRequestFailedException.java
+++ backend/src/main/java/com/woowacourse/kkogkkog/infrastructure/exception/WoowacoursePostMessageRequestFailedException.java
@@ -2,10 +2,6 @@ package com.woowacourse.kkogkkog.infrastructure.exception;
 
 public class WoowacoursePostMessageRequestFailedException extends RuntimeException {
 
-    public WoowacoursePostMessageRequestFailedException() {
-        super("Woowacourse 워크스페이스의 슬랙 DM 전송 요청에 실패하였습니다.");
-    }
-
     public WoowacoursePostMessageRequestFailedException(String message) {
         super(String.format("Woowacourse 워크스페이스의 슬랙 DM 전송 요청에 실패하였습니다. [%s]", message));
     }
