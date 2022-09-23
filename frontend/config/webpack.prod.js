f5d2e70e5c0f07a31e8758e08dd181570c3bc38a
junzzi <jjh01027717502@gmail.com>
2022-09-23 09:08:49 +0900

    feat: webpack compression 추가

--- frontend/config/webpack.prod.js
+++ frontend/config/webpack.prod.js
@@ -2,8 +2,11 @@ const { merge } = require('webpack-merge');
 
 const common = require('./webpack.common');
 
+const CompressionPlugin = require('compression-webpack-plugin');
+
 require('dotenv').config();
 
 module.exports = merge(common, {
   mode: 'production',
+  plugins: [new CompressionPlugin()],
 });
