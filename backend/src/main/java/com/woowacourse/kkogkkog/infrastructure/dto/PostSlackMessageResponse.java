c75f82afe7115d3a1f6eac2bd6607380eee97e73
LEO <nggus5@naver.com>
2022-09-29 14:56:14 +0900

    refactor: 슬랙 외부 라이브러리 응답 Response 객체들 구현 (#386)
    * feat: openIdConnect response 구현
    
    * feat: 슬랙 알림 response 구현
    
    * refactor: PARAMETERIZED_TYPE_REFERENCE 제거
    
    * refactor: error null 방지 처리와 toString 제거
    
    * refactor: 에러 여부를 응답 객체에게 물어보게 수정 - Code Smell 해결

---
+++ backend/src/main/java/com/woowacourse/kkogkkog/infrastructure/dto/PostSlackMessageResponse.java
@@ -0,0 +1,22 @@
+package com.woowacourse.kkogkkog.infrastructure.dto;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class PostSlackMessageResponse {
+
+    private Boolean ok;
+    private String error = "";
+
+    public PostSlackMessageResponse(Boolean ok, String error) {
+        this.ok = ok;
+        this.error = error;
+    }
+
+    public boolean isError() {
+        return ok == null || !ok;
+    }
+}
---
+++ backend/src/main/java/com/woowacourse/kkogkkog/infrastructure/dto/SlackUserTokenResponse.java
@@ -0,0 +1,28 @@
+package com.woowacourse.kkogkkog.infrastructure.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class SlackUserTokenResponse {
+
+    private Boolean ok;
+
+    @JsonProperty(value = "access_token")
+    private String accessToken;
+
+    private String error = "";
+
+    public SlackUserTokenResponse(Boolean ok, String accessToken, String error) {
+        this.ok = ok;
+        this.accessToken = accessToken;
+        this.error = error;
+    }
+
+    public boolean isError() {
+        return ok == null || !ok;
+    }
+}
