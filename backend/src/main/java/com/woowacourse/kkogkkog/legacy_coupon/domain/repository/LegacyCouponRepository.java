6b934cdfe374578df155d751d15900c194a4b7e2
Jeong Jinwoo <jwjeong96@gmail.com>
2022-09-29 12:05:05 +0900

    refactor: 사용되지 않는 레거시 리포지토리 제거

--- backend/src/main/java/com/woowacourse/kkogkkog/legacy_coupon/domain/repository/LegacyCouponRepository.java
+++
@@ -1,8 +0,0 @@
-package com.woowacourse.kkogkkog.legacy_coupon.domain.repository;
-
-import com.woowacourse.kkogkkog.legacy_coupon.domain.LegacyCoupon;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LegacyCouponRepository extends JpaRepository<LegacyCoupon, Long> {
-
-}
