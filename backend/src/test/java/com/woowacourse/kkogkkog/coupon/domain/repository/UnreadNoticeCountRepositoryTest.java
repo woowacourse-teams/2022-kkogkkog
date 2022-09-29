3b11ce0e4905ac8e0300aba868af85c01452cc4b
Jeong Jinwoo <jwjeong96@gmail.com>
2022-09-29 18:30:54 +0900

    feat: HashMap 기반의 읽지 않은 알림 개수에 대한 캐쉬 저장소 구현

---
+++ backend/src/test/java/com/woowacourse/kkogkkog/coupon/domain/repository/UnreadNoticeCountRepositoryTest.java
@@ -0,0 +1,83 @@
+package com.woowacourse.kkogkkog.coupon.domain.repository;
+
+import static com.woowacourse.kkogkkog.support.fixture.domain.CouponHistoryFixture.DECLINE;
+import static com.woowacourse.kkogkkog.support.fixture.domain.CouponHistoryFixture.INIT;
+import static com.woowacourse.kkogkkog.support.fixture.domain.CouponHistoryFixture.REQUEST;
+import static com.woowacourse.kkogkkog.support.fixture.domain.MemberFixture.RECEIVER;
+import static com.woowacourse.kkogkkog.support.fixture.domain.MemberFixture.SENDER;
+import static com.woowacourse.kkogkkog.support.fixture.domain.WorkspaceFixture.KKOGKKOG;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import com.woowacourse.kkogkkog.coupon.domain.Coupon;
+import com.woowacourse.kkogkkog.member.domain.Member;
+import com.woowacourse.kkogkkog.member.domain.Workspace;
+import com.woowacourse.kkogkkog.member.domain.repository.MemberRepository;
+import com.woowacourse.kkogkkog.member.domain.repository.WorkspaceRepository;
+import com.woowacourse.kkogkkog.support.fixture.domain.CouponFixture;
+import com.woowacourse.kkogkkog.support.repository.RepositoryTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RepositoryTest
+class UnreadNoticeCountRepositoryTest {
+
+    @Autowired
+    private CouponHistoryRepository couponHistoryRepository;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private WorkspaceRepository workspaceRepository;
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    private UnreadNoticeCountCacheRepository unreadNoticeCountCacheRepository;
+
+    private Member sender;
+    private Member receiver;
+
+    @BeforeEach
+    void setUp() {
+        Workspace workspace = workspaceRepository.save(KKOGKKOG.getWorkspace());
+        sender = memberRepository.save(SENDER.getMember(workspace));
+        receiver = memberRepository.save(RECEIVER.getMember(workspace));
+        unreadNoticeCountCacheRepository =
+            new UnreadNoticeCountCacheRepository(couponHistoryRepository);
+    }
+
+    @Nested
+    @DisplayName("get 메서드는")
+    class Get {
+
+        @Test
+        @DisplayName("유효한 캐쉬가 존재하지 않는 경우 최신 데이터를 조회하여 반환한다.")
+        void updateAndReadCache() {
+            Coupon coupon = couponRepository.save(CouponFixture.COFFEE.getCoupon(sender, receiver));
+            couponHistoryRepository.save(INIT.getCouponHistory(sender, coupon));
+            couponHistoryRepository.save(REQUEST.getCouponHistory(receiver, coupon));
+            couponHistoryRepository.save(DECLINE.getCouponHistory(sender, coupon));
+
+            Long unreadCount = unreadNoticeCountCacheRepository.get(receiver);
+            assertThat(unreadCount).isEqualTo(2);
+        }
+
+        @Test
+        @DisplayName("유효한 캐쉬가 이미 존재하는 경우, 해당 데이터를 그대로 반환한다.")
+        void readExistingCache() {
+            Coupon coupon = couponRepository.save(CouponFixture.COFFEE.getCoupon(sender, receiver));
+            couponHistoryRepository.save(INIT.getCouponHistory(sender, coupon));
+            couponHistoryRepository.save(REQUEST.getCouponHistory(receiver, coupon));
+            couponHistoryRepository.save(DECLINE.getCouponHistory(sender, coupon));
+
+            Long actual = unreadNoticeCountCacheRepository.get(receiver);
+            Long expected = unreadNoticeCountCacheRepository.get(receiver);
+
+            assertThat(actual).isEqualTo(expected);
+        }
+    }
+}
