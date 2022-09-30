43fc0392f9d5a9ec09e540959f5d8d2afe1a5b04
gytks4 <gytks4@naver.com>
2022-09-30 17:14:15 +0900

    refactor: SlackLogin 분기처리 제거

--- frontend/src/@pages/slack-redirect/index.tsx
+++ frontend/src/@pages/slack-redirect/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { Navigate, useLocation, useNavigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 import Loading from '@/@components/@shared/Loading';
 import useGetSearchParam from '@/@hooks/@common/useGetSearchParams';
@@ -8,8 +8,6 @@ import { PATH } from '@/Router';
 
 const SlackDirect = () => {
   const navigate = useNavigate();
-
-  const pathname = useLocation().pathname;
 
   const code = useGetSearchParam('code');
 
@@ -36,9 +34,7 @@ const SlackDirect = () => {
       }
     };
 
-    if (pathname === PATH.SLACK_LOGIN_REDIRECT) {
-      slackLoginRedirect();
-    }
+    slackLoginRedirect();
 
     // mutate가 실행된 후 해당 컴포넌트가 리렌더링 되기 때문에 dependency에 loginMutate를 넣으면 무한 렌더링이 발생함.
     // eslint-disable-next-line react-hooks/exhaustive-deps
