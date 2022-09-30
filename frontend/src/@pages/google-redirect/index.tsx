775424beab5bce0f4fee8d63aa155ca2818eb37b
gytks4 <gytks4@naver.com>
2022-09-30 17:51:16 +0900

    feat: GoogleRedirect 페이지 구현

---
+++ frontend/src/@pages/google-redirect/index.tsx
@@ -0,0 +1,46 @@
+import { useEffect } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
+
+import Loading from '@/@components/@shared/Loading';
+import useGetSearchParam from '@/@hooks/@common/useGetSearchParams';
+import { useGoogleOAuthLogin } from '@/@hooks/business/user';
+import { PATH } from '@/Router';
+
+const GoogleDirect = () => {
+  const navigate = useNavigate();
+
+  const code = useGetSearchParam('code');
+
+  const { loginByGoogleOAuth } = useGoogleOAuthLogin();
+
+  useEffect(() => {
+    if (!code) {
+      return;
+    }
+
+    const googleLoginRedirect = async () => {
+      try {
+        const response = await loginByGoogleOAuth(code);
+
+        if (response.isNew) {
+          navigate(PATH.SIGNUP, { state: 'google' });
+        } else {
+          navigate(PATH.MAIN, { replace: true });
+        }
+      } catch (error) {
+        navigate(PATH.MAIN, { replace: true });
+
+        throw error;
+      }
+    };
+
+    googleLoginRedirect();
+
+    // mutate가 실행된 후 해당 컴포넌트가 리렌더링 되기 때문에 dependency에 loginMutate를 넣으면 무한 렌더링이 발생함.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return code ? <Loading /> : <Navigate to={PATH.MAIN} />;
+};
+
+export default GoogleDirect;
