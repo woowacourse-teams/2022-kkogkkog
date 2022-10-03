35776586a6e0cc2c3c8011804f813052500de93c
gytks4 <gytks4@naver.com>
2022-10-03 14:58:53 +0900

    fix: QueryClient를 class 타입으로 병합하기

--- frontend/src/types/react-query.d.ts
+++ frontend/src/types/react-query.d.ts
@@ -2,7 +2,7 @@ import type { QueryKey, SetDataOptions } from 'react-query';
 import type { Updater } from 'react-query/types/core/utils';
 
 declare module 'react-query' {
-  export interface QueryClient {
+  export class QueryClient {
     setQueryData<TData>(
       queryKey: QueryKey,
       updater: Updater<TData | undefined, TData | undefined>,
