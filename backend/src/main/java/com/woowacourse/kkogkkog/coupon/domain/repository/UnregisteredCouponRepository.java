8f7620670a4ac59bfcd921ee94f7895674cfbeb5
dwl21 <nggus5@naver.com>
2022-10-04 18:11:06 +0900

    feat: UnregisteredCoupon를 찾는findAllBySender 메서드 구현

--- backend/src/main/java/com/woowacourse/kkogkkog/coupon/domain/repository/UnregisteredCouponRepository.java
+++ backend/src/main/java/com/woowacourse/kkogkkog/coupon/domain/repository/UnregisteredCouponRepository.java
@@ -1,10 +1,20 @@
 package com.woowacourse.kkogkkog.coupon.domain.repository;
 
 import com.woowacourse.kkogkkog.coupon.domain.UnregisteredCoupon;
+import com.woowacourse.kkogkkog.member.domain.Member;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UnregisteredCouponRepository extends JpaRepository<UnregisteredCoupon, Long> {
 
     Optional<UnregisteredCoupon> findByCouponCode(String couponCode);
+
+    @Query("SELECT c "
+        + "FROM UnregisteredCoupon c "
+        + "JOIN FETCH c.sender "
+        + "WHERE c.sender = :member")
+    List<UnregisteredCoupon> findAllBySender(@Param("member") Member member);
 }
