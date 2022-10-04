f20e401b5b36522d1c458ca44193fabae94fbe9b
dwl21 <nggus5@naver.com>
2022-10-04 17:55:00 +0900

    feat: UnregisteredCoupon 을 couponCode 기반으로 찾는 Spring data jpa 메서드 구현

--- backend/src/main/java/com/woowacourse/kkogkkog/coupon/domain/repository/UnregisteredCouponRepository.java
+++ backend/src/main/java/com/woowacourse/kkogkkog/coupon/domain/repository/UnregisteredCouponRepository.java
@@ -1,7 +1,10 @@
 package com.woowacourse.kkogkkog.coupon.domain.repository;
 
 import com.woowacourse.kkogkkog.coupon.domain.UnregisteredCoupon;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UnregisteredCouponRepository extends JpaRepository<UnregisteredCoupon, Long> {
+
+    Optional<UnregisteredCoupon> findByCouponCode(String couponCode);
 }
