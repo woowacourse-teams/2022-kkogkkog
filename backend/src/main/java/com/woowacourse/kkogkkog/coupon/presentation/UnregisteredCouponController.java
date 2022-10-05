12a5c32c805eb2a320d76e55ad4c513bd68a747a
dwl21 <nggus5@naver.com>
2022-10-05 13:27:59 +0900

    feat: 단일 미등록 쿠폰 아이디 상세 조회 API 구현

--- backend/src/main/java/com/woowacourse/kkogkkog/coupon/presentation/UnregisteredCouponController.java
+++ backend/src/main/java/com/woowacourse/kkogkkog/coupon/presentation/UnregisteredCouponController.java
@@ -2,6 +2,7 @@ package com.woowacourse.kkogkkog.coupon.presentation;
 
 import com.woowacourse.kkogkkog.common.presentation.LoginMemberId;
 import com.woowacourse.kkogkkog.coupon.application.UnregisteredCouponService;
+import com.woowacourse.kkogkkog.coupon.application.dto.UnregisteredCouponDetailResponse;
 import com.woowacourse.kkogkkog.coupon.application.dto.UnregisteredCouponResponse;
 import com.woowacourse.kkogkkog.coupon.presentation.dto.CouponsResponse;
 import com.woowacourse.kkogkkog.coupon.presentation.dto.UnregisteredCouponCreateRequest;
@@ -9,6 +10,7 @@ import com.woowacourse.kkogkkog.coupon.presentation.dto.UnregisteredCouponsRespo
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,18 +26,26 @@ public class UnregisteredCouponController {
         this.unregisteredCouponService = unregisteredCouponService;
     }
 
+    @GetMapping
+    public ResponseEntity<UnregisteredCouponsResponse> showAll(@LoginMemberId Long loginMemberId) {
+        List<UnregisteredCouponResponse> unregisteredCoupons = unregisteredCouponService.findAllBySender(
+            loginMemberId);
+        return ResponseEntity.ok(new UnregisteredCouponsResponse(unregisteredCoupons));
+    }
+
+    @GetMapping("/{unregisteredCouponId}")
+    public ResponseEntity<UnregisteredCouponDetailResponse> show(@LoginMemberId Long loginMemberId,
+                                                                 @PathVariable Long unregisteredCouponId) {
+        UnregisteredCouponDetailResponse couponDetailResponse = unregisteredCouponService.findById(
+            loginMemberId, unregisteredCouponId);
+        return ResponseEntity.ok(couponDetailResponse);
+    }
+
     @PostMapping
     public ResponseEntity<UnregisteredCouponsResponse> save(@LoginMemberId Long loginMemberId,
                                                             @RequestBody UnregisteredCouponCreateRequest request) {
         List<UnregisteredCouponResponse> responses = unregisteredCouponService.save(
             request.toUnregisteredCouponSaveRequest(loginMemberId));
         return ResponseEntity.created(null).body(new UnregisteredCouponsResponse(responses));
-    }
-
-    @GetMapping
-    public ResponseEntity<UnregisteredCouponsResponse> show(@LoginMemberId Long loginMemberId) {
-        List<UnregisteredCouponResponse> unregisteredCoupons = unregisteredCouponService.findAllBySender(
-            loginMemberId);
-        return ResponseEntity.ok(new UnregisteredCouponsResponse(unregisteredCoupons));
     }
 }
--- backend/src/test/java/com/woowacourse/kkogkkog/acceptance/UnregisteredCouponAcceptanceTest.java
+++ backend/src/test/java/com/woowacourse/kkogkkog/acceptance/UnregisteredCouponAcceptanceTest.java
@@ -1,17 +1,26 @@
 package com.woowacourse.kkogkkog.acceptance;
 
 import static com.woowacourse.kkogkkog.acceptance.AuthAcceptanceTest.회원가입을_하고;
+import static com.woowacourse.kkogkkog.acceptance.support.AcceptanceContext.invokeGet;
 import static com.woowacourse.kkogkkog.acceptance.support.AcceptanceContext.invokeGetWithToken;
 import static com.woowacourse.kkogkkog.acceptance.support.AcceptanceContext.invokePostWithToken;
+import static com.woowacourse.kkogkkog.support.fixture.domain.MemberFixture.AUTHOR;
 import static com.woowacourse.kkogkkog.support.fixture.domain.MemberFixture.JEONG;
+import static com.woowacourse.kkogkkog.support.fixture.domain.MemberFixture.LEO;
+import static com.woowacourse.kkogkkog.support.fixture.dto.CouponDtoFixture.COFFEE_쿠폰_생성_요청;
 import static com.woowacourse.kkogkkog.support.fixture.dto.UnregisteredCouponDtoFixture.미등록_COFFEE_쿠폰_생성_요청;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import com.woowacourse.kkogkkog.acceptance.support.AcceptanceTest;
+import com.woowacourse.kkogkkog.coupon.application.dto.CouponDetailResponse;
+import com.woowacourse.kkogkkog.coupon.application.dto.UnregisteredCouponDetailResponse;
+import com.woowacourse.kkogkkog.coupon.domain.CouponStatus;
+import com.woowacourse.kkogkkog.coupon.presentation.dto.CouponsResponse;
 import com.woowacourse.kkogkkog.coupon.presentation.dto.UnregisteredCouponsResponse;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
@@ -44,16 +53,38 @@ public class UnregisteredCouponAcceptanceTest extends AcceptanceTest {
         );
     }
 
+    @Test
+    void 단일_미등록_쿠폰을_아이디로_상세_조회할_수_있다() {
+        String senderToken = 회원가입을_하고(JEONG.getMember());
+        UnregisteredCouponsResponse response = 미등록_쿠폰_생성을_요청하고(senderToken,
+            미등록_COFFEE_쿠폰_생성_요청(1));
+        Long unregisteredCouponId = response.getData().get(0).getId();
+
+        var extract = 아이디로_회원의_단일_미등록_쿠폰_상세정보를_조회한다(senderToken, unregisteredCouponId);
+
+        UnregisteredCouponDetailResponse actual = extract.as(UnregisteredCouponDetailResponse.class);
+        assertAll(
+            () -> assertThat(extract.statusCode()).isEqualTo(HttpStatus.OK.value()),
+            () -> assertThat(actual.getId()).isEqualTo(unregisteredCouponId)
+        );
+    }
+
     static ExtractableResponse<Response> 미등록_쿠폰_생성을_요청한다(String token, Object data) {
         return invokePostWithToken("/api/v2/coupons/unregistered", token, data);
     }
 
     static UnregisteredCouponsResponse 미등록_쿠폰_생성을_요청하고(String token, Object data) {
-        ExtractableResponse<Response> response = invokePostWithToken("/api/v2/coupons/unregistered", token, data);
+        ExtractableResponse<Response> response = invokePostWithToken("/api/v2/coupons/unregistered",
+            token, data);
         return response.as(UnregisteredCouponsResponse.class);
     }
 
     static ExtractableResponse<Response> 회원의_미동록_쿠폰_목록들을_조회한다(String token) {
         return invokeGetWithToken("/api/v2/coupons/unregistered", token);
+    }
+
+    static ExtractableResponse<Response> 아이디로_회원의_미등록_단일쿠폰_상세정보를_조회한다(String token,
+                                                                      Long unRegisteredCouponId) {
+        return invokeGetWithToken("/api/v2/coupons/unregistered/" + unRegisteredCouponId, token);
     }
 }
