968f265bb9634d28a9ca4c6b5a0eb7ff9a883283
gytks4 <gytks4@naver.com>
2022-10-06 21:29:39 +0900

    feat: UnregisteredCouponItem 컴포넌트 구현

---
+++ frontend/src/@components/unregistered-coupon/UnregisteredCouponItem/index.tsx
@@ -0,0 +1,44 @@
+import { MouseEventHandler } from 'react';
+
+import UnregisteredCouponStatus from '@/@components/unregistered-coupon/UnregisteredCouponStatus';
+import { THUMBNAIL } from '@/types/coupon/client';
+import { UnregisteredCoupon } from '@/types/unregistered-coupon/client';
+
+import * as Styled from '../../coupon/CouponItem/big.style';
+
+export interface UnregisteredCouponItemProps extends UnregisteredCoupon {
+  className?: string;
+  onClick?: MouseEventHandler<HTMLDivElement>;
+}
+
+const UnregisteredCouponItem = (props: UnregisteredCouponItemProps) => {
+  const { className, onClick, ...coupon } = props;
+
+  const { receiver, couponTag, unregisteredCouponStatus, couponMessage, thumbnail } = {
+    ...coupon,
+    thumbnail: THUMBNAIL[coupon.couponType],
+  };
+
+  return (
+    <Styled.Root className={className} hasCursor={!!onClick} onClick={onClick}>
+      <Styled.CouponPropertyContainer>
+        <UnregisteredCouponStatus status={unregisteredCouponStatus} />
+
+        <Styled.ImageInner>
+          <img src={thumbnail} alt='쿠폰' width={44} height={44} />
+        </Styled.ImageInner>
+      </Styled.CouponPropertyContainer>
+      <Styled.TextContainer>
+        <Styled.Top>
+          <Styled.Member>
+            <Styled.English>To</Styled.English> {receiver?.nickname ?? '?'}
+          </Styled.Member>
+        </Styled.Top>
+        <Styled.Message>{couponMessage}</Styled.Message>
+        <Styled.Hashtag>#{couponTag}</Styled.Hashtag>
+      </Styled.TextContainer>
+    </Styled.Root>
+  );
+};
+
+export default UnregisteredCouponItem;
