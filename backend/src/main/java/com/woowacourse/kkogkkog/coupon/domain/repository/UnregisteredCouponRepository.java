b7853c6f6c1dd93c982b74c10c66620fe4aeed48
dwl21 <nggus5@naver.com>
2022-10-07 14:41:40 +0900

    feat: 미등록 쿠폰 상태별 조회 쿼리 작성

--- backend/src/main/java/com/woowacourse/kkogkkog/coupon/domain/repository/UnregisteredCouponRepository.java
+++ backend/src/main/java/com/woowacourse/kkogkkog/coupon/domain/repository/UnregisteredCouponRepository.java
@@ -1,6 +1,7 @@
 package com.woowacourse.kkogkkog.coupon.domain.repository;
 
 import com.woowacourse.kkogkkog.coupon.domain.UnregisteredCoupon;
+import com.woowacourse.kkogkkog.coupon.domain.UnregisteredCouponStatus;
 import com.woowacourse.kkogkkog.member.domain.Member;
 import java.util.List;
 import java.util.Optional;
@@ -17,4 +18,11 @@ public interface UnregisteredCouponRepository extends JpaRepository<Unregistered
         + "JOIN FETCH c.sender "
         + "WHERE c.sender = :member")
     List<UnregisteredCoupon> findAllBySender(@Param("member") Member member);
+
+    @Query("SELECT c "
+        + "FROM UnregisteredCoupon c "
+        + "JOIN FETCH c.sender "
+        + "WHERE c.sender = :member AND c.unregisteredCouponStatus = :status")
+    List<UnregisteredCoupon> findAllBySender(@Param("member") Member member,
+                                 @Param("status") UnregisteredCouponStatus couponStatus);
 }
