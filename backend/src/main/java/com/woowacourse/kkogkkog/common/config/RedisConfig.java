92d308c414cb41be03581ce0f695e51153ff8e0a
Jeong Jinwoo <jwjeong96@gmail.com>
2022-10-11 20:31:30 +0900

    feat: Redis 연동시 비밀번호 설정하도록 설정 추가

--- backend/src/main/java/com/woowacourse/kkogkkog/common/config/RedisConfig.java
+++ backend/src/main/java/com/woowacourse/kkogkkog/common/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -12,16 +13,21 @@ public class RedisConfig {
 
     private final String redisHost;
     private final int redisPort;
+    private final String redisPassword;
 
     public RedisConfig(@Value("${spring.redis.host}") final String redisHost,
-                       @Value("${spring.redis.port}") final int redisPort) {
+                       @Value("${spring.redis.port}") final int redisPort,
+                       @Value("${spring.redis.password}") final String redisPassword) {
         this.redisHost = redisHost;
         this.redisPort = redisPort;
+        this.redisPassword = redisPassword;
     }
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(redisHost, redisPort);
+        final var redisConfiguration = new RedisStandaloneConfiguration(redisHost, redisPort);
+        redisConfiguration.setPassword(redisPassword);
+        return new LettuceConnectionFactory(redisConfiguration);
     }
 
     @Bean
