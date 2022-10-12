c724c60a5268968a2d84659ce7f39dc120fd04f8
gytks4 <gytks4@naver.com>
2022-10-12 22:28:35 +0900

    feat: 미등록 쿠폰 기간 만료 및 등록 완료 페이지 스타일링

--- frontend/src/@pages/unregistered-coupon-register/ExpiredRegisteredCouponPage/index.tsx
+++ frontend/src/@pages/unregistered-coupon-register/ExpiredRegisteredCouponPage/index.tsx
@@ -1,5 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+
+import Button from '@/@components/@shared/Button';
+import PageTemplate from '@/@components/@shared/PageTemplate';
+import landingLogoImage from '@/assets/images/landing_logo.png';
+import { PATH } from '@/Router';
+
+import * as Styled from './style';
+
 const ExpiredRegisteredCouponPage = () => {
-  return <div>만료 기간이 지나 쿠폰을 등록할 수 없습니다.</div>;
+  const navigate = useNavigate();
+
+  return (
+    <PageTemplate title='꼭꼭' hasHeader={false}>
+      <Styled.Root>
+        <img src={landingLogoImage} alt='로고' width='86' />
+        <Styled.Description>해당 쿠폰은 만료 기간(7일)이 지났습니다.</Styled.Description>
+        <Styled.ButtonInner>
+          <Button onClick={() => navigate(PATH.MAIN)}>홈으로 가기</Button>
+        </Styled.ButtonInner>
+      </Styled.Root>
+    </PageTemplate>
+  );
 };
 
 export default ExpiredRegisteredCouponPage;
--- frontend/src/@pages/unregistered-coupon-register/RegisteredRegisteredCouponPage/index.tsx
+++ frontend/src/@pages/unregistered-coupon-register/RegisteredRegisteredCouponPage/index.tsx
@@ -1,5 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+
+import Button from '@/@components/@shared/Button';
+import PageTemplate from '@/@components/@shared/PageTemplate';
+import landingLogoImage from '@/assets/images/landing_logo.png';
+import { PATH } from '@/Router';
+
+import * as Styled from './style';
+
 const RegisteredRegisteredCouponPage = () => {
-  return <div>이미 등록된 쿠폰입니다.</div>;
+  const navigate = useNavigate();
+
+  return (
+    <PageTemplate title='꼭꼭' hasHeader={false}>
+      <Styled.Root>
+        <img src={landingLogoImage} alt='로고' width='86' />
+        <Styled.Description>해당 쿠폰은 이미 등록되었습니다.</Styled.Description>
+        <Styled.ButtonInner>
+          <Button onClick={() => navigate(PATH.MAIN)}>홈으로 가기</Button>
+        </Styled.ButtonInner>
+      </Styled.Root>
+    </PageTemplate>
+  );
 };
 
 export default RegisteredRegisteredCouponPage;
