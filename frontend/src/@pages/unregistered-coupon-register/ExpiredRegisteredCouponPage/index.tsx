c06639e5f09c11b640b02736f3576b6e2b45ecdf
gytks4 <gytks4@naver.com>
2022-10-12 21:50:06 +0900

    feat: 미등록 쿠폰 만료 페이지, 등록 완료 페이지 구현

---
+++ frontend/src/@pages/unregistered-coupon-register/ExpiredRegisteredCouponPage/index.tsx
@@ -0,0 +1,5 @@
+const ExpiredRegisteredCouponPage = () => {
+  return <div>만료 기간이 지나 쿠폰을 등록할 수 없습니다.</div>;
+};
+
+export default ExpiredRegisteredCouponPage;
---
+++ frontend/src/@pages/unregistered-coupon-register/RegisteredRegisteredCouponPage/index.tsx
@@ -0,0 +1,5 @@
+const RegisteredRegisteredCouponPage = () => {
+  return <div>이미 등록된 쿠폰입니다.</div>;
+};
+
+export default RegisteredRegisteredCouponPage;
