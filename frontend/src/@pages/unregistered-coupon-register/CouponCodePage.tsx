e0d1d6bfbad950fe1054ff595cc90c11b605575d
gytks4 <gytks4@naver.com>
2022-10-12 21:49:33 +0900

    feat: 쿠폰 등록 페이지 구현

---
+++ frontend/src/@pages/unregistered-coupon-register/CouponCodePage.tsx
@@ -0,0 +1,40 @@
+import useGetSearchParam from '@/@hooks/@common/useGetSearchParams';
+import { useFetchUnregisteredCouponByCode } from '@/@hooks/@queries/unregistered-coupon';
+
+import NotFoundPage from '../404';
+import ExpiredRegisteredCouponPage from './ExpiredRegisteredCouponPage';
+import IssuedRegisteredCouponPage from './IssuedRegisteredCouponPage';
+import RegisteredRegisteredCouponPage from './RegisteredRegisteredCouponPage';
+
+const UnregisteredCouponCodePage = () => {
+  const couponCode = useGetSearchParam('couponCode');
+
+  // @TODO: null 처리
+  const { unregisteredCoupon } = useFetchUnregisteredCouponByCode(couponCode ?? '');
+
+  if (couponCode === null) {
+    return <NotFoundPage />;
+  }
+
+  if (unregisteredCoupon === undefined) {
+    return <NotFoundPage />;
+  }
+
+  if (unregisteredCoupon.unregisteredCouponStatus === 'ISSUED') {
+    return (
+      <IssuedRegisteredCouponPage unregisteredCoupon={unregisteredCoupon} couponCode={couponCode} />
+    );
+  }
+
+  if (unregisteredCoupon.unregisteredCouponStatus === 'REGISTERED') {
+    return <RegisteredRegisteredCouponPage />;
+  }
+
+  if (unregisteredCoupon.unregisteredCouponStatus === 'EXPIRED') {
+    return <ExpiredRegisteredCouponPage />;
+  }
+
+  return <NotFoundPage />;
+};
+
+export default UnregisteredCouponCodePage;
