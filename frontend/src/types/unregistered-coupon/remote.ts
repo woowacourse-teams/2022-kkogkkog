61196fe81f9b49579a7f6dccafc63701b30196bb
gytks4 <gytks4@naver.com>
2022-10-12 21:42:34 +0900

    feat: 미등록 쿠폰 등록 요청 API 타입 정의

--- frontend/src/types/unregistered-coupon/remote.ts
+++ frontend/src/types/unregistered-coupon/remote.ts
@@ -2,23 +2,29 @@ import { COUPON_ENG_TYPE, COUPON_HASHTAGS } from '@/types/coupon/client';
 import { Member } from '@/types/user/client';
 import { YYYYMMDDhhmmss } from '@/types/utils';
 
-import { UNREGISTERED_COUPON_STATUS, UnregisteredCoupon } from './client';
+import { UNREGISTERED_COUPON_STATUS } from './client';
 
 export interface UnregisteredCouponListByStatusRequest {
   type: UNREGISTERED_COUPON_STATUS;
 }
 
 export interface UnregisteredCouponListResponse {
-  data: UnregisteredCoupon[];
+  data: UnregisteredCouponResponse[];
 }
 
 export interface UnregisteredCouponResponse {
   id: number;
   couponCode: string;
+  couponId: number | null;
   sender: Member;
+  receiver: Member | null;
   couponTag: COUPON_HASHTAGS;
   couponMessage: string;
   couponType: COUPON_ENG_TYPE;
   unregisteredCouponStatus: UNREGISTERED_COUPON_STATUS;
   createdTime: YYYYMMDDhhmmss;
+}
+
+export interface RegisterUnregisteredCouponRequest {
+  couponCode: string;
 }
