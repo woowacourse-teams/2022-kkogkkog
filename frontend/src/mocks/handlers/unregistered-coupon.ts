e0dcc9f0abf56edc7b01596a4b4d0de0227759b2
gytks4 <gytks4@naver.com>
2022-10-12 21:44:05 +0900

    feat: 쿠폰 코드로 미등록 쿠폰 단건 조회, 쿠폰 등록 msw 모킹

--- frontend/src/mocks/handlers/unregistered-coupon.ts
+++ frontend/src/mocks/handlers/unregistered-coupon.ts
@@ -2,6 +2,7 @@ import { rest } from 'msw';
 
 import { BASE_URL } from '@/apis';
 import { UNREGISTERED_COUPON_STATUS } from '@/types/unregistered-coupon/client';
+import { RegisterUnregisteredCouponRequest } from '@/types/unregistered-coupon/remote';
 
 import unregisteredCouponMock from '../fixtures/unregistered-coupon';
 
@@ -22,6 +23,47 @@ export const unregisteredCouponHandler = [
 
     try {
       const coupon = unregisteredCouponMock.findUnregisteredCoupon(Number(unregisteredCouponId));
+
+      return res(ctx.status(200), ctx.json(coupon));
+    } catch ({ message }) {
+      return res(ctx.status(400), ctx.json({ message }));
+    }
+  }),
+
+  // @TODO: coupon/:couponId 가 먼저 인식되는 현상 해결
+  rest.get(`${BASE_URL}/coupons/unregistered`, (req, res, ctx) => {
+    const couponCode = req.url.searchParams.get('couponCode');
+
+    if (!couponCode) {
+      return res(ctx.status(400), ctx.json({ message: '해당 쿠폰이 없습니다.' }));
+    }
+
+    try {
+      const coupon = unregisteredCouponMock.findUnregisteredCouponByCode(couponCode);
+
+      return res(ctx.status(200), ctx.json(coupon));
+    } catch ({ message }) {
+      return res(ctx.status(400), ctx.json({ message }));
+    }
+  }),
+
+  rest.post<RegisterUnregisteredCouponRequest>(`${BASE_URL}/coupons/code`, (req, res, ctx) => {
+    const {
+      body: { couponCode },
+    } = req;
+
+    try {
+      const coupon = unregisteredCouponMock.findUnregisteredCouponByCode(couponCode);
+
+      if (coupon.unregisteredCouponStatus !== 'ISSUED') {
+        throw new Error('유효하지 않은 쿠폰입니다.');
+      }
+
+      unregisteredCouponMock.current = unregisteredCouponMock.current.map(coupon =>
+        coupon.unregisteredCouponStatus === 'ISSUED'
+          ? { ...coupon, unregisteredCouponStatus: 'REGISTERED' }
+          : coupon
+      );
 
       return res(ctx.status(200), ctx.json(coupon));
     } catch ({ message }) {
