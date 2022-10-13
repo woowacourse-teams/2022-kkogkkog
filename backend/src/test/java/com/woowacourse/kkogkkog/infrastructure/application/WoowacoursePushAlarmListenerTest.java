b30da8de2d409bfde7b7a873afbc5296dd26b789
Hyunta <mohai2618@gmail.com>
2022-10-13 14:29:26 +0900

    fix: 깨지는 테스트 수정
    - 비동기 테스트여서 timeOut을 걸어서 해결함

--- backend/src/test/java/com/woowacourse/kkogkkog/infrastructure/application/WoowacoursePushAlarmListenerTest.java
+++ backend/src/test/java/com/woowacourse/kkogkkog/infrastructure/application/WoowacoursePushAlarmListenerTest.java
@@ -1,13 +1,14 @@
 package com.woowacourse.kkogkkog.infrastructure.application;
 
 import static com.woowacourse.kkogkkog.support.fixture.domain.CouponFixture.COFFEE;
+import static com.woowacourse.kkogkkog.support.fixture.dto.CouponDtoFixture.COFFEE_쿠폰_저장_요청;
 import static com.woowacourse.kkogkkog.support.fixture.dto.CouponDtoFixture.쿠폰_상태_변경_요청;
 import static org.mockito.BDDMockito.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.verify;
 
 import com.woowacourse.kkogkkog.coupon.application.CouponService;
-import com.woowacourse.kkogkkog.coupon.application.dto.CouponStatusRequest;
 import com.woowacourse.kkogkkog.coupon.domain.Coupon;
 import com.woowacourse.kkogkkog.coupon.domain.repository.CouponRepository;
 import com.woowacourse.kkogkkog.infrastructure.domain.WoowacourseUserRepository;
@@ -18,16 +19,14 @@ import com.woowacourse.kkogkkog.member.domain.repository.WorkspaceRepository;
 import com.woowacourse.kkogkkog.support.application.ApplicationTest;
 import com.woowacourse.kkogkkog.support.fixture.domain.MemberFixture;
 import com.woowacourse.kkogkkog.support.fixture.domain.WorkspaceFixture;
-import com.woowacourse.kkogkkog.support.fixture.dto.CouponDtoFixture;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -45,7 +44,7 @@ public class WoowacoursePushAlarmListenerTest {
     private WorkspaceRepository workspaceRepository;
     @Autowired
     private CouponRepository couponRepository;
-    @Mock
+    @MockBean
     private WoowacoursePushAlarmClient woowacoursePushAlarmClient;
     @MockBean
     private WoowacourseUserRepository woowacourseUserRepository;
@@ -59,54 +58,41 @@ public class WoowacoursePushAlarmListenerTest {
 
         @BeforeEach
         void setUp() {
-            Workspace workspace = workspaceRepository.save(
-                WorkspaceFixture.WOOWACOURSE.getWorkspace(1L));
+            Workspace workspace = workspaceRepository.save(WorkspaceFixture.WOOWACOURSE.getWorkspace(1L));
             sender = memberRepository.save(MemberFixture.SENDER.getMember(workspace));
             receiver = memberRepository.save(MemberFixture.RECEIVER.getMember(workspace));
         }
 
-        /**
-         * Mockito 관련 문제가 발생하고 있습니다. 이것저것 해봤는데 해결이 안되네요.
-         * Wanted but not invoked:
-         * woowacoursePushAlarmClient.requestPushAlarm(
-         *     <any string>,
-         *     <any string>
-         * );
-         */
-//        @Test
-//        @DisplayName("우테코 회원의 쿠폰을 생성할 때, woowacourse 워크스페이스로 슬랙 푸시 알림을 보낸다.")
-//        void success_couponSave() {
-//            //given
-//            given(woowacourseUserRepository.contains(anyString())).willReturn(true);
-//            given(woowacourseUserRepository.getUserId(anyString())).willReturn("userId");
-//
-//            //when
-//            couponService.save(CouponDtoFixture.COFFEE_쿠폰_저장_요청(sender.getId(), List.of(receiver.getId())));
-//
-//            //then
-//            verify(woowacoursePushAlarmClient).requestPushAlarm(anyString(), anyString());
-//        }
+        @Test
+        @DisplayName("우테코 회원의 쿠폰을 생성할 때, woowacourse 워크스페이스로 슬랙 푸시 알림을 보낸다.")
+        void success_couponSave() {
+            //given
+            given(woowacourseUserRepository.contains(anyString())).willReturn(true);
+            given(woowacourseUserRepository.getUserId(anyString())).willReturn("userId");
 
-        /**
-         * Mockito 관련 문제가 발생하고 있습니다. 이것저것 해봤는데 해결이 안되네요.
-         * Wanted but not invoked:
-         * woowacoursePushAlarmClient.requestPushAlarm(
-         *     <any string>,
-         *     <any string>
-         * );
-         */
-//        @Test
-//        @DisplayName("쿠폰 상태를 변경할 때, woowacourse 워크스페이스로 슬랙 푸시 알림을 보낸다.")
-//        void success_couponUpdate() {
-//            //given
-//            Coupon coupon = couponRepository.save(COFFEE.getCoupon(sender, receiver));
-//            CouponStatusRequest couponStatusRequest = 쿠폰_상태_변경_요청(receiver.getId(), coupon.getId(), "REQUEST", LocalDateTime.now(), null);
-//
-//            //when
-//            couponService.updateStatus(couponStatusRequest);
-//
-//            //then
-//            verify(woowacoursePushAlarmClient).requestPushAlarm(anyString(), anyString());
-//        }
+            //when
+            couponService.save(COFFEE_쿠폰_저장_요청(sender.getId(), List.of(receiver.getId())));
+
+            //then
+            verify(woowacoursePushAlarmClient, timeout(50))
+                .requestPushAlarm(anyString(), anyString());
+        }
+
+        @RepeatedTest(1000)
+        @DisplayName("쿠폰 상태를 변경할 때, woowacourse 워크스페이스로 슬랙 푸시 알림을 보낸다.")
+        void success_couponUpdate() {
+            //given
+            Coupon coupon = couponRepository.save(COFFEE.getCoupon(sender, receiver));
+            given(woowacourseUserRepository.contains(anyString())).willReturn(true);
+            given(woowacourseUserRepository.getUserId(anyString())).willReturn("userId");
+
+            //when
+            couponService.updateStatus(
+                쿠폰_상태_변경_요청(receiver.getId(), coupon.getId(), "REQUEST", LocalDateTime.now(), null));
+
+            //then
+            verify(woowacoursePushAlarmClient, timeout(50))
+                .requestPushAlarm(anyString(), anyString());
+        }
     }
 }
