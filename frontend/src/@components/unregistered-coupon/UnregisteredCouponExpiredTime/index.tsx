3bda9b15abf020e98293049a4d2032764dc7a8cd
junzzi <jjh01027717502@gmail.com>
2022-10-14 15:39:06 +0900

    refactor: DownCount 컴포넌트로 분리

--- frontend/src/@components/unregistered-coupon/UnregisteredCouponExpiredTime/index.tsx
+++ frontend/src/@components/unregistered-coupon/UnregisteredCouponExpiredTime/index.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
+import DownCount from '@/@components/@shared/DownCount';
 import UpCount from '@/@components/@shared/UpCount';
 import { EXPIRATION_PERIOD } from '@/constants/unregisteredCoupon';
 import { YYYYMMDDhhmmss } from '@/types/utils';
-import { computeDayHourMinSecByMS, computeExpiredTime } from '@/utils/time';
+import { computeDayHourMinSecByMS, computeExpiredTimeByPeriodMS } from '@/utils/time';
 
 import * as Styled from './style';
 
@@ -13,25 +14,11 @@ interface UnregisteredCouponExpiredTimeProps {
 const UnregisteredCouponExpiredTime = (props: UnregisteredCouponExpiredTimeProps) => {
   const { createdTime } = props;
 
-  const expiredTimeMS = computeExpiredTime(createdTime, EXPIRATION_PERIOD);
+  const expiredTimeMS = computeExpiredTimeByPeriodMS(createdTime, EXPIRATION_PERIOD);
 
-  const [remainingTime, setRemainingTime] = useState(expiredTimeMS - Date.now());
+  const [remainingTime] = useState(expiredTimeMS - Date.now());
 
-  useEffect(() => {
-    const remainingTimeSetter = (prevSecond: number, timestamp: number) => {
-      const currentSecond = Math.floor(timestamp / 1000);
-
-      if (prevSecond < currentSecond) {
-        setRemainingTime(prev => prev - 1000);
-      }
-
-      requestAnimationFrame(timestamp => remainingTimeSetter(currentSecond, timestamp));
-    };
-
-    requestAnimationFrame(timestamp => remainingTimeSetter(0, timestamp));
-  }, []);
-
-  const { day, hour, min, sec } = computeDayHourMinSecByMS(remainingTime);
+  const { day, hour, min } = computeDayHourMinSecByMS(remainingTime);
 
   return (
     <Styled.Root>
@@ -44,7 +31,9 @@ const UnregisteredCouponExpiredTime = (props: UnregisteredCouponExpiredTimeProps
       <UpCount limit={min} duration={3000}>
         분
       </UpCount>
-      <Styled.SecContainer>{sec}초</Styled.SecContainer>
+      <DownCount start={remainingTime} decreaseNumber={1000}>
+        초
+      </DownCount>
     </Styled.Root>
   );
 };
