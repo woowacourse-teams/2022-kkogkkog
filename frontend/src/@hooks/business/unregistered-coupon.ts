7b2ee9851300cf321c15c8348463b1ed8b1a0772
gytks4 <gytks4@naver.com>
2022-10-17 14:14:49 +0900

    feat: 미등록 쿠폰 생성 비즈니스 로직 훅 분리

--- frontend/src/@hooks/business/unregistered-coupon.ts
+++ frontend/src/@hooks/business/unregistered-coupon.ts
@@ -1,6 +1,31 @@
-import { RegisterUnregisteredCouponRequest } from '@/types/unregistered-coupon/remote';
+import {
+  CreateUnregisteredCouponRequest,
+  RegisterUnregisteredCouponRequest,
+} from '@/types/unregistered-coupon/remote';
 
-import { useRegisteredUnregisteredCouponMutation } from '../@queries/unregistered-coupon';
+import { useToast } from '../@common/useToast';
+import {
+  useCreateUnregisteredCouponMutation,
+  useRegisteredUnregisteredCouponMutation,
+} from '../@queries/unregistered-coupon';
+
+export const useCreateUnregisteredCoupon = () => {
+  const { displayMessage } = useToast();
+
+  const { mutateAsync } = useCreateUnregisteredCouponMutation();
+
+  const createUnregisteredCoupon = async (body: CreateUnregisteredCouponRequest) => {
+    const { data } = await mutateAsync(body, {
+      onSuccess() {
+        displayMessage('쿠폰을 생성했어요', false);
+      },
+    });
+
+    return data;
+  };
+
+  return { createUnregisteredCoupon };
+};
 
 export const useRegisteredUnregisteredCoupon = ({
   couponCode,
