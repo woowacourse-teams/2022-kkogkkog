e63222cdfde52857fd8b0e2bbff1ea2fd724e200
junzzi <jjh01027717502@gmail.com>
2022-10-17 15:44:21 +0900

    feat: UnregisteredCouponExpiredTime 컴포넌트 스타일링 및 텍스트 추가

--- frontend/src/@components/unregistered-coupon/UnregisteredCouponExpiredTime/style.tsx
+++ frontend/src/@components/unregistered-coupon/UnregisteredCouponExpiredTime/style.tsx
@@ -1,11 +1,28 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 
-export const Root = styled.div`
+export const Root = styled.div``;
+
+export const TimeContainer = styled.div`
   display: flex;
 
   justify-content: center;
   align-items: center;
   gap: 10px;
+
+  font-size: 24px;
+  font-weight: bold;
+
+  ${({ theme }) => css`
+    color: ${theme.colors.primary_500};
+  `}
 `;
 
-export const SecContainer = styled.div``;
+export const Text = styled.div`
+  text-align: center;
+  font-size: 16px;
+
+  ${({ theme }) => css`
+    color: ${theme.colors.primary_500}90;
+  `}
+`;
