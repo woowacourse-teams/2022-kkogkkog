3934f099b33dee2c44bcfdf4302c4ef18ed9a0bd
juunzzi <78349600+juunzzi@users.noreply.github.com>
2022-10-18 16:22:34 +0900

    refactor: msw 변경된 명세에 맞게 대응 (#461)
    * refactor: user mocked API 개발
    
    * refactor: user fixture 수정
    
    * refactor: Coupon Mock API 개발
    
    * refactor: 함수 분리 및 타이핑 추가
    
    * refactor: error 서버와 동기화, type -> interface로 수정
    
    * refactor: 기존 타입을 재활용
    
    * refactor: HTTP Method 수정, 로직 일부 수정

--- frontend/src/mocks/fixtures/coupon.ts
+++ frontend/src/mocks/fixtures/coupon.ts
@@ -1,163 +1,212 @@
-import { COUPON_EVENT, COUPON_STATUS } from '@/types/coupon/client';
-import { UserResponse } from '@/types/user/remote';
+import { Coupon, COUPON_EVENT, COUPON_STATUS } from '@/types/coupon/client';
+import { CouponDetailResponse } from '@/types/coupon/remote';
+import { YYYYMMDDhhmmss } from '@/types/utils';
 
-export default {
+interface FixtureType {
+  current: CouponDetailResponse[];
+  findAcceptedCouponList: () => Coupon[];
+  findReceivedCouponList: () => Coupon[];
+  findSentCouponList: () => Coupon[];
+  findSentCouponListByStatus: (status: COUPON_STATUS) => Coupon[];
+  findReceivedCouponListByStatus: (status: COUPON_STATUS) => Coupon[];
+  findCoupon: (couponId: string) => Coupon;
+  getStatusAfterEvent: (couponEvent: COUPON_EVENT) => COUPON_STATUS;
+  updateFixture: (newFixtureCurrent: CouponDetailResponse[]) => void;
+  generateReservationList: () => { meetingDate: YYYYMMDDhhmmss; coupons: Coupon[] }[];
+}
+
+const fixture: FixtureType = {
   current: [
     {
-      couponId: 0,
+      id: 1,
       sender: {
         id: 1,
-        email: 'euijinkk97@gmail.com',
-        userId: 'aaa1234',
-        workspaceId: 'woowacourse',
-        imageUrl: 'https://avatars.githubusercontent.com/u/24906022?s=48&v=4',
-        password: '1234',
-        nickname: '시지프',
-      },
-      receiver: {
-        id: 0,
-        email: 'wnsgur8397@naver.com',
-        userId: 'aaa123',
-        workspaceId: 'woowacourse',
-        imageUrl: 'https://avatars.githubusercontent.com/u/24906022?s=48&v=4',
-        password: '1234',
+        imageUrl:
+          'https://avatars.slack-edge.com/2022-06-18/3684887341301_d2148dd98666590092cd_512.png',
         nickname: '준찌',
       },
-      memberId: 1,
-      nickname: '시지프',
-      hashtag: '한턱쏘는',
-      imageUrl: 'https://avatars.githubusercontent.com/u/24906022?s=48&v=4',
-      description: '시지프가 준찌에게 보낸 1번째 쿠폰',
+      receiver: {
+        id: 2,
+        imageUrl: 'https://avatars.githubusercontent.com/u/24906022?s=48&v=4',
+        nickname: '시지프',
+      },
+      couponTag: '한턱쏘는',
+      couponMessage: '시지프가 준찌에게 보낸 1번째 쿠폰',
       couponType: 'COFFEE',
-      couponStatus: 'ACCEPTED',
-      meetingDate: '2022-08-19',
+      couponStatus: 'READY',
+      createdTime: '2022-10-12T10:10:10',
+      meetingDate: null,
       couponHistories: [
-        {
-          id: 3,
-          nickname: '시지프',
-          imageUrl: '/assets/images/logo.png',
-          couponType: 'COFFEE',
-          couponEvent: 'REQUEST',
-          meetingDate: '2022-08-08',
-          createdTime: '2022-08-08',
-          message: '이때 만나자!',
-        },
-        {
-          id: 2,
-          nickname: '시지프',
-          imageUrl: '/assets/images/logo.png',
-          couponType: 'COFFEE',
-          couponEvent: 'INIT',
-          meetingDate: '2022-08-08',
-          createdTime: '2022-08-08',
-          message: '',
-        },
         {
           id: 1,
           nickname: '시지프',
-          imageUrl: '/assets/images/logo.png',
+          imageUrl: 'https://avatars.githubusercontent.com/u/24906022?s=48&v=4',
           couponType: 'COFFEE',
           couponEvent: 'REQUEST',
-          meetingDate: '2022-08-08',
-          createdTime: '2022-08-08',
-          message: '선릉에서 만나자!',
+          meetingMessage: '이때 만나자!',
+          meetingDate: '2022-08-08T10:10:10',
+          createdTime: '2022-08-08T10:10:10',
         },
       ],
     },
-
     {
-      couponId: 0,
+      id: 2,
       sender: {
         id: 1,
-        email: 'euijinkk97@gmail.com',
-        userId: 'aaa1234',
-        workspaceId: 'woowacourse',
-        imageUrl: 'https://avatars.githubusercontent.com/u/24906022?s=48&v=4',
-        password: '1234',
-        nickname: '시지프',
-      },
-      receiver: {
-        id: 0,
-        email: 'wnsgur8397@naver.com',
-        userId: 'aaa123',
-        workspaceId: 'woowacourse',
-        imageUrl: 'https://avatars.githubusercontent.com/u/24906022?s=48&v=4',
-        password: '1234',
+        imageUrl:
+          'https://avatars.slack-edge.com/2022-06-18/3684887341301_d2148dd98666590092cd_512.png',
         nickname: '준찌',
       },
-      memberId: 1,
-      nickname: '시지프',
-      hashtag: '한턱쏘는',
-      imageUrl: 'https://avatars.githubusercontent.com/u/24906022?s=48&v=4',
-      description: '시지프가 준찌에게 보낸 1번째 쿠폰',
+      receiver: {
+        id: 2,
+        imageUrl: 'https://avatars.githubusercontent.com/u/24906022?s=48&v=4',
+        nickname: '시지프',
+      },
+      couponTag: '한턱쏘는',
+      couponMessage: '시지프가 준찌에게 보낸 2번째 쿠폰',
       couponType: 'COFFEE',
-      couponStatus: 'ACCEPTED',
-      meetingDate: '2022-08-16',
+      couponStatus: 'REQUESTED',
+      meetingDate: '2022-10-12T10:10:10',
+      createdTime: '2022-10-12T10:10:10',
       couponHistories: [
-        {
-          id: 3,
-          nickname: '시지프',
-          imageUrl: '/assets/images/logo.png',
-          couponType: 'COFFEE',
-          couponEvent: 'REQUEST',
-          meetingDate: '2022-08-08',
-          createdTime: '2022-08-08',
-          message: '이때 만나자!',
-        },
-        {
-          id: 2,
-          nickname: '시지프',
-          imageUrl: '/assets/images/logo.png',
-          couponType: 'COFFEE',
-          couponEvent: 'INIT',
-          meetingDate: '2022-08-08',
-          createdTime: '2022-08-08',
-          message: '',
-        },
         {
           id: 1,
           nickname: '시지프',
-          imageUrl: '/assets/images/logo.png',
+          imageUrl: 'https://avatars.githubusercontent.com/u/24906022?s=48&v=4',
           couponType: 'COFFEE',
           couponEvent: 'REQUEST',
-          meetingDate: '2022-08-08',
-          createdTime: '2022-08-08',
-          message: '선릉에서 만나자!',
+          meetingMessage: '이때 만나자!',
+          meetingDate: '2022-08-08T10:10:10',
+          createdTime: '2022-08-08T10:10:10',
         },
       ],
+    },
+    {
+      id: 3,
+      sender: {
+        id: 1,
+        imageUrl:
+          'https://avatars.slack-edge.com/2022-06-18/3684887341301_d2148dd98666590092cd_512.png',
+        nickname: '준찌',
+      },
+      receiver: {
+        id: 2,
+        imageUrl: 'https://avatars.githubusercontent.com/u/24906022?s=48&v=4',
+        nickname: '시지프',
+      },
+      couponTag: '한턱쏘는',
+      couponMessage: '시지프가 준찌에게 보낸 2번째 쿠폰',
+      couponType: 'COFFEE',
+      couponStatus: 'ACCEPTED',
+      meetingDate: '2022-10-12T10:10:10',
+      createdTime: '2022-10-12T10:10:10',
+      couponHistories: [
+        {
+          id: 1,
+          nickname: '시지프',
+          imageUrl: 'https://avatars.githubusercontent.com/u/24906022?s=48&v=4',
+          couponType: 'COFFEE',
+          couponEvent: 'ACCEPT',
+          meetingMessage: '이때 만나자!',
+          meetingDate: '2022-08-08T10:10:10',
+          createdTime: '2022-08-08T10:10:10',
+        },
+      ],
+    },
+    {
+      id: 4,
+      sender: {
+        id: 2,
+        imageUrl: 'https://avatars.githubusercontent.com/u/24906022?s=48&v=4',
+        nickname: '시지프',
+      },
+      receiver: {
+        id: 1,
+        imageUrl:
+          'https://avatars.slack-edge.com/2022-06-18/3684887341301_d2148dd98666590092cd_512.png',
+        nickname: '준찌',
+      },
+      couponTag: '한턱쏘는',
+      couponMessage: '시지프가 준찌에게 보낸 2번째 쿠폰',
+      couponType: 'COFFEE',
+      couponStatus: 'REQUESTED',
+      meetingDate: '2022-10-12T10:10:10',
+      createdTime: '2022-10-12T10:10:10',
+      couponHistories: [],
     },
   ],
 
-  findReceivedCouponList(loggedUser: UserResponse) {
-    return this.current.filter(({ receiver: { id: receiverId } }) => loggedUser.id === receiverId);
+  findAcceptedCouponList() {
+    return this.current.filter(({ couponStatus }) => couponStatus === 'ACCEPTED');
   },
 
-  findSentCouponList(loggedUser: UserResponse) {
-    return this.current.filter(({ sender: { id: senderId } }) => loggedUser.id === senderId);
+  findReceivedCouponList() {
+    return this.current.filter(({ receiver }) => receiver.id === 1);
   },
 
-  findCoupon(id: string | readonly string[]) {
-    if (typeof id !== 'string') {
+  findSentCouponList() {
+    return this.current.filter(({ sender }) => sender.id === 1);
+  },
+
+  findSentCouponListByStatus(status) {
+    const sentCouponList = this.findSentCouponList();
+
+    return sentCouponList.filter(({ couponStatus }) => couponStatus === status);
+  },
+
+  findReceivedCouponListByStatus(status) {
+    const receivedCouponList = this.findReceivedCouponList();
+
+    return receivedCouponList.filter(({ couponStatus }) => couponStatus === status);
+  },
+
+  findCoupon(couponId) {
+    const coupon = this.current.find(({ id }) => Number(couponId) === id);
+
+    if (!coupon) {
       throw new Error('잘못된 접근입니다.');
     }
 
-    return this.current.find(({ couponId }) => Number(id) === couponId);
+    return coupon;
   },
 
-  getStatusAfterEvent(couponEvent: COUPON_EVENT): COUPON_STATUS {
-    switch (couponEvent) {
-      case 'REQUEST':
-        return 'REQUESTED';
-      case 'CANCEL':
-        return 'READY';
-      case 'DECLINE':
-        return 'READY';
-      case 'ACCEPT':
-        return 'ACCEPTED';
-      case 'FINISH':
-        return 'FINISHED';
-      default:
-        return 'READY';
-    }
+  getStatusAfterEvent(couponEvent) {
+    const mapper: Record<COUPON_EVENT, COUPON_STATUS> = {
+      INIT: 'READY',
+      REQUEST: 'REQUESTED',
+      CANCEL: 'READY',
+      DECLINE: 'READY',
+      ACCEPT: 'ACCEPTED',
+      FINISH: 'FINISHED',
+    };
+
+    return mapper[couponEvent] ?? 'READY';
+  },
+
+  updateFixture(newCouponList) {
+    this.current = newCouponList;
+  },
+
+  generateReservationList() {
+    const acceptedCoupons = this.findAcceptedCouponList();
+
+    const reservationMap = acceptedCoupons.reduce((prev, current) => {
+      if (!current.meetingDate) {
+        return prev;
+      }
+
+      const prevACceptedCoupons = prev[current.meetingDate] ?? [];
+
+      return { ...prev, [current.meetingDate]: [...prevACceptedCoupons, current] };
+    }, {} as Record<string, any[]>);
+
+    const reservationList = Object.entries(reservationMap).map(([key, value]) => ({
+      meetingDate: key as YYYYMMDDhhmmss,
+      coupons: value as Coupon[],
+    }));
+
+    return reservationList;
   },
 };
+
+export default fixture;
--- frontend/src/mocks/handlers/coupon.ts
+++ frontend/src/mocks/handlers/coupon.ts
@@ -2,26 +2,42 @@ import { rest } from 'msw';
 
 import { BASE_URL } from '@/apis';
 import coupons from '@/mocks/fixtures/coupon';
-import users from '@/mocks/fixtures/users';
-import { ChangeCouponStatusRequest, CreateCouponRequest } from '@/types/coupon/remote';
+import { COUPON_STATUS } from '@/types/coupon/client';
+import {
+  ChangeCouponStatusRequest,
+  CouponDetailResponse,
+  CreateCouponRequest,
+} from '@/types/coupon/remote';
+import { YYYYMMDDhhmmss } from '@/types/utils';
 
 export const couponHandler = [
-  rest.get(`${BASE_URL}/coupons`, (req, res, ctx) => {
-    const { headers } = req;
-
+  rest.get(`${BASE_URL}/coupons/accept`, (req, res, ctx) => {
     try {
-      const loggedUser = users.findLoggedUser(headers.get('authorization'));
+      const reservationList = coupons.generateReservationList();
 
-      const receivedCouponList = coupons.findReceivedCouponList(loggedUser);
-
-      const sentCouponList = coupons.findSentCouponList(loggedUser);
-
-      return res(
-        ctx.status(200),
-        ctx.json({ data: { received: receivedCouponList, sent: sentCouponList } })
-      );
+      return res(ctx.status(200), ctx.json({ data: reservationList }));
     } catch ({ message }) {
-      return res(ctx.status(400), ctx.json({ error: message }));
+      return res(ctx.status(401), ctx.json({ message }));
+    }
+  }),
+
+  rest.get(`${BASE_URL}/coupons/sent`, (req, res, ctx) => {
+    try {
+      const sentCoupons = coupons.findSentCouponList();
+
+      return res(ctx.status(200), ctx.json({ data: sentCoupons }));
+    } catch ({ message }) {
+      return res(ctx.status(401), ctx.json({ message }));
+    }
+  }),
+
+  rest.get(`${BASE_URL}/coupons/received`, (req, res, ctx) => {
+    try {
+      const receivedCoupons = coupons.findReceivedCouponList();
+
+      return res(ctx.status(200), ctx.json({ data: receivedCoupons }));
+    } catch ({ message }) {
+      return res(ctx.status(401), ctx.json({ message }));
     }
   }),
 
@@ -31,103 +47,114 @@ export const couponHandler = [
     } = req;
 
     try {
-      const coupon = coupons.findCoupon(couponId);
+      const coupon = coupons.findCoupon(couponId as string);
+
+      console.log(coupon);
 
       return res(ctx.status(200), ctx.json(coupon));
     } catch ({ message }) {
-      return res(ctx.status(400), ctx.json({ error: message }));
+      return res(ctx.status(400), ctx.json({ message }));
+    }
+  }),
+
+  rest.get(`${BASE_URL}/coupons/sent/status`, (req, res, ctx) => {
+    const { url } = req;
+
+    const searchCouponType = url.searchParams.get('type') as COUPON_STATUS;
+
+    try {
+      const sentCoupons = coupons.findSentCouponListByStatus(searchCouponType);
+
+      return res(ctx.status(200), ctx.json({ data: sentCoupons }));
+    } catch ({ message }) {
+      return res(ctx.status(401), ctx.json({ message }));
+    }
+  }),
+
+  rest.get(`${BASE_URL}/coupons/received/status`, (req, res, ctx) => {
+    const { url } = req;
+
+    const searchCouponType = url.searchParams.get('type') as COUPON_STATUS;
+
+    try {
+      const receivedCoupons = coupons.findReceivedCouponListByStatus(searchCouponType);
+
+      return res(ctx.status(200), ctx.json({ data: receivedCoupons }));
+    } catch ({ message }) {
+      return res(ctx.status(401), ctx.json({ message }));
     }
   }),
 
   rest.post<CreateCouponRequest>(`${BASE_URL}/coupons`, (req, res, ctx) => {
     const {
       body: { receiverIds, ...body },
-      headers,
     } = req;
 
     try {
-      const loggedUser = users.findLoggedUser(headers.get('authorization'));
-
-      const newCouponList = receiverIds.map(receiverId => {
-        const receiver = users.findUser(receiverId);
-
-        const newCoupon = {
+      const newCouponList: CouponDetailResponse[] = receiverIds.map(() => {
+        const newCoupon: CouponDetailResponse = {
           id: coupons.current.length + 1,
-          sender: loggedUser,
-          receiver,
+          sender: {
+            id: 1,
+            imageUrl:
+              'https://avatars.slack-edge.com/2022-06-18/3684887341301_d2148dd98666590092cd_512.png',
+            nickname: '준찌',
+          },
+          receiver: {
+            id: 2,
+            imageUrl: 'https://avatars.githubusercontent.com/u/24906022?s=48&v=4',
+            nickname: '시지프',
+          },
           couponStatus: 'READY',
+          meetingDate: null,
+          createdTime: '2022-08-08T10:10:10',
+          couponHistories: [],
           ...body,
         };
 
         return newCoupon;
       });
 
-      coupons.current = [...coupons.current, ...newCouponList];
+      coupons.updateFixture([...coupons.current, ...newCouponList]);
 
       return res(ctx.status(200), ctx.json({ data: newCouponList }));
     } catch ({ message }) {
-      return res(ctx.status(400), ctx.json({ error: message }));
+      return res(ctx.status(401), ctx.json({ message }));
     }
   }),
 
-  rest.post<ChangeCouponStatusRequest>(`${BASE_URL}/coupons/:couponId/event`, (req, res, ctx) => {
+  rest.put<ChangeCouponStatusRequest>(`${BASE_URL}/coupons/:couponId/event`, (req, res, ctx) => {
     const {
       body: { couponEvent, meetingDate },
       params: { couponId },
     } = req;
 
-    const newCouponList = coupons.current.map(coupon => {
-      if (coupon.id === Number(couponId)) {
-        if (meetingDate && couponEvent === 'REQUEST') {
-          return {
-            ...coupon,
-            couponStatus: coupons.getStatusAfterEvent(couponEvent),
-            meetingDate,
-          };
-        }
-
-        if (couponEvent === 'CANCEL') {
-          const omittedCoupon = { ...coupon };
-
-          delete omittedCoupon.meetingDate;
-
-          return { ...omittedCoupon, couponStatus: coupons.getStatusAfterEvent(couponEvent) };
-        }
-
-        return { ...coupon, couponStatus: coupons.getStatusAfterEvent(couponEvent) };
+    const newCouponList: CouponDetailResponse[] = coupons.current.map(coupon => {
+      if (coupon.id !== Number(couponId)) {
+        return coupon;
       }
 
-      return coupon;
+      const couponStatus = coupons.getStatusAfterEvent(couponEvent);
+
+      if (couponStatus === 'READY') {
+        return {
+          ...coupon,
+          couponStatus,
+          meetingDate: null,
+        };
+      }
+
+      return {
+        ...coupon,
+        couponStatus,
+        meetingDate: meetingDate
+          ? `${meetingDate}T10:10:10`
+          : `${coupon.meetingDate as YYYYMMDDhhmmss}T10:10:10`,
+      };
     });
 
-    coupons.current = newCouponList;
+    coupons.updateFixture(newCouponList);
 
     return res(ctx.status(200), ctx.json({ data: newCouponList }));
   }),
-
-  rest.post<ChangeCouponStatusRequest>(
-    `${BASE_URL}/coupons/:couponId/event/request`,
-    (req, res, ctx) => {
-      const {
-        body: { couponEvent, meetingDate },
-        params: { couponId },
-      } = req;
-
-      const newCouponList = coupons.current.map(coupon => {
-        if (coupon.id === Number(couponId)) {
-          return {
-            ...coupon,
-            couponStatus: coupons.getStatusAfterEvent(couponEvent),
-            meetingDate,
-          };
-        }
-
-        return coupon;
-      });
-
-      coupons.current = newCouponList;
-
-      return res(ctx.status(200), ctx.json({ data: newCouponList }));
-    }
-  ),
 ];
