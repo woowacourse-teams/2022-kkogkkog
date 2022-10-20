6366351fd645e1bc4806eaf18f35e19b57af6a32
junzzi <jjh01027717502@gmail.com>
2022-10-20 16:44:16 +0900

    feat: 에러 폴백 홈으로 이동할 수 있도록 구현

--- frontend/src/@components/@shared/ErrorFallback/index.tsx
+++ frontend/src/@components/@shared/ErrorFallback/index.tsx
@@ -1,6 +1,8 @@
 import { AxiosError } from 'axios';
+import { Link } from 'react-router-dom';
 
 import landingLogoImage from '@/assets/images/landing_logo.png';
+import { PATH } from '@/Router';
 import theme from '@/styles/theme';
 
 import Icon from '../Icon';
@@ -12,13 +14,18 @@ export interface ErrorFallbackProps {
   resetErrorBoundary?: () => void;
 }
 
-const ErrorFallback = ({ error, resetErrorBoundary }: ErrorFallbackProps) => {
+const ErrorFallback = ({ resetErrorBoundary }: ErrorFallbackProps) => {
   return (
-    <PageTemplate.ExtendedStyleHeader title='문제가 발생했어요'>
+    <PageTemplate.ExtendedStyleHeader title='문제가 발생했어요' hasHeader={false}>
       <Styled.Root>
-        <img src={landingLogoImage} alt='로고' width='86' />
+        <Link to={PATH.MAIN} css={Styled.EscapeSection} onClick={resetErrorBoundary}>
+          <img src={landingLogoImage} alt='로고' width={50} height={50} />
+          메인으로
+        </Link>
         <Styled.ResetSection onClick={resetErrorBoundary}>
-          <Icon iconName='reload' color={theme.colors.light_grey_200} />
+          <Styled.FlexCenter>
+            <Icon iconName='reload' color={theme.colors.light_grey_200} />
+          </Styled.FlexCenter>
           <button>다시 불러오기</button>
         </Styled.ResetSection>
       </Styled.Root>
