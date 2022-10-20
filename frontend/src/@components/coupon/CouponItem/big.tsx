c2bd5b68fd4195492d6e20cb554c5e1fb427c154
junzzi <jjh01027717502@gmail.com>
2022-10-20 13:48:35 +0900

    fix: thumbnail 객체가 src에 바인딩 되던 이슈 해결

--- frontend/src/@components/coupon/CouponItem/big.tsx
+++ frontend/src/@components/coupon/CouponItem/big.tsx
@@ -59,10 +59,10 @@ BigCouponItem.Preview = function Preview(props: BigCouponItemPreviewProps) {
   };
 
   return (
-    <Styled.Root className={className} hasCursor={false}>
+    <Styled.Root className={className}>
       <Styled.ImageContainer>
         <Styled.ImageInner>
-          <img src={thumbnail} alt='쿠폰' width={44} height={44} />
+          <img src={thumbnail.src} alt='쿠폰' width={44} height={44} />
         </Styled.ImageInner>
       </Styled.ImageContainer>
       <Styled.TextContainer>
