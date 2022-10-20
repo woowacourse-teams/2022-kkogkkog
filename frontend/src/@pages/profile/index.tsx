54ec4cf77fcacd372171bfea58d607ee443876c4
김의진 <gytks4@naver.com>
2022-10-20 11:59:39 +0900

    fix: 로그아웃 이후 라우팅 경로 수정 (#508)
    * fix: 로그아웃 이후 라우팅 경로 수정
    
    * feat: 로그아웃 성공시 Toast 메시지 보여주기
    
    * feat: 로그아웃 성공시 모든 쿼리 제거해주기

--- frontend/src/@pages/profile/index.tsx
+++ frontend/src/@pages/profile/index.tsx
@@ -2,6 +2,7 @@ import { Link, useNavigate } from 'react-router-dom';
 
 import Button from '@/@components/@shared/Button';
 import PageTemplate from '@/@components/@shared/PageTemplate';
+import { useToast } from '@/@hooks/@common/useToast';
 import { useFetchMe } from '@/@hooks/@queries/user';
 import { PATH } from '@/Router';
 
@@ -11,11 +12,13 @@ const ProfilePage = () => {
   const navigate = useNavigate();
 
   const { me, logout } = useFetchMe();
+  const { displayMessage } = useToast();
 
   const onClickLogoutButton = () => {
     logout();
 
-    navigate(PATH.MAIN);
+    navigate(PATH.LANDING);
+    displayMessage('로그아웃 되었습니다.', false);
   };
 
   return (
