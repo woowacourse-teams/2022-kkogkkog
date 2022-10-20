ef157fd76652e06c43686515f70dd7df5c8e6082
dwl21 <nggus5@naver.com>
2022-10-20 12:44:14 +0900

    fix: flyway 스키마 수정

--- backend/src/main/resources/db/migration/V3.3__join_tables_between_coupon_lazy_coupon.sql
+++ backend/src/main/resources/db/migration/V3.3__join_tables_between_coupon_lazy_coupon.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `coupon_lazy_coupon` (
      `id` BIGINT NOT NULL AUTO_INCREMENT
-     ,`coupon_id` BIGINT NOT NULL;
-     ,`lazy_coupon_id` BIGINT NOT NULL;
+     ,`coupon_id` BIGINT
+     ,`lazy_coupon_id` BIGINT NOT NULL
      ,PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
