1a0fc45dc8e62481fefc00c21cbec9463e038d01
dwl21 <nggus5@naver.com>
2022-10-20 15:17:04 +0900

    refactor: 연결 테이블 레포 이름 변경

--- backend/src/main/java/com/woowacourse/kkogkkog/lazycoupon/domain/repository/CouponLazyCouponRepository.java
+++ backend/src/main/java/com/woowacourse/kkogkkog/lazycoupon/domain/repository/CouponLazyCouponRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface LazyCouponRepository extends JpaRepository<CouponLazyCoupon, Long> {
+public interface CouponLazyCouponRepository extends JpaRepository<CouponLazyCoupon, Long> {
 
     Optional<CouponLazyCoupon> findByLazyCouponId(Long id);
 
