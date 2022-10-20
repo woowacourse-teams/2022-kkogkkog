710a78c250a14edbc2b8977e0e09c2e7c97540e0
juunzzi <78349600+juunzzi@users.noreply.github.com>
2022-10-20 17:03:44 +0900

    feat: 에러 폴백 홈으로 이동할 수 있도록 구현 (#523)

--- frontend/src/@components/@shared/ErrorFallback/style.tsx
+++ frontend/src/@components/@shared/ErrorFallback/style.tsx
@@ -1,9 +1,8 @@
-import { css } from '@emotion/react';
+import { css, Theme } from '@emotion/react';
 import styled from '@emotion/styled';
 
 export const Root = styled.div`
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
   gap: 20px;
@@ -15,16 +14,48 @@ export const Root = styled.div`
   `}
 `;
 
-export const ResetSection = styled.div`
+export const EscapeSection = (theme: Theme) => css`
+  width: 100px;
+  height: 80px;
+
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
   align-items: center;
   gap: 12px;
 
-  color: ${({ theme }) => theme.colors.light_grey_200};
+  color: ${theme.colors.primary_400};
+
   cursor: pointer;
 
   & > button {
-    font-size: 20px;
+    font-size: 16px;
   }
+`;
+
+export const ResetSection = styled.div`
+  width: 100px;
+  height: 80px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  color: ${({ theme }) => theme.colors.light_grey_200};
+
+  cursor: pointer;
+
+  & > button {
+    font-size: 16px;
+  }
+`;
+
+export const FlexCenter = styled.div`
+  width: 50px;
+  height: 50px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
