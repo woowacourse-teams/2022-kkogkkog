fb6ed96cfa96002427743866eff22b7f225b7c63
Jeong Jinwoo <jwjeong96@gmail.com>
2022-10-27 15:45:00 +0900

    refactor: 인터셉터의 URI 패턴 최소화

--- backend/src/main/java/com/woowacourse/kkogkkog/auth/config/InterceptorConfig.java
+++ backend/src/main/java/com/woowacourse/kkogkkog/auth/config/InterceptorConfig.java
@@ -18,15 +18,10 @@ public class InterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor(jwtTokenProvider))
-            .addPathPatterns("/api/v2/members/me")
-            .addPathPatterns("/api/v2/members/me/*")
-            .addPathPatterns("/api/v2/coupons")
-            .addPathPatterns("/api/v2/coupons/*")
-            .addPathPatterns("/api/v2/coupons/*/event")
-            .addPathPatterns("/api/v2/coupons/*/event/*")
-            .addPathPatterns("/api/v2/coupons/*/status")
-            .addPathPatterns("/api/v2/lazy-coupons")
-            .addPathPatterns("/api/v2/lazy-coupons/*")
+            .addPathPatterns("/api/v2/members/**")
+            .excludePathPatterns("/api/v2/members")
+            .addPathPatterns("/api/v2/coupons/**")
+            .addPathPatterns("/api/v2/lazy-coupons/**")
             .excludePathPatterns("/api/v2/lazy-coupons/code");
     }
 }
